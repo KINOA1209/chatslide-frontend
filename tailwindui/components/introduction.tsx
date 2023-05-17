1112382f52d0646a8d1879a926589243e2c02c5a
Zhiyuan Wang <wangzyzju@gmail.com>
2023-05-16 22:13:16 -0700

    Change title in landing page

--- tailwindui/components/introduction.tsx
+++ tailwindui/components/introduction.tsx
@@ -27,7 +27,7 @@ export default function Introduction() {
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
             <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
-              Doctor <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Lambda</span>
+              Dr. <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Lambda</span>
             </h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Empower education with AI</p>
