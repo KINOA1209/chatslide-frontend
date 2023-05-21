c6d929f54ca590e0f0c619a7896c7f7a170417bc
Qualia-Li <liquanlai1995@gmail.com>
2023-05-21 13:50:18 -0700

    update favicon and logo

--- tailwindui/components/ui/logo.tsx
+++ tailwindui/components/ui/logo.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import logo from '@/public/images/logo_transparent.png'
+import logo from '@/public/images/logo_no_text.png'
 
 export default function Logo() {
   return (
