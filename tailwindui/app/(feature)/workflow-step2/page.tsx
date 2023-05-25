accb271f1e74419f08d8110ef73d02740bf1df93
Qualia-Li <liquanlai1995@gmail.com>
2023-05-24 23:20:36 -0700

    visualize

--- tailwindui/app/(feature)/workflow-step2/page.tsx
+++ tailwindui/app/(feature)/workflow-step2/page.tsx
@@ -1,55 +1,59 @@
 'use client'
 
-import Link from 'next/link'
 import React, { useState, useEffect } from 'react';
 
+const OutlineVisualizer = ({ outline }) => {
+  console.log(outline);
+  const [outlineData, setOutlineData] = useState(outline);
 
-export default function WorkflowStep2() {
-  const [outline, setOutline] = useState('');
+  console.log(outlineData);
 
-  useEffect(() => {
-    const storedOutline = localStorage.getItem('outline');
-    if (storedOutline) {
-      const outline = JSON.parse(storedOutline);
-      setOutline(outline.res);
-    }
-  }, []);
-
-  const handleChange = (event) => {
-    setOutline(event.target.value);
-  }
-
+  const handleChange = (e, sectionIndex, key) => {
+    const { value } = e.target;
+    console.log(e, sectionIndex, key, value);
+//     setOutlineData((prevOutlineData) => ({
+//       ...prevOutlineData,
+//       [sectionIndex]: {
+//         ...prevOutlineData[sectionIndex],
+//         [key]: value,
+//       },
+//     }));
+  };
 
   return (
-    <section className="bg-gradient-to-b from-gray-100 to-white">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
-          {/* Page header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h1 className="h1">Step 2: Generate Slides</h1>
+    <div className="max-w-3xl mx-auto mt-8">
+      {Object.keys(outlineData).map((sectionIndex) => (
+        <div key={sectionIndex} className="mb-8">
+          <h3 className="text-xl font-bold">
+            Section {parseInt(sectionIndex)}: {outlineData[sectionIndex].title}
+          </h3>
+          <div className="mt-4">
+            {outlineData[sectionIndex].details.map((detail, detailIndex) => (
+              <input
+                key={detailIndex}
+                className="form-input w-full text-gray-800 mb-2"
+                value={detail}
+                onChange={(e) => handleChange(e, sectionIndex, 'details')}
+                placeholder={`Detail ${detailIndex}`}
+              />
+            ))}
           </div>
-
-          {/* Form */}
-          <div className="max-w-sm mx-auto">
-            <form>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Outline <span className="text-red-600">*</span></label>
-                  <textarea id="outline" type="text" className="form-input w-full text-gray-800 h-40" value={outline} onChange={handleChange} placeholder="Enter the outline" required />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mt-6">
-                <div className="w-full px-3">
-                  <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">Next</button>
-                </div>
-              </div>
-            </form>
-          </div>
-
         </div>
-      </div>
-    </section>
-  )
-}
-  
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+    const storedOutline = localStorage.getItem('outline');
+const outline = JSON.parse(storedOutline);
+const outlineRes = JSON.parse(outline.res)
+
+  return (
+    <div className="bg-gray-100 min-h-screen py-8">
+      <OutlineVisualizer outline={outlineRes} />
+    </div>
+  );
+};
+
+export default App;
