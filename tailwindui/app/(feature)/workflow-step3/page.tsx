38550ede864ce95eaa86fc4597fb2de1a262aa86
Qualia-Li <liquanlai1995@gmail.com>
2023-05-31 22:54:23 -0700

    Add generate slides

--- tailwindui/app/(feature)/workflow-step3/page.tsx
+++ tailwindui/app/(feature)/workflow-step3/page.tsx
@@ -2,23 +2,20 @@
 
 import React, {useState, useEffect} from 'react';
 import OutlineFrom from '@/components/outline-form'
-import { useRouter } from 'next/navigation';
 
-const OutlineVisualizer = ({outline}: { outline: any }) => {
-    const router = useRouter();
-    console.log(outline);
-    const [outlineData, setOutlineData] = useState(outline);
+const OutlineVisualizer = ({slide}: { slide: any }) => {
+    console.log(slide);
+    const [backendResponse, setBackendResponse] = useState(slide);
 
-    console.log(outlineData);
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>, sectionIndex: string, detailIndex: number, key: string) => {
-        const {value} = e.target;
-        setOutlineData((prevOutlineData: any) => {
-            const updatedOutlineData = JSON.parse(JSON.stringify(prevOutlineData));
-            updatedOutlineData[sectionIndex]['content'][detailIndex] = value;
-            return updatedOutlineData;
-        });
-    };
+//
+//     const handleChange = (e: React.ChangeEvent<HTMLInputElement>, sectionIndex: string, detailIndex: number, key: string) => {
+//         const {value} = e.target;
+//         setOutlineData((prevOutlineData: any) => {
+//             const updatedOutlineData = JSON.parse(JSON.stringify(prevOutlineData));
+//             updatedOutlineData[sectionIndex]['content'][detailIndex] = value;
+//             return updatedOutlineData;
+//         });
+//     };
 
       const [isSubmitting, setIsSubmitting] = useState(false);
       const [timer, setTimer] = useState(0);
@@ -63,16 +60,14 @@ const OutlineVisualizer = ({outline}: { outline: any }) => {
             body: JSON.stringify(formData)
           });
 
-//           console.log(formData);
-//           console.log(response);
+          console.log(formData);
+          console.log(response);
 
           if (response.ok) {
-            const resp = await response.json();
-            console.log(resp);
+            const slide = await response.json();
             setIsSubmitting(false);
             // Store the data in local storage
-            console.log(resp.data);
-            localStorage.setItem('slide', JSON.stringify(resp.data.res));
+            localStorage.setItem('slide', JSON.stringify(slide.data));
 
             // Redirect to a new page with the data
             router.push('workflow-step3');
@@ -96,25 +91,16 @@ const OutlineVisualizer = ({outline}: { outline: any }) => {
                     <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
                         <h1 className="h1">Step 3: Edit Slides</h1>
                     </div>
-                    {outlineData && Object.keys(outlineData).map((sectionIndex) => (
-                        <div key={sectionIndex} className="mb-8">
-                            <h3 className="text-xl font-bold">
-                                Section {parseInt(sectionIndex)}: {outlineData[sectionIndex].title}
-                            </h3>
-                            <div className="mt-4">
-                                {outlineData[sectionIndex].content.map((detail: any, detailIndex: number) => (
-                                    <input
-                                        key={detailIndex}
-                                        className="form-input w-full text-gray-800 mb-2"
-                                        value={detail}
-                                        onChange={(e) => handleChange(e, sectionIndex, detailIndex, 'content')}
-                                        placeholder={`Detail ${detailIndex}`}
-                                    />
-                                ))}
-                            </div>
-                        </div>
-                    ))}
-
+ <div>
+        <h2>Text:</h2>
+        <textarea
+          className="w-full px-3 py-2 mt-2 text-gray-700 border rounded-lg focus:outline-none focus:shadow-outline"
+          value={backendResponse}
+          onChange={(e) => setBackendResponse(e.target.value)}
+          rows={10}
+          cols={50}
+        />
+      </div>
 
                     {/* Form */}
                     <div className="max-w-sm mx-auto">
@@ -135,13 +121,11 @@ const OutlineVisualizer = ({outline}: { outline: any }) => {
 };
 
 const App = () => {
-    const storedOutline = typeof localStorage !== 'undefined' ? localStorage.getItem('outline') : null;
-    const outline = storedOutline ? JSON.parse(storedOutline) : null;
-    const outlineRes = outline ? JSON.parse(outline.res) : null;
+    const slide = localStorage.getItem('slide');
 
     return (
         <div className="bg-gray-100 min-h-screen py-8">
-            <OutlineVisualizer outline={outlineRes}/>
+            <OutlineVisualizer slide={slide}/>
         </div>
     );
 };
--- tailwindui/components/topic-form.tsx
+++ tailwindui/components/topic-form.tsx
@@ -36,6 +36,11 @@ const TopicForm: React.FC = () => {
         language: (event.target as HTMLFormElement).language.value,
     };
 
+    localStorage.setItem('topic', formData.topic);
+    localStorage.setItem('requirements', formData.requirements);
+    localStorage.setItem('audience', formData.audience);
+    localStorage.setItem('language', formData.language);
+
     console.log("created form data");
 
     try {
@@ -59,17 +64,20 @@ const TopicForm: React.FC = () => {
         console.log(outlinesJson.data.requirements);
         console.log(outlinesJson.data.topic);
         console.log(outlinesJson.data.res);
+        console.log(outlinesJson.data.foldername);
+
 
         // cookies doesn't work because it needs 'use server'
         // cookies().set("topic", outlinesJson.data.audience);
 
         // Store the data in local storage
         localStorage.setItem('outline', JSON.stringify(outlinesJson.data));
+        localStorage.setItem('foldername', outlinesJson.data.foldername);
 
         // Redirect to a new page with the data
         router.push('workflow-step2');
       } else {
-//         alert("Request failed: " + response.status);
+        alert("Request failed: " + response.status);
         console.log(response)
         setIsSubmitting(false);
       }
