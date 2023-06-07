c7642c87922e5fe4367702bec3aceb568bc8b53b
Rex1012 <haorong.chen97@gmail.com>
2023-06-07 14:15:38 -0700

    updated footer and logo points to / instead of drlambda.ai

--- tailwindui/components/ui/footer.tsx
+++ tailwindui/components/ui/footer.tsx
@@ -1,26 +1,36 @@
-import Logo from './logo'
+import Logo from "./logo";
 
 export default function Footer() {
   return (
     <footer>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
         {/* Top area: Blocks */}
         <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
-
           {/* 1st block */}
           <div className="sm:col-span-12 lg:col-span-3">
-            <div className="mb-2">
+            <div className="flex justify-center mb-2">
               <Logo />
             </div>
-            <div className="text-sm text-gray-600">
-              <a href="#0" className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">Terms</a> · <a href="#0" className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">Privacy Policy</a>
+            <div className="flex justify-center mb-2 text-sm text-gray-600">
+              <a
+                href="/terms"
+                className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out"
+              >
+                Terms
+              </a>{" "}
+              ·{" "}
+              <a
+                href="/privacy"
+                className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out"
+              >
+                Privacy Policy
+              </a>
             </div>
           </div>
 
           {/* 2nd block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">Products</h6>
+            {/* <h6 className="text-gray-800 font-medium mb-2">Products</h6>
             <ul className="text-sm">
               <li className="mb-2">
                 <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Web Studio</a>
@@ -37,12 +47,12 @@ export default function Footer() {
               <li className="mb-2">
                 <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Command-line</a>
               </li>
-            </ul>
+            </ul> */}
           </div>
 
           {/* 3rd block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">Resources</h6>
+            {/* <h6 className="text-gray-800 font-medium mb-2">Resources</h6>
             <ul className="text-sm">
               <li className="mb-2">
                 <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Documentation</a>
@@ -59,12 +69,12 @@ export default function Footer() {
               <li className="mb-2">
                 <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Partners</a>
               </li>
-            </ul>
+            </ul> */}
           </div>
 
           {/* 4th block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-800 font-medium mb-2">Company</h6>
+            {/* <h6 className="text-gray-800 font-medium mb-2">Company</h6>
             <ul className="text-sm">
               <li className="mb-2">
                 <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Home</a>
@@ -81,23 +91,47 @@ export default function Footer() {
               <li className="mb-2">
                 <a href="#0" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Privacy Policy</a>
               </li>
-            </ul>
+            </ul> */}
           </div>
 
           {/* 5th block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
             <h6 className="text-gray-800 font-medium mb-2">Subscribe</h6>
-            <p className="text-sm text-gray-600 mb-4">Get the latest news and articles to your inbox every month.</p>
+            <p className="text-sm text-gray-600 mb-4">
+              Get the latest news and articles to your inbox every month.
+            </p>
             <form>
               <div className="flex flex-wrap mb-4">
                 <div className="w-full">
-                  <label className="block text-sm sr-only" htmlFor="newsletter">Email</label>
+                  <label className="block text-sm sr-only" htmlFor="newsletter">
+                    Email
+                  </label>
                   <div className="relative flex items-center max-w-xs">
-                    <input id="newsletter" type="email" className="form-input w-full text-gray-800 px-3 py-2 pr-12 text-sm" placeholder="Your email" required />
-                    <button type="submit" className="absolute inset-0 left-auto" aria-label="Subscribe">
-                      <span className="absolute inset-0 right-auto w-px -ml-px my-2 bg-gray-300" aria-hidden="true"></span>
-                      <svg className="w-3 h-3 fill-current text-blue-600 mx-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
+                    <input
+                      id="newsletter"
+                      type="email"
+                      className="form-input w-full text-gray-800 px-3 py-2 pr-12 text-sm"
+                      placeholder="Your email"
+                      required
+                    />
+                    <button
+                      type="submit"
+                      className="absolute inset-0 left-auto"
+                      aria-label="Subscribe"
+                    >
+                      <span
+                        className="absolute inset-0 right-auto w-px -ml-px my-2 bg-gray-300"
+                        aria-hidden="true"
+                      ></span>
+                      <svg
+                        className="w-3 h-3 fill-current text-blue-600 mx-3 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                          fillRule="nonzero"
+                        />
                       </svg>
                     </button>
                   </div>
@@ -107,36 +141,50 @@ export default function Footer() {
               </div>
             </form>
           </div>
-
         </div>
 
         {/* Bottom area */}
         <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
-
           {/* Social as */}
           <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
             <li>
-              <a href="https://twitter.com/drlambda_ai" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter">
-                <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+              <a
+                href="https://twitter.com/drlambda_ai"
+                className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
+                aria-label="Twitter"
+              >
+                <svg
+                  className="w-8 h-8 fill-current"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
                 </svg>
               </a>
             </li>
             <li className="ml-4">
-              <a href="https://www.youtube.com/@drlambda_ai" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Facebook">
-                <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                     <path d="M29.5,11.3c-0.32-1.54-1.54-2.75-3.08-3.08C20.75,7.2,16,7.2,16,7.2s-4.75,0-10.42,1.02c-1.54,0.33-2.75,1.54-3.08,3.08 C2.2,14.25,2.2,16,2.2,16s0,1.75,0.3,3.7c0.33,1.54,1.54,2.75,3.08,3.08C11.25,23.8,16,23.8,16,23.8s4.75,0,10.42-1.02 c1.54-0.33,2.75-1.54,3.08-3.08c0.3-1.95,0.3-3.7,0.3-3.7S29.8,12.85,29.5,11.3z M13.5,18.15V11.85L21.35,16L13.5,18.15z"/>
+              <a
+                href="https://www.youtube.com/@drlambda_ai"
+                className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
+                aria-label="Facebook"
+              >
+                <svg
+                  className="w-8 h-8 fill-current"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M29.5,11.3c-0.32-1.54-1.54-2.75-3.08-3.08C20.75,7.2,16,7.2,16,7.2s-4.75,0-10.42,1.02c-1.54,0.33-2.75,1.54-3.08,3.08 C2.2,14.25,2.2,16,2.2,16s0,1.75,0.3,3.7c0.33,1.54,1.54,2.75,3.08,3.08C11.25,23.8,16,23.8,16,23.8s4.75,0,10.42-1.02 c1.54-0.33,2.75-1.54,3.08-3.08c0.3-1.95,0.3-3.7,0.3-3.7S29.8,12.85,29.5,11.3z M13.5,18.15V11.85L21.35,16L13.5,18.15z" />
                 </svg>
               </a>
             </li>
           </ul>
 
           {/* Copyrights note */}
-          <div className="text-sm text-gray-600 mr-4">&copy; drlambda.ai. All rights reserved.</div>
-
+          <div className="text-sm text-gray-600 mr-4">
+            &copy; drlambda.ai. All rights reserved.
+          </div>
         </div>
-
       </div>
     </footer>
-  )
+  );
 }
