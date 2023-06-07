4858312dd5b5b6dbc116cbdc4ed8fc2f1b553ba0
Rex1012 <haorong.chen@qq.com>
2023-06-07 14:15:38 -0700

    updated footer and logo points to / instead of drlambda.ai

--- tailwindui/components/ui/logo.tsx
+++ tailwindui/components/ui/logo.tsx
@@ -1,15 +1,11 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import logo from '@/public/images/logo_no_text.png'
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/images/logo_no_text.png";
 
 export default function Logo() {
   return (
-        <Link href="https://drlambda.ai" className="block" aria-label="Cruip">
-          <Image
-            src={logo}
-            alt="Dr. Lambda"
-            className="w-16 h-16"
-          />
-        </Link>
-  )
+    <Link href="/" className="block" aria-label="Cruip">
+      <Image src={logo} alt="Dr. Lambda" className="w-16 h-16" />
+    </Link>
+  );
 }
