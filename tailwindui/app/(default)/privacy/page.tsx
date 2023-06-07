9de5b18f4fb1245f7a28b7df118ef5cc931e4df1
Rex1012 <haorong.chen@qq.com>
2023-06-07 10:48:51 -0700

    created templates for tos and privacy page

---
+++ tailwindui/app/(default)/privacy/page.tsx
@@ -0,0 +1,24 @@
+export const metadata = {
+  title: "Workflow - Dr. Lambda",
+  description: "terms and condtions",
+};
+
+export default function Terms() {
+  return (
+    <section className="bg-gradient-to-b from-gray-100 to-white">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+          {/* Page header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h1 className="h1">Privacy</h1>
+          </div>
+
+          {/* Form */}
+          <div className="max-w-sm mx-auto">
+            <p>Privacy</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
