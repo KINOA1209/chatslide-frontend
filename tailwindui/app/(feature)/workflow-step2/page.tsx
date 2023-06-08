eac24a64a9145029983025f91b9874ac9e877f43
Qualia-Li <liquanlai1995@gmail.com>
2023-06-08 09:44:06 -0700

    wip support chinese pdf generation

--- tailwindui/app/(feature)/workflow-step2/page.tsx
+++ tailwindui/app/(feature)/workflow-step2/page.tsx
@@ -49,12 +49,14 @@ const OutlineVisualizer = ({outline}: { outline: any }) => {
         const audience = typeof window !== 'undefined' ? localStorage.getItem('audience') : null;
         const foldername = typeof window !== 'undefined' ? localStorage.getItem('foldername') : null;
         const topic = typeof window !== 'undefined' ? localStorage.getItem('topic') : null;
+        const language = typeof window !== 'undefined' ? localStorage.getItem('language') : null;
 
         const formData = {
             res: outline,
             audience: audience,
             foldername: foldername,
             topic: topic,
+            language: language,
             additional_requirements: 'test',
         };
 
