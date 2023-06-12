4a892b5c92c5f56f027483a41f6d76c3f22c9799
Qualia-Li <liquanlai1995@gmail.com>
2023-06-12 16:17:43 -0700

    resize slides

--- tailwindui/components/Slides.tsx
+++ tailwindui/components/Slides.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ImageList from './ImageList';
 
-const App: React.FC = () => {
+type AppProps = {
+  height?: number,
+};
+
+const App: React.FC<AppProps> = ({ height = 80 }) => {
   const foldername = typeof localStorage !== 'undefined' ? localStorage.getItem('foldername') : '';
   const image_files = typeof localStorage !== 'undefined' ?  JSON.parse(localStorage.getItem('image_files') || '') : [];
 
@@ -9,7 +13,7 @@ const App: React.FC = () => {
 
   return (
     <div>
-      <ImageList urls={imageUrls} />
+      <ImageList urls={imageUrls} height={height} />
     </div>
   );
 };
