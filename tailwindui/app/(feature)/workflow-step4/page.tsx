f2827242fc502118bdb07a23167c41bc25531d79
Qualia-Li <liquanlai1995@gmail.com>
2023-06-11 17:13:57 -0700

    make page4 work

--- tailwindui/app/(feature)/workflow-step4/page.tsx
+++ tailwindui/app/(feature)/workflow-step4/page.tsx
@@ -1,146 +1,113 @@
 'use client'
 
-import React, {useState, useEffect, FormEvent} from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { useRouter } from 'next/navigation';
+import Timer from '@/components/Timer';
 
-const TranscriptVisualizer = ({transcript}: { transcript: any }) => {
-  const [transcriptData, setTranscriptData] = useState(transcript);
-  const router = useRouter();
+const TranscriptVisualizer = ({ transcript }: { transcript: any }) => {
+    const [transcriptData, setTranscriptData] = useState(transcript);
+    const router = useRouter();
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, sectionIndex: string, detailIndex: number, key: string) => {
-    const {value} = e.target;
-    setTranscriptData((prevOutlineData: any) => {
-        const updatedOutlineData = JSON.parse(JSON.stringify(prevOutlineData));
-        updatedOutlineData[sectionIndex]['content'][detailIndex] = value;
-        return updatedOutlineData;
-    });
-  };
-
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [timer, setTimer] = useState(0);
-
-  useEffect(() => {
-    let interval: NodeJS.Timeout;
-
-    if (isSubmitting) {
-      interval = setInterval(() => {
-        setTimer(prevTimer => prevTimer + 1); // Increment the timer
-      }, 1000); // Increment every second
-    }
-
-    return () => {
-      clearInterval(interval); // Clear the interval on component unmount
-    };
-  }, [isSubmitting]);
-
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    console.log("submitting");
-    event.preventDefault();
-    localStorage.setItem('transcript', JSON.stringify(transcriptData));
-
-    setIsSubmitting(true);
-    setTimer(0);
-
-    const foldername = typeof window !== 'undefined' ? localStorage.getItem('foldername') : null;
-
-    const formData = {
-        res: transcriptData,
-        foldername: foldername,
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>, sectionIndex: string, detailIndex: number, key: string) => {
+        const { value } = e.target;
+        setTranscriptData((prevOutlineData: any) => {
+            const updatedOutlineData = JSON.parse(JSON.stringify(prevOutlineData));
+            updatedOutlineData[sectionIndex]['content'][detailIndex] = value;
+            return updatedOutlineData;
+        });
     };
 
-    console.log(formData);
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
-    try {
-      const response = await fetch('/api/generate_audio', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      });
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        console.log("submitting");
+        event.preventDefault();
+        localStorage.setItem('transcript', JSON.stringify(transcriptData));
 
-      if (response.ok) {
-        const resp = await response.json();
-        console.log(resp);
-        setIsSubmitting(false);
-        // Store the data in local storage
-        console.log(resp.data);
-        localStorage.setItem('audio_files', JSON.stringify(resp.data.res));
+        setIsSubmitting(true);
 
-        // Redirect to a new page with the data
-        router.push('workflow-step3');
-      } else {
-        alert("Request failed: " + response.status);
-        console.log(response)
-        setIsSubmitting(false);
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      setIsSubmitting(false);
-    }
-  };
+        const foldername = typeof window !== 'undefined' ? localStorage.getItem('foldername') : null;
 
-  return (
-    <section className="bg-gradient-to-b from-gray-100 to-white">
-    <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+        const formData = {
+            res: transcriptData,
+            foldername: foldername,
+        };
 
-        {/* Page header */}
-        <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h1 className="h1">Step 4: Edit Transcript</h1>
-        </div>
+        console.log(formData);
 
-        {transcriptData && Object.keys(transcriptData).map((sectionIndex) => (
-            <div key={sectionIndex} className="mb-8">
-                <h3 className="text-xl font-bold">
-                    Section {parseInt(sectionIndex)}: {transcriptData[sectionIndex].title}
-                </h3>
-                <div className="mt-4">
-                    {transcriptData[sectionIndex].content.map((detail: any, detailIndex: number) => (
-                        <input
-                            key={detailIndex}
-                            className="form-input w-full text-gray-800 mb-2"
-                            value={detail}
-                            onChange={(e) => handleChange(e, sectionIndex, detailIndex, 'content')}
-                            placeholder={`Detail ${detailIndex}`}
+        try {
+            const response = await fetch('/api/generate_audio', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+
+            if (response.ok) {
+                const resp = await response.json();
+                console.log(resp);
+                setIsSubmitting(false);
+                // Store the data in local storage
+                console.log(resp.data);
+                localStorage.setItem('audio_files', JSON.stringify(resp.data.res));
+
+                // Redirect to a new page with the data
+                router.push('workflow-step3');
+            } else {
+                alert("Request failed: " + response.status);
+                console.log(response)
+                setIsSubmitting(false);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            setIsSubmitting(false);
+        }
+    };
+
+    return (
+        <section className="bg-gradient-to-b from-gray-100 to-white">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6">
+                <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+                    {/* Page header */}
+                    <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+                        <h1 className="h1">Step 4: Edit Transcript</h1>
+                    </div>
+                    <div className="mt-4">
+                        <textarea
+                            className="form-input w-full text-gray-800 mb-2 resize-none h-80"
+                            value={transcriptData}
+                            onChange={(event) => setTranscriptData(event.target.value)}
                         />
-                    ))}
+                    </div>
+
+
+                    {/* Form */}
+                    <div className="max-w-sm mx-auto">
+                        <form onSubmit={handleSubmit}>
+                            <div className="flex flex-wrap -mx-3 mt-6">
+                                <div className="w-full px-3">
+                                    <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">
+                                        Generate Slides
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <Timer expectedSeconds={30} isSubmitting={isSubmitting} />
+
                 </div>
             </div>
-        ))}
-
-        {/* Form */}
-        <div className="max-w-sm mx-auto">
-            <form onSubmit={handleSubmit}>
-                <div className="flex flex-wrap -mx-3 mt-6">
-                    <div className="w-full px-3">
-                        <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">
-                            Generate Slides
-                        </button>
-                    </div>
-                </div>
-            </form>
-        </div>
-
-        {isSubmitting && (
-        <div className="mt-4 text-center">
-            <span className="text-sm text-gray-500">
-            This usually takes 15 seconds. Time elapsed: {timer} seconds
-            </span>
-        </div>
-        )}
-
-        </div>
-    </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default function WorkflowStep4() {
-  const transcriptData = typeof localStorage !== 'undefined' ? localStorage.getItem('transcript') : null;
-
-  <div className="bg-gray-100 min-h-screen py-8">
-    <TranscriptVisualizer transcript={transcriptData}/>
-  </div>
+    const transcriptData = typeof localStorage !== 'undefined' ? localStorage.getItem('transcript') : null;
+    return (
+        <div className="bg-gray-100 min-h-screen py-8">
+            <TranscriptVisualizer transcript={transcriptData} />
+        </div>)
 }
-    
