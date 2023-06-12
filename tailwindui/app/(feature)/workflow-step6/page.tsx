e9b51367e58f0f15683b10c017aaf26d23f7968d
Qualia-Li <liquanlai1995@gmail.com>
2023-06-11 23:07:57 -0700

    add review video

---
+++ tailwindui/app/(feature)/workflow-step6/page.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import React, { useState, useEffect, FormEvent } from 'react';
+import { useRouter } from 'next/navigation';
+import Video from '@/components/Video';
+
+const VideoVisualizer = ({ videoFile, foldername }: { videoFile: string, foldername: string }) => {
+    const router = useRouter();
+
+    return (
+        <section className="bg-gradient-to-b from-gray-100 to-white">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6">
+                <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+                    {/* Page header */}
+                    <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+                        <h1 className="h1">Step 6: Review Video</h1>
+                    </div>
+
+                    <Video filename={videoFile} foldername={foldername} />
+
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default function WorkflowStep6() {
+    const videoFile = typeof localStorage !== 'undefined' ? (localStorage.getItem('video_file') || "") : "";
+    const foldername = typeof localStorage !== 'undefined' ? (localStorage.getItem('foldername') || "") : "";
+
+    return (
+        <div className="bg-gray-100 min-h-screen py-8">
+            <VideoVisualizer videoFile={videoFile} foldername={foldername} />
+        </div>)
+}
---
+++ tailwindui/components/Video.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface Props {
+    filename: string;
+    foldername: string;
+}
+
+const VideoPlayer: React.FC<Props> = ({ filename, foldername }) => {
+    const videoSource = `/api/video?foldername=${foldername}&filename=${filename}`;
+
+    return (
+        <div>
+            <video height="720" controls>
+                <source src={videoSource} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    );
+};
+
+export default VideoPlayer;
