8b58323d99c71fe2dd97dbeaafd1e90cd1b58d2c
Rex1012 <haorong.chen@qq.com>
2023-06-15 15:31:57 -0700

    add dropdown functions and fix header behaviors

--- tailwindui/components/ui/header.tsx
+++ tailwindui/components/ui/header.tsx
@@ -4,48 +4,19 @@ import { useState, useEffect } from "react";
 
 import Link from "next/link";
 import Logo from "./logo";
-import Dropdown from "@/components/utils/dropdown";
+import DropdownButton from "@/components/utils/dropdown";
 import MobileMenu from "./mobile-menu";
-import { useRouter, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
   const [top, setTop] = useState<boolean>(true);
   const [accessToken, setAccessToken] = useState("");
-  const router = useRouter();
+  const [loading, setLoading] = useState(true);
   const pathname = usePathname();
 
   // detect whether user has scrolled the page down by 10px
   const scrollHandler = () => {
     window.pageYOffset > 10 ? setTop(false) : setTop(true);
-  };
-
-  const handleSignOut = async () => {
-    //send a request to the server to delete the access token
-
-    const headers = new Headers();
-    if (accessToken) {
-      console.log("access token", accessToken);
-      headers.append("Authorization", `Bearer ${accessToken}`);
-    }
-    headers.append("Content-Type", "application/json");
-
-    try {
-      const response = await fetch("/api/logout", {
-        method: "GET",
-        headers: headers,
-      });
-      console.log(response);
-
-      // Remove the access token from local storage
-      localStorage.removeItem("access_token");
-      localStorage.removeItem("signed_in");
-
-      setTimeout(() => {
-        console.log(router.push("/"));
-      }, 500);
-    } catch (error) {
-      console.log(error);
-    }
   };
 
   useEffect(() => {
@@ -61,17 +32,12 @@ export default function Header() {
     } else {
       setAccessToken("");
     }
-    console.log("access token", accessToken);
+    setLoading(false);
   }, [pathname]);
 
-  //write a function when jumped to new page, run setAccessToken
-  // useEffect(() => {
-  //   const token = localStorage.getItem("access_token");
-  //   if (token) {
-  //     setAccessToken(token);
-  //   }
-  //   console.log("access token", accessToken);
-  // }, [router.pathname]);
+  if (loading) {
+    return <p></p>;
+  }
 
   return (
     <header
@@ -89,7 +55,11 @@ export default function Header() {
           {/* Desktop navigation */}
           <nav className="hidden md:flex md:grow">
             {/* Desktop sign in links */}
-            {!accessToken && (
+            {accessToken ? (
+              <ul className="flex grow justify-end flex-wrap items-center">
+                <DropdownButton />
+              </ul>
+            ) : (
               <ul className="flex grow justify-end flex-wrap items-center">
                 <li>
                   <Link
@@ -108,27 +78,8 @@ export default function Header() {
                   </Link>
                 </li>
               </ul>
-            )}
-            {accessToken && (
-              <ul className="flex grow justify-end flex-wrap items-center">
-                <li>
-                  <button
-                    className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3"
-                    onClick={() => router.push("/dashboard")}
-                  >
-                    Dashboard
-                  </button>
-                </li>
-                <li>
-                  <button
-                    className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3"
-                    onClick={handleSignOut}
-                  >
-                    Sign out
-                  </button>
-                </li>
-              </ul>
-            )}
+
+              )}
           </nav>
 
           <MobileMenu />
