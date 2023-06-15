62aad2c81c3ff6a168a7a521fe4c5813399e8948
Zhiyuan Wang <wangzyzju@gmail.com>
2023-06-14 23:01:25 -0700

    Add create project backend api and frontend placeholder page

---
+++ tailwindui/app/(feature)/workflow-create-project/page.tsx
@@ -0,0 +1,21 @@
+export default function CreateProject() {
+    return (
+        <section className="bg-gradient-to-b from-gray-100 to-white">
+          <div className="max-w-6xl mx-auto px-4 sm:px-6">
+            <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+            {/* Page header */}
+            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+                <h1 className="h1">Step 1: Create new project</h1>
+            </div>
+
+            {/* Form */}
+            <div className="max-w-sm mx-auto">
+                {/* placeholder */}
+            </div>
+
+            </div>
+          </div>
+        </section>
+    )
+}
