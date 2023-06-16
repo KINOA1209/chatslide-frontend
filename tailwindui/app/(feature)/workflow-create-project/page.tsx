90a9475cf5bb8b8d61ec231a41052c9fa1b11b04
Rex1012 <haorong.chen@qq.com>
2023-06-15 18:20:54 -0700

    added a submit handler and link dashboard to create project page

--- tailwindui/app/(feature)/workflow-create-project/page.tsx
+++ tailwindui/app/(feature)/workflow-create-project/page.tsx
@@ -1,10 +1,14 @@
 "use client"
 
 import React, { useState, ChangeEvent, FormEvent } from 'react';
+import { useRouter } from 'next/navigation';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function CreateProject() {
   const [projectName, setProjectName] = useState('');
   const [projectDescription, setProjectDescription] = useState('');
+  const router = useRouter();
 
   const handleProjectNameChange = (event: ChangeEvent<HTMLInputElement>) => {
     setProjectName(event.target.value);
@@ -13,11 +17,52 @@ export default function CreateProject() {
     setProjectDescription(event.target.value);
   };
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // Perform submission logic here
-    console.log('Project Name:', projectName);
-    console.log('Project Description:', projectDescription);
+    const formData = {
+      project_name: (event.target as HTMLFormElement).project_name.value,
+      project_description: (event.target as HTMLFormElement).project_description.value,
+    }
+    
+    try {
+      const response = await fetch("/api/create-project", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      console.log(formData);
+      console.log(response);
+
+      if (response.ok) {
+        const projectCreateInfo = await response.json();
+        console.log(projectCreateInfo);
+        if (projectCreateInfo.status === "success") {
+          setTimeout(() => {
+            router.push("/workflow-intro");
+          }, 500);
+        } else {
+          toast.error(projectCreateInfo.message, {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+      }
+    } catch (error) {
+      console.log("Error:", error);
+    }
+
+
+    
   };
 
     return (
@@ -35,12 +80,12 @@ export default function CreateProject() {
             <div className="max-w-sm mx-auto">
             <form onSubmit={handleSubmit}>
               <div className="mb-4">
-                <label htmlFor="projectName" className="block font-medium text-gray-700">
+                <label htmlFor="project_name" className="block font-medium text-gray-700">
                   Project Name
                 </label>
                 <input
                   type="text"
-                  id="projectName"
+                  id="project_name"
                   className="mt-1 focus:ring-blue-600 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                   value={projectName}
                   onChange={handleProjectNameChange}
@@ -48,11 +93,11 @@ export default function CreateProject() {
               </div>
 
               <div className="mb-4">
-                <label htmlFor="projectDescription" className="block font-medium text-gray-700">
+                <label htmlFor="project_description" className="block font-medium text-gray-700">
                   Project Description
                 </label>
                 <textarea
-                  id="projectDescription"
+                  id="project_description"
                   className="mt-1 focus:ring-blue-600 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                   value={projectDescription}
                   onChange={handleProjectDescriptionChange}
@@ -65,6 +110,7 @@ export default function CreateProject() {
               >
                 Create Project
               </button>
+              <ToastContainer />
             </form>
           </div>
             </div>
