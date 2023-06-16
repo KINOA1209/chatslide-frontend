8b80cb2c2f4dfc221d83a488bf9d479064d3e041
Rex1012 <haorong.chen97@gmail.com>
2023-06-15 18:05:24 -0700

    fill in create project template

--- tailwindui/app/(dashboard)/dashboard/page.tsx
+++ tailwindui/app/(dashboard)/dashboard/page.tsx
@@ -12,20 +12,20 @@ export default function Dashboard() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h1 className="h1 text-blue-500">User Dashboard</h1>
+            <h1 className="h1 text-blue-600">User Dashboard</h1>
           </div>
           <div className="grid gap-4">
             {projects.map((project) => (
               <div
                 key={project.id}
-                className="border-solid border-2 border-blue-500 p-4 rounded-md shadow-md"
+                className="border-solid border-2 border-blue-600 p-4 rounded-md shadow-md"
               >
                 <h2 className="text-lg font-semibold">{project.name}</h2>
                 <p className="text-sm mt-2">{project.description}</p>
               </div>
             ))}
             <div className="flex justify-center items-center my-20">
-              <button className="flex items-center bg-blue-500 text-white px-4 py-2 rounded-md shadow-md">
+              <button className="flex items-center bg-blue-600 text-white px-4 py-2 rounded-md shadow-md">
                 <span>Start New Project</span>
               </button>
             </div>
