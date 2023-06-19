4540810c34e980cc77c115febf127fade9a41f86
Rex1012 <haorong.chen@qq.com>
2023-06-19 00:54:15 -0700

    udpate dashboard ui

--- tailwindui/app/(dashboard)/dashboard/page.tsx
+++ tailwindui/app/(dashboard)/dashboard/page.tsx
@@ -1,13 +1,35 @@
+"use client"
+
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 
 const projects = [
   { id: 1, name: 'Project 1', description: 'This is the first project.' },
   { id: 2, name: 'Project 2', description: 'This is the second project.' },
   { id: 3, name: 'Project 3', description: 'This is the third project.' },
+  { id: 4, name: 'Project 4', description: 'This is the fourth project.' },
+  { id: 5, name: 'Project 5', description: 'This is the fifth project.' },
+  { id: 6, name: 'Project 6', description: 'This is the sixth project.' },
+  // Add more projects if needed
 ];
 
 export default function Dashboard() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const projectsPerPage = 3;
+  const totalPages = Math.ceil(projects.length / projectsPerPage);
+  const indexOfLastProject = currentPage * projectsPerPage;
+  const indexOfFirstProject = indexOfLastProject - projectsPerPage;
+  const currentProjects = projects.slice(indexOfFirstProject, indexOfLastProject);
+
+  const goToNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const goToPreviousPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
   return (
     <section className="bg-gradient-to-b from-gray-100 to-white">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
@@ -15,8 +37,8 @@ export default function Dashboard() {
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h1 className="h1 text-blue-600">User Dashboard</h1>
           </div>
-          <div className="grid gap-4">
-            {projects.map((project) => (
+          <div className="flex flex-col gap-4">
+            {currentProjects.map((project) => (
               <div
                 key={project.id}
                 className="border-solid border-2 border-blue-600 p-4 rounded-md shadow-md"
@@ -25,11 +47,33 @@ export default function Dashboard() {
                 <p className="text-sm mt-2">{project.description}</p>
               </div>
             ))}
-            <div className="flex justify-center items-center my-20">
-              <Link className="flex items-center bg-blue-600 text-white px-4 py-2 rounded-md shadow-md" href={'/workflow-create-project'}>
-                <span>Start New Project</span>
-              </Link>
-            </div>
+          </div>
+          <div className="flex justify-center items-center mt-6">
+            <button
+              className={`bg-blue-600 text-white px-4 py-2 rounded-md shadow-md mr-2 ${
+                currentPage === 1 ? 'opacity-50 cursor-not-allowed' : ''
+              }`}
+              onClick={goToPreviousPage}
+              disabled={currentPage === 1}
+            >
+              Previous
+            </button>
+            <button
+              className={`bg-blue-600 text-white px-4 py-2 rounded-md shadow-md ${
+                currentPage === totalPages ? 'opacity-50 cursor-not-allowed' : ''
+              }`}
+              onClick={goToNextPage}
+              disabled={currentPage === totalPages}
+            >
+              Next
+            </button>
+          </div>
+          <div className="flex justify-center items-center mt-8">
+            <Link href="/workflow-create-project">
+              <span className="flex items-center bg-blue-600 text-white px-4 py-2 rounded-md shadow-md cursor-pointer">
+                Start New Project
+              </span>
+            </Link>
           </div>
         </div>
       </div>
