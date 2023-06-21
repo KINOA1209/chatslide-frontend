736983ca6b777697569aa35195f838d3efe2aaae
Rex1012 <haorong.chen97@gmail.com>
2023-06-21 13:47:50 -0700

    fix create project workflow and toast land in place

--- tailwindui/app/(feature)/workflow-intro/page.tsx
+++ tailwindui/app/(feature)/workflow-intro/page.tsx
@@ -5,37 +5,9 @@
 //   description: 'Create new content',
 // }
 
-import Link from "next/link";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useRouter, useSearchParams } from "next/navigation";
-import React, { useState, useEffect } from "react";
-
 export default function WorkflowIntro() {
-  // const signed_in = useSearchParams().toString();
-  if (typeof window !== "undefined") {
-    var signed_in = localStorage.getItem("signed_in");
-  }
-
-  useEffect(() => {
-    console.log(signed_in);
-    if (signed_in && signed_in === "true") {
-      toast.success("Sign in successfully", {
-        position: "top-center",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-      localStorage.removeItem("signed_in");
-    }
-  });
   return (
     <section className="bg-gradient-to-b from-gray-100 to-white">
-      <ToastContainer />
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
           {/* Page header */}
