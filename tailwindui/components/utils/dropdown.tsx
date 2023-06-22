27bff17d5a8cadb574497886aa1594d5fb41c730
Rex1012 <haorong.chen@qq.com>
2023-06-22 14:10:45 -0700

    clear localstorage when signed out

--- tailwindui/components/utils/dropdown.tsx
+++ tailwindui/components/utils/dropdown.tsx
@@ -43,6 +43,7 @@ const DropdownButton: React.FC<DropdownButtonProps> = () => {
     const signOut = async () => {
         try {
             await auth.signOut();
+            localStorage.clear();
             console.log('You have signed out!');
             router.push('/');
         } catch (error: any) {
