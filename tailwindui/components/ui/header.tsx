adca0f97b9c6af681999089d7270de49d3c00968
Qualia-Li <liquanlai1995@gmail.com>
2023-06-21 23:50:06 -0700

    use google auth to manage state

--- tailwindui/components/ui/header.tsx
+++ tailwindui/components/ui/header.tsx
@@ -7,85 +7,84 @@ import Logo from "./logo";
 import DropdownButton from "@/components/utils/dropdown";
 import MobileMenu from "./mobile-menu";
 import { usePathname } from "next/navigation";
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+import { auth } from "../Firebase";
 
 export default function Header() {
-  const [top, setTop] = useState<boolean>(true);
-  const [accessToken, setAccessToken] = useState("");
-  const [loading, setLoading] = useState(true);
-  const pathname = usePathname();
+    const [top, setTop] = useState<boolean>(true);
+    const [accessToken, setAccessToken] = useState("");
+    const [loading, setLoading] = useState(true);
+    const pathname = usePathname();
 
-  // detect whether user has scrolled the page down by 10px
-  const scrollHandler = () => {
-    window.pageYOffset > 10 ? setTop(false) : setTop(true);
-  };
+    const [user, setUser] = useState<User | null>(null);
 
-  useEffect(() => {
-    scrollHandler();
-    window.addEventListener("scroll", scrollHandler);
-    return () => window.removeEventListener("scroll", scrollHandler);
-  }, [top]);
 
-  useEffect(() => {
-    const token = localStorage.getItem("access_token");
-    console.log("token", token);
-    if (token) {
-      setAccessToken(token);
-    } else {
-      setAccessToken("");
-    }
-    setLoading(false);
-  }, [accessToken, pathname]);
+    // detect whether user has scrolled the page down by 10px
+    const scrollHandler = () => {
+        window.pageYOffset > 10 ? setTop(false) : setTop(true);
+    };
 
-  if (loading) {
-    return <p></p>;
-  }
+    useEffect(() => {
+        scrollHandler();
+        window.addEventListener("scroll", scrollHandler);
+        return () => window.removeEventListener("scroll", scrollHandler);
+    }, [top]);
 
-  return (
-    <header
-      className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${
-        !top ? "bg-white backdrop-blur-sm shadow-lg" : ""
-      }`}
-    >
-      <div className="max-w-6xl mx-auto px-5 sm:px-6">
-        <div className="flex items-center justify-between h-16 md:h-20">
-          {/* Site branding */}
-          <div className="shrink-0 mr-4">
-            <Logo />
-          </div>
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+        });
 
-          {/* Desktop navigation */}
-          <nav className="hidden md:flex md:grow">
-            {/* Desktop sign in links */}
-            {accessToken ? (
-              <ul className="flex grow justify-end flex-wrap items-center">
-                <DropdownButton accessToken={accessToken} setAccessToken={setAccessToken}/>
-              </ul>
-            ) : (
-              <ul className="flex grow justify-end flex-wrap items-center">
-                <li>
-                  <Link
-                    href="/signin"
-                    className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3"
-                  >
-                    Sign in
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/signup"
-                    className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3"
-                  >
-                    <span>Sign up</span>
-                  </Link>
-                </li>
-              </ul>
+        // Clean up subscription on unmount
+        return () => unsubscribe();
+    }, []);
 
-              )}
-          </nav>
 
-          <MobileMenu />
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header
+            className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top ? "bg-white backdrop-blur-sm shadow-lg" : ""
+                }`}
+        >
+            <div className="max-w-6xl mx-auto px-5 sm:px-6">
+                <div className="flex items-center justify-between h-16 md:h-20">
+                    {/* Site branding */}
+                    <div className="shrink-0 mr-4">
+                        <Logo />
+                    </div>
+
+                    {/* Desktop navigation */}
+                    <nav className="hidden md:flex md:grow">
+                        {/* Desktop sign in links */}
+                        {user ? (
+                            <ul className="flex grow justify-end flex-wrap items-center">
+                                <DropdownButton accessToken={accessToken} setAccessToken={setAccessToken} />
+                            </ul>
+                        ) : (
+                            <ul className="flex grow justify-end flex-wrap items-center">
+                                <li>
+                                    <Link
+                                        href="/signin"
+                                        className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3"
+                                    >
+                                        Sign in
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        href="/signup"
+                                        className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3"
+                                    >
+                                        <span>Sign up</span>
+                                    </Link>
+                                </li>
+                            </ul>
+
+                        )}
+                    </nav>
+
+                    <MobileMenu />
+                </div>
+            </div>
+        </header>
+    );
 }
--- tailwindui/components/utils/dropdown.tsx
+++ tailwindui/components/utils/dropdown.tsx
@@ -2,172 +2,122 @@
 
 import React, { useState, useEffect, useRef } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
+import { User } from 'firebase/auth';
+import { auth } from '../Firebase';
+import { toast } from 'react-toastify';
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+
 
 interface DropdownButtonProps {
-  accessToken: string;
-  setAccessToken: (token: string) => void;
 }
 
-const DropdownButton: React.FC<DropdownButtonProps> = ({ accessToken, setAccessToken }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [username, setUsername] = useState("");
-  const dropdownRef = useRef<HTMLDivElement>(null);
-  const router = useRouter();
-  const pathname = usePathname();
+const DropdownButton: React.FC<DropdownButtonProps> = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+    const router = useRouter();
+    const [user, setUser] = useState<User | null>(null);
 
-  useEffect(() => {
-    let timer: NodeJS.Timeout;
-    const delayedGetUsername = () => {
-      timer = setTimeout(() => {
-        getUsername()
-          .then((username) => {
-            if (username) {
-              setUsername(username);
-            }
-          })
-          .catch((error) => {
-            console.error('Error:', error);
-            // Handle the error as needed
-          });
-      }, 10); // Adjust the delay time as needed
-    };
-
-    // Call the delayedGetUsername function
-    delayedGetUsername();
-
-    // Clear the timer if the component unmounts or if accessToken changes
-    return () => clearTimeout(timer);
-  }, [accessToken]);
-  
-
-  useEffect(() => {
-    const handleOutsideClick = (event: MouseEvent) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-        setIsOpen(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleOutsideClick);
-
-    return () => {
-      document.removeEventListener('mousedown', handleOutsideClick);
-    };
-  }, []);
-
-
-  async function getUsername() {
-    const headers = new Headers();
-    if (accessToken) {
-      headers.append("Authorization", `Bearer ${accessToken}`);
-    }
-    headers.append("Content-Type", "application/json");
-
-    try {
-      const response = await fetch('/api/profile', {
-        method: "GET",
-        headers: headers,
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
         });
-      if (!response.ok) {
-        throw new Error('Failed to fetch profile');
-      }
-      
-      const data = await response.json();
-      console.log(data.username);
-      return data.username;
-    } catch (error) {
-      console.error(error);
-      // Handle the error as needed
-      return null;
-    }
-  }
 
-  const handleSignOut = async () => {
-    //send a request to the server to delete the access token
+        // Clean up subscription on unmount
+        return () => unsubscribe();
+    }, []);
 
-    const headers = new Headers();
-    if (accessToken) {
-      headers.append("Authorization", `Bearer ${accessToken}`);
-    }
-    headers.append("Content-Type", "application/json");
+    useEffect(() => {
+        const handleOutsideClick = (event: MouseEvent) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+                setIsOpen(false);
+            }
+        };
 
-    try {
-      const response = await fetch("/api/logout", {
-        method: "GET",
-        headers: headers,
-      });
-      console.log(response);
+        document.addEventListener('mousedown', handleOutsideClick);
 
-      // Remove the access token from local storage
-      localStorage.clear();
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, []);
 
-      console.log(router.push("/"))
-      setTimeout(() => {
-        setAccessToken("");
-      }, 100);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const signOut = async () => {
+        try {
+            await auth.signOut();
+            console.log('You have signed out!');
+            router.push('/');
+        } catch (error: any) {
+            console.error(error);
+            toast.error(error.message, {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+        }
+    };
 
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
 
 
-  return (
-    <div className="relative inline-block text-left" ref={dropdownRef}>
-      <div>
-        <button
-          type="button"
-          className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
-          id="dropdown-menu-button"
-          onClick={toggleDropdown}
-          aria-expanded={isOpen}
-          aria-haspopup="true"
-        >
-          Hi, {username}
-        </button>
-      </div>
-
-      {isOpen && (
-        <div
-          className="origin-top-right absolute right-0 mt-2 w-40 rounded-xl shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-300"
-          role="menu"
-          aria-orientation="vertical"
-          aria-labelledby="dropdown-menu-button"
-        >
-          <div className="py-1" role="none">
-            <a
-              href="/dashboard"
-              className="block px-4 py-1 text-sm text-blue-600 hover:bg-gray-200"
-              role="menuitem"
-            >
-              Projects
-            </a>
-            <a
-              href="#"
-              className="block px-4 py-1 text-sm text-blue-600 hover:bg-gray-200"
-              role="menuitem"
-            >
-              Account settings
-            </a>
-          </div>
-          <div className="py-1" role="none">
-            <div className="py-0.2" role="none">
-              <a
-                onClick={handleSignOut}
-                className="block px-4 py-1 text-sm text-blue-600 hover:bg-gray-200 cursor-pointer"
-                role="menuitem"
-              >
-                Sign out
-              </a>
+    return (
+        <div className="relative inline-block text-left" ref={dropdownRef}>
+            <div>
+                <button
+                    type="button"
+                    className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+                    id="dropdown-menu-button"
+                    onClick={toggleDropdown}
+                    aria-expanded={isOpen}
+                    aria-haspopup="true"
+                >
+                    Hi, {user && user.email}
+                </button>
             </div>
-          </div>
+
+            {isOpen && (
+                <div
+                    className="origin-top-right absolute right-0 mt-2 w-40 rounded-xl shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-300"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="dropdown-menu-button"
+                >
+                    <div className="py-1" role="none">
+                        <a
+                            href="/dashboard"
+                            className="block px-4 py-1 text-sm text-blue-600 hover:bg-gray-200"
+                            role="menuitem"
+                        >
+                            Projects
+                        </a>
+                        <a
+                            href="#"
+                            className="block px-4 py-1 text-sm text-blue-600 hover:bg-gray-200"
+                            role="menuitem"
+                        >
+                            Account settings
+                        </a>
+                    </div>
+                    <div className="py-1" role="none">
+                        <div className="py-0.2" role="none">
+                            <a
+                                onClick={signOut}
+                                className="block px-4 py-1 text-sm text-blue-600 hover:bg-gray-200 cursor-pointer"
+                                role="menuitem"
+                            >
+                                Sign out
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default DropdownButton;
