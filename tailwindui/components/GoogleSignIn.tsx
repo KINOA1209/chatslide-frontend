2fde37efaa4308ff7a9f80e975c55d4130425c77
Rex1012 <haorong.chen@qq.com>
2023-06-23 09:30:40 -0700

    link project to new user

--- tailwindui/components/GoogleSignIn.tsx
+++ tailwindui/components/GoogleSignIn.tsx
@@ -1,27 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { auth, googleProvider } from './Firebase';
-import { signInWithPopup, signOut } from 'firebase/auth';
-import { useRouter, useSearchParams } from "next/navigation";
+import { getAuth, onAuthStateChanged, signInWithPopup, User } from 'firebase/auth';
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const GoogleSignIn: React.FC = () => {
     const router = useRouter();
+    const pathname = usePathname();
     const searchParams = useSearchParams();
     const nextUri = searchParams.get("next");
-
-
+    const [accessToken, setAccessToken] = useState<string | null>(null);
+  
     const signInWithGoogle = async () => {
-        try {
-            await signInWithPopup(auth, googleProvider);
-            console.log('You are signed in!');
-            if (nextUri == null) {
-                router.push("/dashboard");
-              } else {
-                router.push(nextUri);
-              }
-        } catch (error) {
-            console.error(error);
-        }
+      try {
+        await signInWithPopup(auth, googleProvider);
+        console.log('You are signed in!');
+      } catch (error) {
+        console.error(error);
+      }
     };
+  
+    useEffect(() => {
+      const auth = getAuth();
+  
+      const unsubscribe = onAuthStateChanged(auth, (currentUser: User | null) => {
+        if (currentUser) {
+          currentUser.getIdToken()
+            .then((token) => {
+              setAccessToken(token);
+              console.log('accessToken: ', token); // Log the updated token here
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        } else {
+          setAccessToken(null);
+        }
+      });
+  
+      return () => {
+        unsubscribe();
+      };
+    }, []);
+  
+    useEffect(() => {
+      if (accessToken) { // Check if accessToken is not null or empty
+        const project_id = localStorage.getItem('project_id') || '';
+        const fetchProject = async () => {
+          try {
+            const response = await fetch('/api/link_project', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`
+              },
+              body: JSON.stringify({ 'project_id': project_id }),
+            });
+            console.log(response);
+            router.push(nextUri || "/dashboard"); // Use nextUri or fallback to "/dashboard"
+          } catch (error) {
+            console.error(error);
+          }
+        };
+        // if project_id is not null or empty, fetch project
+        if (project_id){
+          fetchProject();
+        }
+      }
+    }, [accessToken, nextUri, router]);
+
 
     return (
         <button onClick={signInWithGoogle} className="btn px-0 text-white bg-red-600 hover:bg-red-700 w-full relative flex items-center">
