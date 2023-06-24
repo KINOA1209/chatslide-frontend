56ba0b08c0b8bfc601f48218d7f40bcc2327e213
yjl450 <yil212@ucsd.edu>
2023-06-24 02:12:15 -0700

    initial commit of progress indicator

--- tailwindui/app/(feature)/workflow-generate-outlines/page.tsx
+++ tailwindui/app/(feature)/workflow-generate-outlines/page.tsx
@@ -1,3 +1,6 @@
+
+import ProgressBox from "@/components/steps";
+
 export const metadata = {
   title: 'Workflow - Dr. Lambda',
   description: 'Create new content',
@@ -10,7 +13,7 @@ export default function WorkflowStep1() {
     <section className="bg-gradient-to-b from-gray-100 to-white">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
+          <ProgressBox currentInd={0}/>
           {/* Page header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h1 className="h1">Step 1: Generate Outlines</h1>
