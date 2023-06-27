5cd30249526cc4a690dad41246d60246b32017b2
Qualia-Li <liquanlai1995@gmail.com>
2023-06-27 15:18:02 -0700

    add google analytics

---
+++ tailwindui/components/GoogleAnalytics.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+class GoogleAnalytics extends React.Component {
+    render() {
+        return (
+            <Helmet>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-FW9YH27P0H"></script>
+                <script>
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-FW9YH27P0H');
+                `}
+                </script>
+            </Helmet>
+        );
+    }
+}
+
+export default GoogleAnalytics;
