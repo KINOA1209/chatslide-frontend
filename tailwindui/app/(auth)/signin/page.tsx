49956cf461125cf5f51928973d76fd884cc65c98
Qualia-Li <liquanlai1995@gmail.com>
2023-06-28 17:30:59 -0700

    resolve conflict

--- tailwindui/app/(auth)/signin/page.tsx
+++ tailwindui/app/(auth)/signin/page.tsx
@@ -44,7 +44,7 @@ export default function SignIn() {
                 <div className="pt-32 pb-12 md:pt-40 md:pb-20">
                     {/* Page header */}
                     <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                        <h1 className="h1">💙 Welcome back.</h1>
+                        <h1 className="h1">💙 Welcome back!</h1>
                     </div>
 
                     {/* Form */}
