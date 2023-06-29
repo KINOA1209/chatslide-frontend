0a90a287fb22af6214846f573a53c87bcd066752
Rex1012 <haorong.chen@qq.com>
2023-06-29 14:04:47 -0700

    update load project

--- tailwindui/app/(dashboard)/project/[id]/page.tsx
+++ tailwindui/app/(dashboard)/project/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState, useEffect } from 'react';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import Link from 'next/link';
 import AuthService from '@/components/utils/AuthService';
 
@@ -16,11 +16,15 @@ interface Project {
     page_count: string;
     outline: string;
     transcripts: string;
+    image_files: string;
+    audio_files: string;
+    video_file: string;
 }
 
 const ProjectDetail = () => {
     const [project, setProject] = useState<Project | null>(null);
     const pathname = usePathname();
+    const router = useRouter();
 
     useEffect(() => {
         // Create a scoped async function within the hook.
@@ -40,17 +44,40 @@ const ProjectDetail = () => {
 
     useEffect(() => {
         if (project) {
-            // Store values in sessionStorage
-            sessionStorage.setItem('topic', project.topic);
-            sessionStorage.setItem('requirements', project.requirements);
-            sessionStorage.setItem('audience', project.audience);
-            sessionStorage.setItem('language', project.language);
-            sessionStorage.setItem('foldername', project.foldername);
-            sessionStorage.setItem('page_count', project.page_count);
-            sessionStorage.setItem('outline', JSON.stringify(project.outline));
-            sessionStorage.setItem('transcripts', JSON.stringify(project.transcripts));
-
-            console.log('page_count', project.page_count);
+            // Store values in sessionStorage if they exist
+            if (project.topic) {
+                sessionStorage.setItem('topic', project.topic);
+            }
+            if (project.requirements) {
+                sessionStorage.setItem('requirements', project.requirements);
+            }
+            if (project.audience) {
+                sessionStorage.setItem('audience', project.audience);
+            }
+            if (project.language) {
+                sessionStorage.setItem('language', project.language);
+            }
+            if (project.foldername) {
+                sessionStorage.setItem('foldername', project.foldername);
+            }
+            if (project.page_count) {
+                sessionStorage.setItem('page_count', project.page_count);
+            }
+            if (project.outline) {
+                sessionStorage.setItem('outline', JSON.stringify(project.outline));
+            }
+            if (project.transcripts) {
+                sessionStorage.setItem('transcripts', JSON.stringify(project.transcripts));
+            }
+            if (project.image_files) {
+                sessionStorage.setItem('image_files', JSON.stringify(project.image_files));
+            }
+            if (project.audio_files) {
+                sessionStorage.setItem('audio_files', JSON.stringify(project.audio_files));
+            }
+            if (project.video_file) {
+                sessionStorage.setItem('video_file', project.video_file);
+            }
         }
     }, [project]);
 
@@ -62,12 +89,14 @@ const ProjectDetail = () => {
         headers.append('Content-Type', 'application/json');
 
         try {
+            // set project_id in sessionStorage
             const project_id = pathname.split('/').pop();
             if (project_id) {
                 console.log('this is project_id', project_id);
                 sessionStorage.setItem('project_id', project_id);
             }
-
+            
+            // fetch project details
             const response = await fetch('/api/get_project_details', {
                 method: 'POST',
                 headers: headers,
@@ -86,7 +115,49 @@ const ProjectDetail = () => {
         }
     };
 
-    // ... existing code ...
+
+    const redirect = ['/workflow-generate-outlines',
+        '/workflow-edit-outlines',
+        '/workflow-review-slides',
+        '/workflow-edit-transcript',
+        'workflow-review-audio',
+        'workflow-review-video'];
+    const projectFinishedSteps: () => number[] = () => {
+        const finishedStepsArray: number[] = [];
+        if (typeof window !== 'undefined' && sessionStorage.getItem('topic')) {
+            finishedStepsArray.push(0);
+        }
+        if (typeof window !== 'undefined' && sessionStorage.getItem('outline')) {
+            finishedStepsArray.push(1);
+        }
+        if (typeof window !== 'undefined' && sessionStorage.getItem('image_files')) {
+            finishedStepsArray.push(2);
+        }
+        if (typeof window !== 'undefined' && sessionStorage.getItem('transcripts')) {
+            finishedStepsArray.push(3);
+        }
+        if (typeof window !== 'undefined' && sessionStorage.getItem('audio_files')) {
+            finishedStepsArray.push(4);
+        }
+        if (typeof window !== 'undefined' && sessionStorage.getItem('video_file')) {
+            finishedStepsArray.push(5);
+        }
+        return finishedStepsArray;
+    }
+
+    const handleClick = async () => {
+        const finishedSteps = projectFinishedSteps();
+      
+        if (finishedSteps.length > 0) {
+          const lastFinishedStep = finishedSteps[finishedSteps.length - 1];
+          const redirectURL = redirect[lastFinishedStep];
+          router.push(redirectURL);
+        } else {
+            router.push('/workflow-intro');
+        }
+      };
+
+
 
     return (
         <section className="bg-gradient-to-b from-gray-100 to-white">
@@ -120,18 +191,10 @@ const ProjectDetail = () => {
 
                         {/* Button Group */}
                         <div className="flex justify-center mt-4">
-                            <Link className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded mr-2 btn-size"
-                                href="/workflow-generate-outlines">
-                                Edit Prompt
-                            </Link>
-                            <Link className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded mr-2 btn-size"
-                                href='workflow-edit-outlines'>
-                                Edit Outlines
-                            </Link>
-                            <Link className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded btn-size"
-                                href='workflow-edit-transcript'>
-                                Edit Transcripts
-                            </Link>
+                            <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded mr-2 btn-size"
+                            onClick={handleClick}>
+                                Continue Project
+                            </button>
                         </div>
                     </div>
                 </div>
