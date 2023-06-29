a1ce52cb9465740a86299121c94f8c461383c03c
Rex1012 <haorong.chen97@gmail.com>
2023-06-29 14:04:47 -0700

    update load project

--- tailwindui/components/Slides.tsx
+++ tailwindui/components/Slides.tsx
@@ -7,9 +7,7 @@ type AppProps = {
 
 const App: React.FC<AppProps> = ({ height = 80 }) => {
   const foldername = typeof sessionStorage !== 'undefined' ? sessionStorage.getItem('foldername') : '';
-  const page_count = typeof sessionStorage !== 'undefined' ? parseInt(sessionStorage.getItem('page_count') || '0', 10) : 0;
-
-  const image_files = Array.from({ length: page_count }, (_, index) => `page${index + 1}.jpg`);
+  const image_files = typeof sessionStorage !== 'undefined' ? JSON.parse(sessionStorage.getItem('image_files') || '[]') : [];
 
   const imageUrls = image_files.map((filename: string) => `/api/jpg?foldername=${foldername}&filename=${filename}`);
   console.log(image_files)
