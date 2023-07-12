95c719e2a4dcba2c76dc915b48c0edd0985df40d
yjl450 <yil212@ucsd.edu>
2023-07-11 17:18:30 -0700

    add project deletion

--- tailwindui/app/(dashboard)/dashboard/page.tsx
+++ tailwindui/app/(dashboard)/dashboard/page.tsx
@@ -96,6 +96,60 @@ export default function Dashboard() {
         window.open(`/project/${projectId}`, '_blank');
     };
 
+    const handleDelete = async (e: React.MouseEvent<HTMLDivElement>, projectId: number) => {
+        e.stopPropagation();
+        // Modal for warning
+        alert(`Project ${projectId} will be deleted`);
+        // Communicate with server if user confirm deletion
+        if (true) {
+            const projectDeleteData = {
+                project_id: projectId
+            }
+            try {
+                const { userId, idToken: token } = await AuthService.getCurrentUserTokenAndId();
+                const response = await fetch("/api/delete_project", {
+                    method: "DELETE",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                    body: JSON.stringify(projectDeleteData),
+                });
+                if (response.ok) {
+                    const projectDeleteFeedback = await response.json();
+                    if (response.status === 200) {
+                        setProjects(projects.filter(proj => proj.id !== projectId));
+                        toast.success("Project deleted successfully", {
+                            position: "top-center",
+                            autoClose: 2000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "light",
+                        });
+                    } else {
+                        // error handling does not work
+                        toast.error(projectDeleteFeedback.message, {
+                            position: "top-center",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "light",
+                        });
+                    }
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+    };
+
     return (
         <section className="bg-gradient-to-b from-gray-100 to-white">
             <ToastContainer />
@@ -108,11 +162,19 @@ export default function Dashboard() {
                         {currentProjects.map((project) => (
                             <div
                                 key={project.id}
-                                className="border-solid border-2 border-blue-600 p-4 rounded-md shadow-md cursor-pointer"
+                                className="flex border-solid border-2 border-blue-600 p-4 rounded-md shadow-md cursor-pointer"
                                 onClick={() => handleProjectClick(project.id)}
                             >
-                                <h2 className="text-lg font-semibold">{project.name}</h2>
-                                <p className="text-sm mt-2">{project.description}</p>
+                                <div className='grow'>
+                                    <h2 className="text-lg font-semibold">{project.name}</h2>
+                                    <p className="text-sm mt-2">{project.description}</p>
+                                </div>
+                                <div className='text-lg opacity-25 hover:opacity-100' onClick={e => handleDelete(e, project.id)}>
+                                    <svg className='w-12' data-name="Capa 1" id="Capa_1" viewBox="0 0 20 19.84" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M10.17,10l3.89-3.89a.37.37,0,1,0-.53-.53L9.64,9.43,5.75,5.54a.37.37,0,1,0-.53.53L9.11,10,5.22,13.85a.37.37,0,0,0,0,.53.34.34,0,0,0,.26.11.36.36,0,0,0,.27-.11l3.89-3.89,3.89,3.89a.34.34,0,0,0,.26.11.35.35,0,0,0,.27-.11.37.37,0,0,0,0-.53Z" />
+                                    </svg>
+                                </div>
                             </div>
                         ))}
                     </div>
@@ -128,7 +190,7 @@ export default function Dashboard() {
                                 </button>
                             ) : (
                                 <button
-                                    className={`bg-blue-600 text-white px-4 py-2 rounded-md shadow-md`}
+                                    className={`bg-blue-600 text-white px-4 py-2 rounded-md shadow-md mr-2`}
                                     onClick={goToPreviousPage}
                                     style={{ minWidth: '120px' }}
                                 >
