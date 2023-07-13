ab6bc2b361eb8fc357c8e98ae96e49ee0ca85241
Qualia-Li <liquanlai1995@gmail.com>
2023-07-12 20:45:09 -0700

    generating voice

--- tailwindui/app/(feature)/workflow-edit-transcript/page.tsx
+++ tailwindui/app/(feature)/workflow-edit-transcript/page.tsx
@@ -92,7 +92,7 @@ const TranscriptVisualizer = ({ transcripts, imageUrls }: { transcripts: [], ima
                     <div className="flex flex-wrap -mx-3 mt-6">
                         <div className="w-full px-3">
                             <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">
-                                Generate Voice
+                                {isSubmitting ? 'Generating...' : 'Generate Voice'}
                             </button>
                         </div>
                     </div>
