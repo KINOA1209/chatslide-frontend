37481f383be015d559d233c90e9c3b2d9be7a7f7
Rex1012 <haorong.chen97@gmail.com>
2023-07-14 15:14:44 -0700

    remove keep me signed in checkbox

--- tailwindui/components/login-form.tsx
+++ tailwindui/components/login-form.tsx
@@ -111,16 +111,6 @@ const LoginForm: React.FC = () => {
                     />
                 </div>
             </div>
-            <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                    <div className="flex justify-between">
-                        <label className="flex items-center" htmlFor="remember">
-                            <input id="remember" type="checkbox" className="form-checkbox" />
-                            <span className="text-gray-600 ml-2">Keep me signed in</span>
-                        </label>
-                    </div>
-                </div>
-            </div>
             <div className="flex flex-wrap -mx-3 mt-6">
                 <div className="w-full px-3">
                     <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">
