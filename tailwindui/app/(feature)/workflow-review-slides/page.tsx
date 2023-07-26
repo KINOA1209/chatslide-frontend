139af34f61cf7333a97eb5de45c181560a44bd35
Rex1012 <haorong.chen97@gmail.com>
2023-07-25 18:57:11 -0700

    feedback system

--- tailwindui/app/(feature)/workflow-review-slides/page.tsx
+++ tailwindui/app/(feature)/workflow-review-slides/page.tsx
@@ -9,11 +9,12 @@ import GoBackButton from '@/components/GoBackButton';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ProjectProgress from "@/components/steps";
+import FeedbackForm from '@/components/feedback';
 
 const SlideVisualizer = ({ slide_files }: { slide_files: any }) => {
     console.log(slide_files);
     const [isSubmitting, setIsSubmitting] = useState(false);
-
+    
     if (typeof window !== "undefined") {
         var signed_in = sessionStorage.getItem("signed_in");
     }
@@ -62,6 +63,39 @@ const SlideVisualizer = ({ slide_files }: { slide_files: any }) => {
 export default function WorkflowStep3() {
     const slide_files = typeof sessionStorage !== 'undefined' ? sessionStorage.getItem('slide_files') : [];
     const contentRef = useRef<HTMLDivElement>(null);
+    const [showModal, setShowModal] = useState<boolean>(false);
+
+    // State variable to track whether the timer has finished
+    const [timerFinished, setTimerFinished] = useState(false);
+
+    // Timer completion handler
+    const handleTimerCompletion = () => {
+        setTimerFinished(true);
+    };
+
+    // Start the timer when the component mounts
+    useEffect(() => {
+        const timer = setTimeout(handleTimerCompletion, 10000); // 10 seconds
+
+        // Clean up the timer when the component unmounts
+        return () => clearTimeout(timer);
+    }, []);
+
+    const handleOpenModal = () => {
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    // Check if the timer has finished before opening the modal
+    useEffect(() => {
+        if (timerFinished) {
+            handleOpenModal();
+        }
+    }, [timerFinished]);
+
     return (
         <div>
             <ProjectProgress currentInd={2} contentRef={contentRef} />
@@ -75,6 +109,16 @@ export default function WorkflowStep3() {
                 </p>
                 <br />
                 <SlideVisualizer slide_files={slide_files} />
+            </div>
+            <div className="fixed bottom-10 right-10">
+                <button
+                onClick={handleOpenModal}
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue active:bg-blue-700"
+                >
+                Feedback
+                </button>
+
+                {showModal && <FeedbackForm onClose={handleCloseModal} />}
             </div>
         </div>
     );
