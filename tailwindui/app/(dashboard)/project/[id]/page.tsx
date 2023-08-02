92270e74ed91b4ab5091d72ec1ac26e5ca4940f8
Rex1012 <haorong.chen@qq.com>
2023-08-02 15:13:01 -0700

    remove intermidiate step when loading project

--- tailwindui/app/(dashboard)/project/[id]/page.tsx
+++ tailwindui/app/(dashboard)/project/[id]/page.tsx
@@ -1,12 +1,10 @@
 'use client'
 
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import { usePathname, useRouter } from 'next/navigation';
-import Link from 'next/link';
 import AuthService from '@/components/utils/AuthService';
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Dialog, Transition } from '@headlessui/react';
+
 
 interface Project {
     project_name: string;
@@ -25,20 +23,11 @@ interface Project {
     video_file: string;
 }
 
-const ProjectDetail = () => {
+const ProjectLoading = () => {
     const [project, setProject] = useState<Project | null>(null);
     const pathname = usePathname();
     const router = useRouter();
-    const [deleteInd, setDeleteInd] = useState(-1);
-
-    const [isOpen, setIsOpen] = useState(false);
-    function closeModal() {
-        setIsOpen(false)
-    };
-
-    function openModal() {
-        setIsOpen(true)
-    };
+    
     useEffect(() => {
         // Create a scoped async function within the hook.
         const fetchUser = async () => {
@@ -94,6 +83,7 @@ const ProjectDetail = () => {
             if (project.video_file) {
                 sessionStorage.setItem('video_file', project.video_file);
             }
+            handleRedirect();
         }
     }, [project]);
 
@@ -161,7 +151,7 @@ const ProjectDetail = () => {
         return finishedStepsArray;
     }
 
-    const handleClick = async () => {
+    const handleRedirect = async () => {
         const finishedSteps = projectFinishedSteps();
 
         if (finishedSteps.length > 0) {
@@ -173,175 +163,19 @@ const ProjectDetail = () => {
         }
     };
 
-    const handleDelete = async () => {
-        const projectIdString = sessionStorage.getItem('project_id');
-        if (projectIdString !== null) {
-            // Modal for warning
-            setDeleteInd(parseInt(projectIdString));
-            setIsOpen(true);
-        }
-    };
-
-    const confirmDelete = async () => {
-        setIsOpen(false);
-        if (deleteInd == -1) {
-            throw "Error";
-        }
-        const projectDeleteData = {
-            project_id: deleteInd
-        }
-        try {
-            const { userId, idToken: token } = await AuthService.getCurrentUserTokenAndId();
-            const response = await fetch("/api/delete_project", {
-                method: "DELETE",
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                },
-                body: JSON.stringify(projectDeleteData),
-            });
-            if (response.ok) {
-                const projectDeleteFeedback = await response.json();
-                if (response.status === 200) {
-                    router.push('/dashboard');
-                    toast.success("Project deleted successfully", {
-                        position: "top-center",
-                        autoClose: 2000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "light",
-                    });
-                } else {
-                    // error handling does not work
-                    toast.error(projectDeleteFeedback.message, {
-                        position: "top-center",
-                        autoClose: 5000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "light",
-                    });
-                }
-            }
-        } catch (error) {
-            console.error(error);
-        }
-        setDeleteInd(-1);
-    }
 
     return (
         <section className="bg-gradient-to-b from-gray-100 to-white">
-            <div className="max-w-6xl mx-auto px-4 sm:px-6">
-                <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-                    <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                        <div className="container mx-auto px-4 py-8 bg-white rounded-lg shadow-md">
-                            {project ? (
-                                <>
-                                    <h1 className="text-2xl font-bold mb-4">{project.project_name}</h1>
-                                    <p className="text-gray-600 mb-2">{project.project_description}</p>
-                                    <div className="border-t border-gray-200 mt-4 pt-4">
-                                        <p className="text-gray-600 mb-2">
-                                            <strong>Topic:</strong> {project.topic}
-                                        </p>
-                                        <p className="text-gray-600 mb-2">
-                                            <strong>Prior Knowledge:</strong> {project.requirements}
-                                        </p>
-                                        <p className="text-gray-600 mb-2">
-                                            <strong>Audience:</strong> {project.audience}
-                                        </p>
-                                        <p className="text-gray-600 mb-2">
-                                            <strong>Language:</strong> {project.language}
-                                        </p>
-                                    </div>
-                                </>
-                            ) : (
-                                <p>Loading project details...</p>
-                            )}
-                        </div>
-
-                        {/* Button Group */}
-                        <div className="flex justify-center mt-4">
-                            <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded mr-2 btn-size"
-                                onClick={handleClick}>
-                                Continue Project
-                            </button>
-                        </div>
-
-                        <div className="flex justify-center mt-4">
-                            <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded mr-2 btn-size"
-                                onClick={handleDelete}>
-                                Delete Project
-                            </button>
-                        </div>
-                    </div>
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+            <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+                <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+                    <p>Loading project...</p>
                 </div>
             </div>
-            <Transition appear show={isOpen} as={Fragment}>
-                <Dialog as="div" className="relative z-10" onClose={closeModal}>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <div className="fixed inset-0 bg-black bg-opacity-25" />
-                    </Transition.Child>
-
-                    <div className="fixed inset-0 overflow-y-auto">
-                        <div className="flex min-h-full items-center justify-center p-4 text-center">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 scale-95"
-                                enterTo="opacity-100 scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 scale-100"
-                                leaveTo="opacity-0 scale-95"
-                            >
-                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                                    <Dialog.Title
-                                        as="h3"
-                                        className="text-lg font-medium leading-6 text-gray-900"
-                                    >
-                                        Delete Project?
-                                    </Dialog.Title>
-                                    <div className="mt-2">
-                                        <p className="text-sm text-gray-500">
-                                            Deleted Project cannot be restored.
-                                        </p>
-                                    </div>
-
-                                    <div className="flex">
-                                        <div className="flex justify-center mt-4">
-                                            <button className="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded mr-2 btn-size"
-                                                onClick={confirmDelete}>
-                                                Yes
-                                            </button>
-                                        </div>
-                                        <div className="flex justify-center mt-4">
-                                            <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded mr-2 btn-size"
-                                                onClick={closeModal}>
-                                                Cancel
-                                            </button>
-                                        </div>
-                                    </div>
-                                </Dialog.Panel>
-                            </Transition.Child>
-                        </div>
-                    </div>
-                </Dialog>
-            </Transition>
-        </section>
+        </div>
+    </section>
     );
 
 };
 
-export default ProjectDetail;
+export default ProjectLoading;
