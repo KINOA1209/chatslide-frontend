3d215a596b0424dec799a9d4575704845ffd707b
rachelli <ximeralii0@gmail.com>
2023-08-04 11:15:58 +0800

    customize slides, add historical inputs in Topic

--- tailwindui/components/utils/UserService.tsx
+++ tailwindui/components/utils/UserService.tsx
@@ -20,7 +20,30 @@ class DatabaseService {
         throw error;
       }
     }
+    static async getUserHistoricalInput(idToken:string) {
+      try {
+        const response = await fetch(`/api/user/<string:uid>/historical_input`, {
+          method: 'GET',
+          headers: {
+              'Authorization': `Bearer ${idToken}`,
+          },
+      });
+        if (response.status === 200){
+          const data = await response.json();
+          if (data.message){
+            return null;
+          }else{
+            return data
+          }
+        }else{
+          throw new Error(`Error status: ${response.status}`);
+        }
+      } catch (error) {
+        console.error('Failed to fetch user historical inputs:', error);
+        throw error;
+      }
+    }
   }
-  
+
   export default DatabaseService;
   
