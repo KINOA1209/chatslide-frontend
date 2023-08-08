3e3f13d3bc90757231ebb7148c45dd0fda19645c
rachelli <ximeralii0@gmail.com>
2023-08-08 19:49:04 +0800

    move to slides page

--- tailwindui/app/(feature)/workflow-edit-outlines/page.tsx
+++ tailwindui/app/(feature)/workflow-edit-outlines/page.tsx
@@ -3,7 +3,6 @@
 import React, { useState, useRef } from 'react';
 import { useRouter } from 'next/navigation';
 import { FormEvent } from 'react';
-import { ChangeEvent } from 'react';
 import Timer from '@/components/Timer';
 import ProjectProgress from "@/components/steps";
 import AuthService from '@/components/utils/AuthService';
@@ -49,61 +48,12 @@ const OutlineVisualizer = ({ outline }: { outline: OutlineDataType }) => {
     };
 
     const [isSubmitting, setIsSubmitting] = useState(false); const [timer, setTimer] = useState(0);
-    const [addBackgroundImage, setBackgroundImage] = useState((typeof window !== 'undefined' && sessionStorage.addBackgroundImage !== undefined) ? (sessionStorage.addBackgroundImage === 'true') : false);
-    const [file, setFile] = useState<File | null>(null);
-    const [fileName, setFileName] = useState<string>('No file chosen');
-    const [selectedOption, setSelectedOption] = useState<string>('None');
-
-    const convertImageToBase64 = (file: File): Promise<string> => {
-        return new Promise((resolve, reject) => {
-          const reader = new FileReader();
-          reader.onloadend = () => {
-            if (typeof reader.result === 'string') {
-              resolve(reader.result);
-            } else {
-              reject(new Error('Failed to convert image to Base64.'));
-            }
-          };
-          reader.readAsDataURL(file);
-        });
-    };
-    const convertImagePathToBase64 = (imagePath: string): Promise<string> => {
-        return new Promise((resolve, reject) => {
-            fetch(imagePath)
-            .then((response) => response.blob())
-            .then((blob) => {
-              const file = new File([blob], 'imageFile'); 
-              const reader = new FileReader();
-              reader.onloadend = () => {
-                if (typeof reader.result === 'string') {
-                  resolve(reader.result);
-                } else {
-                  reject(new Error('Failed to convert image path to Base64.'));
-                }
-              };
-              reader.readAsDataURL(file);
-            })
-        });
-    };
-    const handleOptionChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setSelectedOption(e.target.value);
-    };
-    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files) {
-          setFile(e.target.files[0]);
-          setFileName(e.target.files[0].name);
-        }    
-    };
-    const handleBackgroundChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setBackgroundImage(event.target.checked);
-    };
 
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         console.log("submitting");
         event.preventDefault();
         setIsSubmitting(true);
         setTimer(0);
-        let formData;
 
         // remove empty entries
         console.log(outlineData);
@@ -122,69 +72,18 @@ const OutlineVisualizer = ({ outline }: { outline: OutlineDataType }) => {
 
         const addEquations = typeof window !== 'undefined' ? sessionStorage.getItem('addEquations') : null;
 
-        if(addBackgroundImage){
-            if(selectedOption === 'customize'){
-                if(file){
-                    const imageBase64 = await convertImageToBase64(file);
-                    formData = {
-                        res: JSON.stringify({ ...outlineData }),
-                        audience: audience,
-                        foldername: foldername,
-                        topic: topic,
-                        language: language,
-                        additional_requirements: 'test',
-                        project_id: project_id,
-                        addBackgroundImage: addBackgroundImage,
-                        file:imageBase64,
-                        addEquations: addEquations
-                    };
-                }
-            }else if(selectedOption === 'default1'){
-                const imagePath = "../../../../slidesBackground/theme1.jpg";
-                const imageBase64 = await convertImagePathToBase64(imagePath);
-                formData = {
-                    res: JSON.stringify({ ...outlineData }),
-                    audience: audience,
-                    foldername: foldername,
-                    topic: topic,
-                    language: language,
-                    additional_requirements: 'test',
-                    project_id: project_id,
-                    addBackgroundImage: addBackgroundImage,
-                    file:imageBase64,
-                    addEquations: addEquations
-                };
-            }else if(selectedOption === 'default2'){
-                const imagePath = "../../../../slidesBackground/theme2.jpg";
-                const imageBase64 = await convertImagePathToBase64(imagePath);
-                formData = {
-                    res: JSON.stringify({ ...outlineData }),
-                    audience: audience,
-                    foldername: foldername,
-                    topic: topic,
-                    language: language,
-                    additional_requirements: 'test',
-                    project_id: project_id,
-                    addBackgroundImage: addBackgroundImage,
-                    file:imageBase64,
-                    addEquations: addEquations
-                };
-            }
-        }else{
-            formData = {
-                res: JSON.stringify({ ...outlineData }),
-                audience: audience,
-                foldername: foldername,
-                topic: topic,
-                language: language,
-                additional_requirements: 'test',
-                project_id: project_id,
-                addBackgroundImage: addBackgroundImage,
-                addEquations: addEquations
-            };
-        }
-
-
+        const formData = {
+            res: JSON.stringify({ ...outlineData }),
+            audience: audience,
+            foldername: foldername,
+            topic: topic,
+            language: language,
+            additional_requirements: 'test',
+            project_id: project_id,
+            addBackgroundImage: false,
+            addEquations: addEquations
+        };
+        
         console.log(formData);
 
         try {
@@ -376,53 +275,7 @@ const OutlineVisualizer = ({ outline }: { outline: OutlineDataType }) => {
                     </div>
                 </div>
             ))}
-            <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3 mt-2 flex">
-                    <input
-                        type="checkbox"
-                        id="addBackgroundImage"
-                        className="form-checkbox text-gray-800"
-                        checked={addBackgroundImage}
-                        onChange={handleBackgroundChange}/>
-                    <label
-                        className="block text-gray-800 text-sm font-medium mb-1"
-                        htmlFor="addBackgroundImage">
-                        &nbsp; &nbsp; Add background images to my slides
-                    </label>
-                </div>
-                {addBackgroundImage && (
-                    <form onSubmit={handleSubmit} encType="multipart/form-data" className="text-gray-800 text-sm font-medium mb-1" style={{display: 'flex', alignItems: 'center'}}>
-                        <div className="px-10 mt-1">
-                            <label><input type="radio" value="default1" checked={selectedOption === 'default1'} onChange={handleOptionChange} />
-                                <span className='mx-2'>Theme 1</span>
-                            </label>
-                            {selectedOption === "default1" && (
-                                <img src="../../../../slidesBackground/theme1.jpg" style={{width: "200px", height: "150px"}}/>
-                            )}
-                            <br/>
-                            <label><input type="radio" value="default2" checked={selectedOption === 'default2'} onChange={handleOptionChange} />
-                                <span className='mx-2'>Theme 2</span>
-                            </label>
-                            {selectedOption === "default2" && (
-                                <img src="../../../../slidesBackground/theme2.jpg" style={{width: "200px", height: "150px"}}/>
-                            )}
-                            <br/>
-                            <label><input type="radio" value="customize" checked={selectedOption === 'customize'} onChange={handleOptionChange} />
-                                <span className='mx-2'>Choose your own</span>
-                            </label>
-                            {selectedOption === 'customize' && (
-                                <div>
-                                    <label htmlFor="imageUpload" className="btn text-white bg-blue-600 hover:bg-blue-700 mx-6">
-                                        Choose File
-                                    </label>
-                                    <span>{fileName}</span>
-                                    <input type="file" id="imageUpload" accept="image/*"  onChange={handleFileChange}  style={{display: 'none'}}/>
-                                </div>
-                            )}
-                        </div>  
-                    </form>  
-                )}
-            </div>
+            
             {/* Form */}
             <div className="max-w-sm mx-auto">
                 <form onSubmit={handleSubmit}>
