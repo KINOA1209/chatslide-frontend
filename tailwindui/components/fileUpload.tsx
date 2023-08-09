76b3fd63fc8f1a3b36600ccd6bae8ec6422096d0
yjl450 <yil212@ucsd.edu>
2023-08-08 22:39:10 -0700

    add file format check

--- tailwindui/components/fileUpload.tsx
+++ tailwindui/components/fileUpload.tsx
@@ -1,15 +1,37 @@
 import React, { ChangeEvent, FC, useState, useRef } from 'react';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const supportedFormats = ['PDF', 'JPG']; // For prompt displayy
+const supportedExtensions = ['pdf', 'jpg', 'jpeg']; // For checking logic
 
 interface FileUploadButtonProps {
     onFileSelected: (file: File | null) => void;
+    formats?: string[],
+    extensions?: string[],
 }
 
-export const FileUploadButton: FC<FileUploadButtonProps> = ({ onFileSelected }) => {
+export const FileUploadButton: FC<FileUploadButtonProps> = ({ onFileSelected, formats = supportedFormats, extensions = supportedExtensions }) => {
     const [fileName, setFileName] = useState<string | null>(null);
     const inputFileRef = useRef<HTMLInputElement>(null);
 
     const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files ? e.target.files[0] : null;
+
+        const ext = file?.name.split('.').pop()?.toLowerCase();
+        if (ext && !extensions.includes(ext)) {
+            toast.error(ext.toUpperCase() + ' file is not supported!', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            return;
+        }
 
         if (file) {
             setFileName(file.name);
@@ -27,6 +49,7 @@ export const FileUploadButton: FC<FileUploadButtonProps> = ({ onFileSelected }) 
 
     return (
         <div>
+            <ToastContainer />
             <input
                 type="file"
                 id="file-upload"
@@ -39,8 +62,15 @@ export const FileUploadButton: FC<FileUploadButtonProps> = ({ onFileSelected }) 
                 type="button"
                 onClick={handleClick}
             >
-                {fileName || 'Upload Supporting PDF'}
+                {fileName || 'Upload Files'}
             </button>
+            <div className='text-sm text-gray-400'>Supported file formats: {formats.map((f, index) => {
+                if (index !== formats.length - 1) {
+                    return f + ', ';
+                } else {
+                    return f;
+                }
+            })}</div>
         </div>
     );
 };
