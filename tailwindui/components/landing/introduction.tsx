ed98acd7c6064c100265ff0ade5884d07987e614
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-08-09 15:35:34 -0700

    update landing page

--- tailwindui/components/landing/introduction.tsx
+++ tailwindui/components/landing/introduction.tsx
@@ -37,7 +37,7 @@ const InternalIntroduction = () => {
       {/* Illustration behind hero content */}
       <div className="h-full absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none -z-1" aria-hidden="true">
         <Image
-          className="h-full max-w-none"
+          className="h-full max-w-none opacity-70"
           src={background}
           height={(typeof window !== 'undefined' && window.innerHeight < 768) ? window.innerHeight : 768}
           style={{ objectFit: "contain" }}
@@ -54,15 +54,12 @@ const InternalIntroduction = () => {
           <div className="text-center pb-12 md:pb-16">
             <div className="max-w-3xl mx-auto">
               <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Dr. <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Lambda</span></h1>
-              <p className="text-2xl md:text-3xl text-blue-400 mb-8 font-bold" data-aos="zoom-y-out" data-aos-delay="150">Remix your knowledge</p>
+              <p className="text-3xl md:text-4xl text-black-400 mb-8 font-bold" data-aos="zoom-y-out" data-aos-delay="150">Remix your knowledge</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0 cursor-pointer" onClick={handleOnClick}>
                     {currentUser ? 'My dashboard' : 'Start free trial'}
                   </a>
-                </div>
-                <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#more">Learn more</a>
                 </div>
               </div>
             </div>
