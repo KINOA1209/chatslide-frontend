9a124dbe67fcb785a5b405d083fbb08be179b24e
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-08-09 22:00:06 -0700

    update hero text

---
+++ tailwindui/components/landing/HeroText.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+
+interface Props {
+    slides: string[];
+    interval?: number; // Time interval for automatic slide change in milliseconds
+    color?: string;
+}
+
+const TextCarousel: React.FC<Props> = ({ slides, interval = 3000, color = "text-blue-500" }) => {
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+    const nextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex >= slides.length - 1 ? 0 : prevIndex + 1));
+    };
+
+    useEffect(() => {
+        const slideTimer = setTimeout(nextSlide, interval);
+
+        // Cleanup the timer when the component is unmounted or before re-running the effect
+        return () => {
+            clearTimeout(slideTimer);
+        };
+    }, [currentIndex]);
+
+    return (
+        <div className="carousel-container relative overflow-hidden h-[4rem] max-w-xl mx-auto">
+            <ul className="carousel-list flex flex-col transition-transform duration-500" style={{ transform: `translateY(-${currentIndex * 4}rem)` }}>
+                {slides.map((slide, index) => (
+                    <li key={index} className="carousel-item h-[4rem] text-center py-1 flex-none">
+                         <h1 className={`text-5xl font-extrabold leading-tighter tracking-tighter text-left ${color}`}>{slide}</h1>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+const HeroText = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        setIsVisible(true);
+    }, []);
+
+    return (
+        <div className={`flex flex-col items-center transition-opacity duration-5000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+            <div className="flex items-end space-x-4">
+                <h1 className="text-4xl font-extrabold leading-tighter tracking-tighter mb-1">Create</h1>
+                <TextCarousel slides={["Slides", "Script", "Audio", "Video"]} interval={3000} color="text-blue-500"/>
+            </div>
+            <div className="flex items-end space-x-4">
+                <h1 className="text-4xl font-extrabold leading-tighter tracking-tighter mb-1">with</h1>
+                <TextCarousel slides={["PDF", "Youtube", "Prompt"]} interval={3000} color="text-teal-400"/>
+            </div>
+            <div className="flex items-end space-x-4">
+                <h1 className="text-4xl font-extrabold leading-tighter tracking-tighter mb-1">without</h1>
+                <TextCarousel slides={["Hardwork", "Confusion"]} interval={5000} color="text-black-400"/>
+            </div>
+        </div>
+    )
+}
+
+export default HeroText;
--- tailwindui/components/landing/introduction.tsx
+++ tailwindui/components/landing/introduction.tsx
@@ -5,12 +5,14 @@ import background from '@/public/images/landing.jpeg'
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import AuthService from "../utils/AuthService";
-import RollerTitle from './rollerTitle';
+import HeroText from './HeroText';
 
 const InternalIntroduction = () => {
   const router = useRouter();
 
   const [currentUser, setCurrentUser] = useState(null);
+
+  const [imageOpacity, setImageOpacity] = useState('opacity-100');
 
   useEffect(() => {
     const fetchCurrentUser = async () => {
@@ -19,6 +21,8 @@ const InternalIntroduction = () => {
     }
 
     fetchCurrentUser();
+
+    setImageOpacity('opacity-50');
   }, []); // The empty array means this effect runs once on mount and never again
 
 
@@ -38,7 +42,7 @@ const InternalIntroduction = () => {
       {/* Illustration behind hero content */}
       <div className="h-full absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none -z-1" aria-hidden="true">
         <Image
-          className="h-full max-w-none opacity-70"
+          className={`h-full max-w-none ${imageOpacity} transition-opacity duration-5000`}
           src={background}
           height={(typeof window !== 'undefined' && window.innerHeight < 768) ? window.innerHeight : 768}
           style={{ objectFit: "contain" }}
@@ -54,9 +58,10 @@ const InternalIntroduction = () => {
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
             <div className="max-w-3xl mx-auto">
-                <RollerTitle />
+                <HeroText />
               {/* <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Dr. <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Lambda</span></h1> */}
-              <p className="text-2xl md:text-2xl text-black-400 mb-8 font-bold" data-aos="zoom-y-out" data-aos-delay="150">Remix your knowledge with Dr. Lambda</p>
+              <br></br>
+              {/* <p className="text-2xl md:text-2xl text-black-400 mb-8 font-bold" data-aos="zoom-y-out" data-aos-delay="150">Remix your knowledge with Dr. Lambda</p> */}
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0 cursor-pointer" onClick={handleOnClick}>
