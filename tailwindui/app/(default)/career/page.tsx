03d40b57576f3527bef9c137e7b38a505111dbdc
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-08-29 14:16:00 -0700

    add career page

---
+++ tailwindui/app/(default)/career/page.tsx
@@ -0,0 +1,70 @@
+export const metadata = {
+    title: "Career - Dr. Lambda",
+    description: "Join to work with us at Dr. Lambda.",
+};
+
+export default function Career() {
+    return (
+        <section className="bg-gradient-to-b from-gray-100 to-white">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6">
+                <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+                    {/* Page header */}
+                    <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+                        <h1 className="h1">Careers at Dr. Lambda</h1>
+                    </div>
+                    {/* Founders Section */}
+                    <div className="max-w-3xl mx-auto pb-4">
+                        <div className="pb-8">
+                            <h2 className="h3 text-2xl">🧑🏻‍🎨 Design</h2>
+                        </div>
+
+                        {/* Founder 1 */}
+                        <div className="flex flex-col md:flex-row items-center pb-8">
+                            <div>
+                                <h3 className="h3 text-xl mb-2">UX Design Internship at Dr. Lambda</h3>
+                                <p className="text-base text-gray-600">
+                                    Seeking a driven UX enthusiast passionate about innovative design and education? Dr. Lambda invites you to reshape the educational landscape as a UX Design Intern.<br /><br />
+
+                                    <strong>About Dr. Lambda:</strong><br />
+                                    We're at the forefront of AI-driven educational solutions, championing adaptive and engaging learning for all.<br /><br />
+
+                                    <strong>Role Insight:</strong><br />
+                                    Engage with our designers to craft user-centric designs amplifying our students' learning experience. This role is ideal for design aficionados with expertise in industrial, product, or UX design, eager to dive into a transformative space.<br /><br />
+
+                                    <strong>Key Tasks:</strong><br />
+                                    - Partner with our designers to craft intuitive UIs for our AI-driven platforms.<br />
+                                    - Design, prototype, and iterate on user flows and interfaces using tools like Figma.<br />
+                                    - Spearhead user research, harnessing insights to refine designs.<br />
+                                    - Collaborate with diverse teams to harmonize design with product development.<br />
+                                    - Pitch groundbreaking ideas enhancing our user experience.<br /><br />
+
+                                    <strong>What You Bring:</strong><br />
+                                    - Degree in industrial design, product design, UX, or similar.<br />
+                                    - 1+ year of design-centric professional experience.<br />
+                                    - Robust portfolio demonstrating your design prowess and methodologies.<br />
+                                    - Mastery of design tools, especially Figma.<br />
+                                    - Outstanding communication, teamwork, and a penchant for education tech.<br />
+                                    - Meticulous problem-solving abilities.<br /><br />
+
+                                    <strong>Our Promise:</strong><br />
+                                    - A vibrant workspace fostering creativity.<br />
+                                    - Collaborate with AI and education veterans.<br />
+                                    - Directly impact global learners with your designs.<br />
+                                    - Access to the latest in edtech and design.<br />
+                                    - Competitive pay with avenues for growth at Dr. Lambda.<br /><br />
+
+                                    Eager to redefine education with your design finesse? Apply now for the UX Design Internship at Dr. Lambda!<br /><br />
+
+                                    <strong>How to Apply:</strong><br />
+                                    Share your resume, portfolio, and a cover letter detailing your journey and interest in the role. Send your applications to <a href="mailto:contact@drlambda.ai">contact@drlambda.ai</a>.<br /><br />
+
+                                    Note: Dr. Lambda champions diversity and inclusivity. We welcome all applicants.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
