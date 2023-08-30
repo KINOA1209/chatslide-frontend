ef845a52e7d53f794e1970130a5d5b8c13363706
yjl450 <yil212@ucsd.edu>
2023-08-30 15:20:00 -0700

    change video margin

--- tailwindui/components/landing/samplevideos.tsx
+++ tailwindui/components/landing/samplevideos.tsx
@@ -26,7 +26,7 @@ export default function SampleVideos() {
         <div className="py-12 md:py-20">
 
           {/* Items */}
-          <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+          <div>
             <LiteYouTubeEmbed
               aspectHeight={9}
               aspectWidth={16}
