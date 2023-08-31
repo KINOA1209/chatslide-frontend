7e90d002b4b3013042e46fe72f5b1c43460f908f
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-08-30 17:42:19 -0700

    update career page

--- tailwindui/app/(default)/career/page.tsx
+++ tailwindui/app/(default)/career/page.tsx
@@ -12,21 +12,16 @@ export default function Career() {
                     <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
                         <h1 className="h1">Careers at Dr. Lambda</h1>
                     </div>
-                    {/* Founders Section */}
                     <div className="max-w-3xl mx-auto pb-4">
                         <div className="pb-8">
                             <h2 className="h3 text-2xl">🧑🏻‍🎨 Design</h2>
                         </div>
 
-                        {/* Founder 1 */}
                         <div className="flex flex-col md:flex-row items-center pb-8">
                             <div>
-                                <h3 className="h3 text-xl mb-2">UX Design Internship at Dr. Lambda</h3>
+                                <h3 className="h3 text-xl mb-2">UX Design Intern</h3>
                                 <p className="text-base text-gray-600">
                                     Seeking a driven UX enthusiast passionate about innovative design and education? Dr. Lambda invites you to reshape the educational landscape as a UX Design Intern.<br /><br />
-
-                                    <strong>About Dr. Lambda:</strong><br />
-                                    We're at the forefront of AI-driven educational solutions, championing adaptive and engaging learning for all.<br /><br />
 
                                     <strong>Role Insight:</strong><br />
                                     Engage with our designers to craft user-centric designs amplifying our students' learning experience. This role is ideal for design aficionados with expertise in industrial, product, or UX design, eager to dive into a transformative space.<br /><br />
@@ -62,9 +57,59 @@ export default function Career() {
                                 </p>
                             </div>
                         </div>
+
+                    </div>
+                    <div className="max-w-3xl mx-auto pb-4">
+                        <div className="pb-8">
+                            <h2 className="h3 text-2xl">🧑🏽‍💻 Engineering</h2>
+                        </div>
+                        <div className="flex flex-col md:flex-row items-center pb-8">
+                            <div>
+                                <h3 className="h3 text-xl mb-2">Frontend Developer</h3>
+                                <p className="text-base text-gray-600">
+                                    Are you a dynamic Frontend Developer, eager to craft web applications that stand out? We invite you to join our vibrant team and enhance the digital experiences we offer.<br /><br />
+
+                                    <strong>Role Insight:</strong><br />
+                                    Collaborate with our design and backend teams to produce top-notch web applications. This role is perfect for developers proficient in React, Redux, and modern web technologies looking to make a significant impact.<br /><br />
+
+                                    <strong>Key Tasks:</strong><br />
+                                    - Develop and maintain user-centric features using React.js.<br />
+                                    - Build reusable components for future deployment.<br />
+                                    - Translate high-fidelity designs into impeccable code.<br />
+                                    - Optimize applications for diverse devices and browsers.<br />
+                                    - Stay updated with cutting-edge web technologies.<br /><br />
+
+                                    <strong>What You Bring:</strong><br />
+                                    - Strong proficiency in JavaScript, React.js, and Redux.<br />
+                                    - Knowledge of RESTful APIs and modern authentication mechanisms.<br />
+                                    - Expertise in HTML5, CSS3, and responsive design.<br />
+                                    - Experience with front-end development tools, such as Babel, Webpack, and NPM.<br />
+                                    - The knack to interpret business needs into technical solutions.<br /><br />
+
+                                    <strong>Preferred Expertise:</strong><br />
+                                    - A degree in Computer Science or a related field.<br />
+                                    - Familiarity with CSS pre-processors like LESS and SASS.<br />
+                                    - Hands-on experience with Git or similar versioning tools.<br /><br />
+
+                                    <strong>Our Promise:</strong><br />
+                                    - A stimulating environment nurturing innovation.<br />
+                                    - Collaborate with seasoned professionals in the tech domain.<br />
+                                    - A chance to shape the future of our web applications.<br />
+                                    - Access to continuous learning and professional growth.<br />
+                                    - Competitive compensation and benefits.<br /><br />
+
+                                    Ready to elevate our web experiences with your coding artistry? Apply now for the Frontend Developer position!<br /><br />
+
+                                    <strong>How to Apply:</strong><br />
+                                    Kindly forward your resume, portfolio, and a cover letter elucidating your passion and fit for the role. Direct your applications to <a href="mailto:contact@drlambda.ai">contact@drlambda.ai</a>.<br /><br />
+
+                                    Note: We are an equal opportunity employer, ardently championing diversity and inclusivity. All talented developers are encouraged to apply.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </section >
     );
 }
