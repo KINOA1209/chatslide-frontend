05d56733d76aecec84ee4321f24523400465800b
yjl450 <yil212@ucsd.edu>
2023-09-07 23:24:13 -0700

    fix workflow background (ios safari)

--- tailwindui/app/(feature)/layout.tsx
+++ tailwindui/app/(feature)/layout.tsx
@@ -12,14 +12,12 @@ export default function WorkflowLayout({
   children: React.ReactNode
 }) {
   return (
-    <main className="flex flex-col" style={{ minHeight: '100vh' }}>
+    <main className="flex flex-col min-h-[100vh]">
+      <div className="w-[100vw] h-[100vh] -z-1 fixed" style={{
+        background: 'linear-gradient(169deg, rgba(237,113,206,0.602) 0%, rgba(199,157,223,0.566) 13%, rgba(141,165,225,0.397) 40%, rgba(42,136,237,0.221) 50%, rgba(255,255,255,1) 77%, rgba(255,255,255,1) 100%)',
+      }}></div>
       <Header loginRequired={true} isLanding={false} refList={[]} />
-      <section className="bg-gradient-to-b from-gray-100 to-white pb-8 grow flex flex-col"
-        style={{
-          background: 'linear-gradient(169deg, rgba(237,113,206,0.602) 0%, rgba(199,157,223,0.566) 13%, rgba(141,165,225,0.397) 40%, rgba(42,136,237,0.221) 50%, rgba(255,255,255,1) 77%, rgba(255,255,255,1) 100%)',
-          backgroundSize: '100vw 100vh',
-          backgroundAttachment: 'fixed'
-        }}>
+      <section className="pb-8 grow flex flex-col">
         {/* Content */}
         {children}
       </section>
