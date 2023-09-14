12b28ca971de9bfdcfc9ae9bc8218c966d4aa95f
yjl450 <yil212@ucsd.edu>
2023-09-13 19:25:47 -0700

    init account setting page

---
+++ tailwindui/app/(account)/layout.tsx
@@ -0,0 +1,38 @@
+import { WorkflowFooter } from '@/components/ui/footer'
+import Header from '@/components/ui/header';
+
+export const metadata = {
+  title: 'My Account - Dr. Lambda',
+  description: 'Create new content',
+}
+
+export default function AccountLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <Header loginRequired={true} isLanding={false} refList={[]} />
+      <main className="flex flex-col min-h-[100vh]">
+        <div className='grow flex flex-col'>
+          <div className="bg-gradient-to-b from-gray-100 to-white grow flex flex-col h-full pt-16 md:pt-32">
+            <div className='grow h-full w-full max-w-6xl mx-auto px-4 flex flex-row'>
+              <div className='grow max-w-[25%] flex-col items-center hidden md:flex'>
+                <h1 className="h4">My Account</h1>
+                <div className='flex flex-col gap-y-8 mt-8'>
+                  <div className='btn rounded-full bg-gray-200 w-full'>Profile</div>
+                  <div className='btn rounded-full bg-gray-200 w-full'>Password</div>
+                  <div className='btn rounded-full bg-gray-200 w-full'>Subscription</div>
+                  <div className='btn rounded-full bg-gray-200 w-full'>History</div>
+                </div>
+              </div>
+              <div className='grow'>{children}</div>
+            </div>
+          </div>
+        </div>
+        <WorkflowFooter />
+      </main>
+    </>
+  )
+}
