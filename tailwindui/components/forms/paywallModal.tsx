32f7b4f4d8f01f7d0fcbc76d7543eb8647aac6c1
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-09-28 19:33:49 -0700

    fit smaller screen

--- tailwindui/components/forms/paywallModal.tsx
+++ tailwindui/components/forms/paywallModal.tsx
@@ -9,36 +9,33 @@ interface PaywallModalProps {
 
 const PaywallModal: React.FC<PaywallModalProps> = ({ setShowModal }) => {
 
+    const modalRef = React.useRef<HTMLDivElement>(null);
+
     const handleCloseModal = () => {
         setShowModal(false);
+    };
+
+    const handleOutsideClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        if (event.target === modalRef.current) {
+            handleCloseModal();
+        }
     };
 
     return (
         <>
             <div
+                ref={modalRef}
                 id="staticModal"
                 data-modal-backdrop="static"
                 tabIndex={-1}
                 aria-hidden="true"
                 className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center w-full h-full"
+                onClick={handleOutsideClick}
             >
                 <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 w-128">
-                    {/* Modal header */}
-                    <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                            Premium Features
-                        </h3>
-                        <button
-                            type="button"
-                            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                            onClick={handleCloseModal}
-                        >
-                            Close
-                        </button>
-                    </div>
                     {/* Modal body */}
                     <div className="p-6 space-y-6">
-                        <Pricing />
+                        <Pricing fewerCards={true} />
                     </div>
                 </div>
             </div>
