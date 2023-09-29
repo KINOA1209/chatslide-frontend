b95c72aab31e575f8a24a4087d3d16e28e6e91fe
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-09-29 12:22:14 -0700

    refactor

---
+++ tailwindui/components/slides/SlideVisualizer.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useRef } from 'react';
+import TranscriptForm from '@/components/forms/transcriptForm';
+import Timer from '@/components/ui/Timer';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+//import SlidesHTML from '@/components/SlidesHTML';
+//import SaveToPdfHtml from '@/components/forms/saveToPdfHtml';
+import { SlideElement, Slide } from '@/components/SlidesHTML';
+import dynamic from 'next/dynamic'
+
+
+const SlidesHTML = dynamic(() => import('@/components/SlidesHTML'));
+const SaveToPdfHtml = dynamic(() => import('@/components/forms/saveToPdfHtml'));
+
+// const SlidesHTML = dynamic(
+//     () => import('@/components/SlidesHTML'),
+//     { ssr: false }
+// )
+
+// const SaveToPdfHtml = dynamic(
+//     () => import('@/components/forms/saveToPdfHtml'),
+//     { ssr: false }
+// )
+
+const SlideVisualizer = () => {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [showPopup, setShowPopup] = useState<boolean>(false);
+
+    const [finalSlides, setFinalSlides] = useState<Slide[]>([]);
+
+    return (
+        <div>
+            <div className="max-w-4xl mx-auto px-4 sm:px-6">
+
+                <SlidesHTML finalSlides={finalSlides} setFinalSlides={setFinalSlides} />
+
+                {/* Form */}
+                <TranscriptForm
+                    isSubmitting={isSubmitting}
+                    setIsSubmitting={setIsSubmitting}
+                    finalSlides={finalSlides}
+                />
+
+                {/* Timer */}
+                <Timer expectedSeconds={60} isSubmitting={isSubmitting} />
+
+                <SaveToPdfHtml finalSlides={finalSlides} />
+            </div>
+
+        </div>
+    );
+};
+
+export default SlideVisualizer;
