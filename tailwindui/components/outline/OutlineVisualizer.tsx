d078558921bb59c5f41c9c446609c8bea277c811
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-10-04 23:02:56 -0700

    generate preview only

--- tailwindui/components/outline/OutlineVisualizer.tsx
+++ tailwindui/components/outline/OutlineVisualizer.tsx
@@ -162,13 +162,92 @@ const OutlineVisualizer = ({ outline }: { outline: OutlineDataType }) => {
         handleSubmit();
     };
 
+    async function query_resources(project_id: any, resources: any, outlineData: any) {
+        const { userId, idToken: token } = await AuthService.getCurrentUserTokenAndId();
+        const headers = new Headers();
+        if (token) {
+            headers.append('Authorization', `Bearer ${token}`);
+        }
+
+        const response = await fetch('/api/query_resources', {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify({
+                outlines: JSON.stringify({ ...outlineData }),
+                resources: resources,
+                project_id: project_id
+            })
+        });
+
+        if (response.ok) {
+            return await response.json();
+        } else {
+            // alert("Request failed: " + response.status);
+            console.log(response)
+            // setIsSubmittingScript(false);
+            // setIsSubmittingSlide(false);
+        }
+    }
+
+    async function generateScripts(formData: any, token: string) {
+        mixpanel.track('Generate Script', {
+            formData: formData
+        });
+        const response = await fetch('/api/scripts_only', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(formData)
+        });
+
+        if (response.ok) {
+            const resp = await response.json();
+            // console.log(resp);
+            setIsSubmittingScript(false);
+            // Store the data in local storage
+            // console.log(resp.data);
+            sessionStorage.setItem('transcripts', JSON.stringify(resp.data.res));
+            // Redirect to a new page with the data
+            router.push('workflow-edit-script');
+        } else {
+            alert("Request failed: " + response.status);
+            // console.log(response)
+            setIsSubmittingScript(false);
+        }
+    }
+
+    async function generateSlidesPreview(formData: any, token: string) {
+        mixpanel.track('Generate HTML', {
+            formData: formData
+        });
+        const response = await fetch('/api/generate_html', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+
+        if (response.ok) {
+            const resp = await response.json();
+            setIsSubmittingSlide(false);
+            sessionStorage.setItem('html', JSON.stringify(resp.data.res));
+            router.push('workflow-review-slides');
+        } else {
+            alert(`Server is busy now. Please try again later. Reference code: ` + sessionStorage.getItem('project_id'));
+            console.log(response)
+            setIsSubmittingSlide(false);
+        }
+    }
+
     const handleSubmit = async () => {
         setTimer(0);
         let formData: any = {};
 
-
         // remove empty entries
-        // console.log(outlineData);
         const outlineCopy = [...outlineData];
         for (let i = 0; i < outlineCopy.length; i++) {
             outlineCopy[i].content = outlineCopy[i].content.filter(s => { return s.length > 0 });
@@ -185,49 +264,6 @@ const OutlineVisualizer = ({ outline }: { outline: OutlineDataType }) => {
         const addEquations = typeof window !== 'undefined' ? sessionStorage.getItem('addEquations') : null;
         const extraKnowledge = typeof window !== 'undefined' ? sessionStorage.getItem('extraKnowledge') : null;
 
-        async function query_resources(project_id: any, resources: any, outlineData: any) {
-            const { userId, idToken: token } = await AuthService.getCurrentUserTokenAndId();
-            const headers = new Headers();
-            if (token) {
-                headers.append('Authorization', `Bearer ${token}`);
-            }
-
-            const response = await fetch('/api/query_resources', {
-                method: 'POST',
-                headers: headers,
-                body: JSON.stringify({
-                    outlines: JSON.stringify({ ...outlineData }),
-                    resources: resources,
-                    project_id: project_id
-                })
-            });
-
-            if (response.ok) {
-                return await response.json();
-            } else {
-                // alert("Request failed: " + response.status);
-                console.log(response)
-                // setIsSubmittingScript(false);
-                // setIsSubmittingSlide(false);
-            }
-        }
-
-        if (resources && resources.length > 0 && !extraKnowledge) {
-            try {
-                console.log('querying vector database');
-                const extraKnowledge = await query_resources(project_id, resources, outlineData);
-                sessionStorage.setItem('extraKnowledge', JSON.stringify(extraKnowledge.data.res));
-                sessionStorage.setItem('outline_item_counts',
-                    JSON.stringify(extraKnowledge.data.outline_item_counts));
-            } catch (error) {
-                console.log('Error querying vector database', error);
-                // return; 
-            }
-        }
-        else {
-            console.log('no need to query vector database');
-        }
-
         formData = {
             res: JSON.stringify({ ...outlineData }),
             outlines: JSON.stringify({ ...outlineData }),
@@ -241,106 +277,40 @@ const OutlineVisualizer = ({ outline }: { outline: OutlineDataType }) => {
             model_name: isGpt35 ? 'gpt-3.5-turbo' : 'gpt-4',
             slidePages: slidePages,
             wordPerSubpoint: wordPerSubpoint,
+            endIndex: 2,  // generate first 2 sections only
         };
 
-        // console.log(formData);
-        mixpanel.track('Generate Slides', {
-            'audience': audience,
-            'language': language,
-            'project_id': project_id,
-            'addEquations': addEquations,
-            'extraKnowledge': extraKnowledge,
-            'model_name': isGpt35 ? 'gpt-3.5-turbo' : 'gpt-4',
-            'slidePages': slidePages,
-            'wordPerSubpoint': wordPerSubpoint,
-        });
+        if (resources && resources.length > 0 && !extraKnowledge) {
+            try {
+                console.log('querying vector database');
+                const extraKnowledge = await query_resources(project_id, resources, outlineData);
+                sessionStorage.setItem('extraKnowledge', JSON.stringify(extraKnowledge.data.res));
+                sessionStorage.setItem('outline_item_counts',
+                    JSON.stringify(extraKnowledge.data.outline_item_counts));
+                formData.extraKnowledge = extraKnowledge;
+                formData.outline_item_counts = extraKnowledge.data.outline_item_counts;
+            } catch (error) {
+                console.log('Error querying vector database', error);
+                // return; 
+            }
+        }
+        else {
+            console.log('no need to query vector database');
+        }
 
         try {
-            // this is defined before
-            const extraKnowledge = sessionStorage.getItem('extraKnowledge');
-            const outline_item_counts = sessionStorage.getItem('outline_item_counts');
-            if (extraKnowledge) {
-                // add pdf knowledge to formData
-                formData.extraKnowledge = extraKnowledge;
-            }
-            if (outline_item_counts) {
-                // add outline item counts to formData
-                formData.outline_item_counts = outline_item_counts;
-            }
-
             const { userId, idToken: token } = await AuthService.getCurrentUserTokenAndId();
-
-            // generate slides
-            if (toSlides === true) {
-                const { userId, idToken: token } = await AuthService.getCurrentUserTokenAndId();
-                mixpanel.track('Generate HTML', {
-                    'Project ID': sessionStorage.getItem('project_id'),
-                    'Language': language,
-                });
-                const response = await fetch('/api/generate_html', {
-                    method: 'POST',
-                    headers: {
-                        'Authorization': `Bearer ${token}`,
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(formData)
-                });
-                // console.log('formData is:', formData);
-
-
-                if (response.ok) {
-                    const resp = await response.json();
-                    // console.log(resp);
-                    setIsSubmittingSlide(false);
-                    // Store the data in local storage
-                    // console.log(resp.data);
-
-                    // Redirect to a new page with the data
-                    router.push('workflow-review-slides');
-                } else {
-                    alert(`Server is busy now. Please try again later. Reference code: ` + sessionStorage.getItem('project_id'));
-                    console.log(response)
-                    setIsSubmittingSlide(false);
-                }
+            if (toSlides) {
+                await generateSlidesPreview(formData, token);
             }
-            // generate script direclty
             else {
-                mixpanel.track('Generate Script', {
-                    'Project ID': sessionStorage.getItem('project_id'),
-                    'Language': language,
-                });
-                const response = await fetch('/api/scripts_only', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${token}`
-                    },
-                    body: JSON.stringify(formData)
-                });
-
-                // console.log('formData is:', formData);
-
-                if (response.ok) {
-                    const resp = await response.json();
-                    // console.log(resp);
-                    setIsSubmittingScript(false);
-                    // Store the data in local storage
-                    // console.log(resp.data);
-                    sessionStorage.setItem('transcripts', JSON.stringify(resp.data.res));
-                    // Redirect to a new page with the data
-                    router.push('workflow-edit-script');
-                } else {
-                    alert("Request failed: " + response.status);
-                    // console.log(response)
-                    setIsSubmittingScript(false);
-                }
+                await generateScripts(formData, token);
             }
         } catch (error) {
             console.error('Error:', error);
             setIsSubmittingSlide(false);
             setIsSubmittingScript(false);
         }
-
     };
 
     const handleAddDetail = (e: React.MouseEvent<SVGSVGElement>, sectionIndex: number, detailIndex: number) => {
