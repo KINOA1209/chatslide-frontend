16cf56cd2a0478cb4215afd3417b265ce938a93a
Zhiyuan Wang <wangzyzju@gmail.com>
2023-10-16 23:21:25 -0700

    audio page sends /api/mp3 requests with authorization headers

--- tailwindui/components/Audio.tsx
+++ tailwindui/components/Audio.tsx
@@ -1,13 +1,15 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 interface Props {
     filename: string;
     foldername: string;
+    token?: string;
 }
 
-const PlayButton: React.FC<Props> = ({ filename, foldername }) => {
+const PlayButton: React.FC<Props> = ({ filename, foldername, token }) => {
     const audioRef = useRef<HTMLAudioElement | null>(null);
-    const [isPlaying, setIsPlaying] = React.useState(false);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [audioBlobUrl, setAudioBlobUrl] = useState<string | null>(null);
 
     const handleAudio = () => {
         if (audioRef.current) {
@@ -32,11 +34,29 @@ const PlayButton: React.FC<Props> = ({ filename, foldername }) => {
         };
     }, []);
 
-    const audioSource = `/api/audio?foldername=${foldername}&filename=${filename}`;
+    useEffect(() => {
+        const fetchAudio = async () => {
+            try {
+                const headers = {
+                    'Authorization': `Bearer ${token}`
+                };
+                const response = await fetch(`/api/audio?foldername=${foldername}&filename=${filename}`, { headers });
+                const audioBlob = await response.blob();
+                const blobUrl = URL.createObjectURL(audioBlob);
+                setAudioBlobUrl(blobUrl);
+            } catch (error) {
+                console.error('Failed to fetch audio:', error);
+            }
+        };
+
+        fetchAudio();
+    }, []);
+
+    // const audioSource = `/api/audio?foldername=${foldername}&filename=${filename}`;
 
     return (
         <div>
-            <audio ref={audioRef} src={audioSource} preload="auto" />
+            {audioBlobUrl && <audio ref={audioRef} src={audioBlobUrl} preload="auto" />}
             <button 
                 className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700" 
                 onClick={handleAudio}
