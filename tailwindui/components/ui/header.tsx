fcf9394f6ccc3703ea42366c8b2a456ef14ad032
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-10-17 11:37:43 -0700

    update nav bar logo size align and font

--- tailwindui/components/ui/header.tsx
+++ tailwindui/components/ui/header.tsx
@@ -1,184 +1,196 @@
-"use client";
+'use client'
 
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 
-import Link from "next/link";
-import {Logo} from "./logo";
-import DropdownButton from "@/components/utils/dropdown";
-import MobileMenu from "./mobile-menu";
-import { useRouter } from "next/navigation";
-import GoogleAnalytics from "@/components/integrations/GoogleAnalytics";
-import Hotjar from "@/components/integrations/Hotjar";
+import Link from 'next/link'
+import { Logo } from './logo'
+import DropdownButton from '@/components/utils/dropdown'
+import MobileMenu from './mobile-menu'
+import { useRouter } from 'next/navigation'
+import GoogleAnalytics from '@/components/integrations/GoogleAnalytics'
+import Hotjar from '@/components/integrations/Hotjar'
 // import AuthService from "../utils/AuthService";
-import { Auth, Hub } from 'aws-amplify';
-import mixpanel from 'mixpanel-browser';
-import AuthService from "../utils/AuthService";
-import { DrlambdaLogoIcon } from "../new_landing/Icons";
+import { Auth, Hub } from 'aws-amplify'
+import mixpanel from 'mixpanel-browser'
+import AuthService from '../utils/AuthService'
+import { DrlambdaLogoIcon } from '../new_landing/Icons'
 
 interface HeaderProps {
-    loginRequired: boolean,
-    isLanding: boolean,
-    refList?: Array<React.RefObject<HTMLDivElement>>
+  loginRequired: boolean
+  isLanding: boolean
+  refList?: Array<React.RefObject<HTMLDivElement>>
 }
-const Header = ({loginRequired, isLanding = false, refList }: HeaderProps) => {
-    const [top, setTop] = useState<boolean>(true);
-    const [userId, setUserId] = useState(null);
-    // const [username, setUsername] = useState(null);
-    const [loading, setLoading] = useState(true);
+const Header = ({ loginRequired, isLanding = false, refList }: HeaderProps) => {
+  const [top, setTop] = useState<boolean>(true)
+  const [userId, setUserId] = useState(null)
+  // const [username, setUsername] = useState(null);
+  const [loading, setLoading] = useState(true)
 
-    const router = useRouter();
+  const router = useRouter()
 
-    // detect whether user has scrolled the page down by 10px
-    const scrollHandler = () => {
-        window.scrollY > 10 ? setTop(false) : setTop(true);
-    };
+  // detect whether user has scrolled the page down by 10px
+  const scrollHandler = () => {
+    window.scrollY > 10 ? setTop(false) : setTop(true)
+  }
 
-    useEffect(() => {
-        scrollHandler();
-        window.addEventListener("scroll", scrollHandler);
-        return () => window.removeEventListener("scroll", scrollHandler);
-    }, [top]);
+  useEffect(() => {
+    scrollHandler()
+    window.addEventListener('scroll', scrollHandler)
+    return () => window.removeEventListener('scroll', scrollHandler)
+  }, [top])
 
-    useEffect(() => {
-        mixpanel.init('22044147cd36f20bf805d416e1235329', { debug: true, track_pageview: true, persistence: 'localStorage', ignore_dnt: true });
+  useEffect(() => {
+    mixpanel.init('22044147cd36f20bf805d416e1235329', {
+      debug: true,
+      track_pageview: true,
+      persistence: 'localStorage',
+      ignore_dnt: true,
+    })
 
-        const checkUser = async () => {
-            try {
-                const {userId, idToken} = await AuthService.getCurrentUserTokenAndId();
-                setUserId(userId);
-                mixpanel.identify(userId);
-                setLoading(false);
-            } catch {
-                console.log('No authenticated user.');
-                if (loginRequired) {
-                    router.push('/signin')
-                }
-                setLoading(false);
-            }
-        };
-
-        // check the current user when component loads
-        checkUser();
-        
-        const listener = (data: any) => {
-            switch (data.payload.event) {
-                case 'signIn':
-                    console.log('user signed in');
-                    checkUser();
-                    break;
-                case 'signOut':
-                    console.log('user signed out');
-                    setUserId(null);
-                    break;
-                default:
-                    break;
-            }
-        };
-
-        // add auth event listener
-        Hub.listen('auth', listener);
-
-        // remove auth event listener on cleanup
-        return () => {
-            Hub.remove('auth', listener);
-        };
-    }, []);
-
-    const handScrollTo = (e: React.MouseEvent<HTMLDivElement>, index: number) => {
-        if (isLanding && refList && refList[index].current) {
-            refList[index].current?.scrollIntoView();
+    const checkUser = async () => {
+      try {
+        const { userId, idToken } = await AuthService.getCurrentUserTokenAndId()
+        setUserId(userId)
+        mixpanel.identify(userId)
+        setLoading(false)
+      } catch {
+        console.log('No authenticated user.')
+        if (loginRequired) {
+          router.push('/signin')
         }
+        setLoading(false)
+      }
     }
 
-    if (loading) {
-        // Render a loading state or a blank placeholder
-        return (
-            <header
-                className={`fixed w-full z-30 bg-white md:bg-opacity-90 transition duration-300 ease-in-out ${!top ? "bg-white backdrop-blur-sm shadow-lg" : ""
-                    }`}
-            >
-                <div className="max-w-4/5 mx-auto px-5 sm:px-6">
-                    <div className="flex items-center justify-between h-16 md:h-20">
-                        {/* Site branding */}
-                        <div className="flex flex-row items-center md:items-end grow md:grow-0">
-                            <Logo />
-                            <div className="grow md:grow-0 flex justify-center md:justify-start">
-                                <div className="w-fit text-xl md:text-3xl text-transparent bg-clip-text bg-gradient-to-r from-blue-600  to-purple-500 md:relative md:-bottom-[7px]" style={{ fontFamily: 'Lexend, sans-serif' }}>
-                                    <a href="/">DrLambda</a>
-                                </div>
-                            </div>
-                        </div>
+    // check the current user when component loads
+    checkUser()
 
-                        {/* Desktop navigation */}
-                        <nav className="hidden lg:flex w-[272px]"></nav>
-
-                        <MobileMenu refList={refList} />
-                    </div>
-                </div>
-                <GoogleAnalytics />
-                <Hotjar />
-            </header>
-        );
+    const listener = (data: any) => {
+      switch (data.payload.event) {
+        case 'signIn':
+          console.log('user signed in')
+          checkUser()
+          break
+        case 'signOut':
+          console.log('user signed out')
+          setUserId(null)
+          break
+        default:
+          break
+      }
     }
 
+    // add auth event listener
+    Hub.listen('auth', listener)
 
+    // remove auth event listener on cleanup
+    return () => {
+      Hub.remove('auth', listener)
+    }
+  }, [])
+
+  const handScrollTo = (e: React.MouseEvent<HTMLDivElement>, index: number) => {
+    if (isLanding && refList && refList[index].current) {
+      refList[index].current?.scrollIntoView()
+    }
+  }
+
+  if (loading) {
+    // Render a loading state or a blank placeholder
     return (
-        <header
-            className={`fixed w-full z-30 bg-gray-800 md:bg-opacity-90 transition duration-300 ease-in-out ${!top ? "bg-gray-800 backdrop-blur-sm shadow-lg" : ""
-                }`}
-        >
-            <div className="max-w-4/5 mx-auto px-5 sm:px-6">
-                <div className="flex items-center justify-between h-16 md:h-20">
-                    {/* Site branding */}
-                    <div className="flex flex-row items-center md:items-end grow md:grow-0">
-                        <Logo />
-                        <div className="grow md:grow-0 flex flex-row justify-center md:justify-start">
-                            <div className="w-fit text-xl md:text-3xl text-gray-200 bg-clip-text bg-gradient-to-r md:relative md:-bottom-[7px]" style={{ fontFamily: 'Lexend, sans-serif' }}>
-                                <a href="/">DrLambda</a>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    {/* Desktop navigation */}
-                    <nav className="hidden lg:flex w-[272px]">
-                        {/* Desktop sign in links */}
-                        {userId ? (
-                            <ul className="flex grow justify-end flex-wrap items-center">
-                                <DropdownButton />
-                            </ul>
-                        ) : (
-                            <ul className="flex grow justify-end flex-nowrap items-center">
-                                <li>
-                                    <Link
-                                        href="/signin"
-                                        className="btn-sm drop-shadow-xl rounded-full text-white w-full mb-4 sm:w-auto sm:mb-0 cursor-pointer mr-4"
-                                        style={{backgroundColor: "#1D222A", backgroundSize: '100%', fontFamily: 'Lexend, sans-serif' }}
-                                    >
-                                        Sign in
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        href="/signup"
-                                        className="btn-sm drop-shadow-xl rounded-full text-white w-full mb-4 sm:w-auto sm:mb-0 cursor-pointer"
-                                        style={{backgroundColor: "#1D222A", backgroundSize: '100%', fontFamily: 'Lexend, sans-serif' }}
-                                    >
-                                        <span>Join for Free</span>
-                                    </Link>
-                                </li>
-                            </ul>
-
-                        )}
-                    </nav>
-
-                    <MobileMenu refList={refList} />
+      <header
+        className={`fixed w-full z-30 bg-white md:bg-opacity-90 transition duration-300 ease-in-out ${
+          !top ? 'bg-white backdrop-blur-sm shadow-lg' : ''
+        }`}
+      >
+        <div className='max-w-4/5 mx-auto px-5 sm:px-6'>
+          <div className='flex items-center justify-between h-12'>
+            {/* Site branding */}
+            <div className='flex flex-row justify-center items-center gap-[0.37rem] md:items-end grow md:grow-0'>
+              <Logo />
+              <div className='grow md:grow-0 flex justify-center md:justify-start'>
+                <div className='w-fit h-[1.5rem] text-xl md:text-[1.3125rem]text-transparent bg-clip-text bg-gradient-to-r from-blue-600  to-purple-500 md:relative md:bottom-[3px] font-creato-medium'>
+                  <a href='/'>DrLambda</a>
                 </div>
+              </div>
             </div>
 
-            <GoogleAnalytics />
-        </header>
-    );
+            {/* Desktop navigation */}
+            <nav className='hidden lg:flex w-[272px]'></nav>
+
+            <MobileMenu refList={refList} />
+          </div>
+        </div>
+        <GoogleAnalytics />
+        <Hotjar />
+      </header>
+    )
+  }
+
+  return (
+    <header
+      className={`fixed w-full z-30 bg-gray-800 md:bg-opacity-90 transition duration-300 ease-in-out ${
+        !top ? 'bg-gray-800 backdrop-blur-sm shadow-lg' : ''
+      }`}
+    >
+      <div className='max-w-4/5 mx-auto px-5 sm:px-6'>
+        <div className='flex items-center justify-between h-12'>
+          {/* Site branding */}
+          <div className='flex flex-row items-center md:items-end grow md:grow-0 gap-[0.37rem]'>
+            <Logo />
+            <div className='grow md:grow-0 flex flex-row justify-center item-center md:justify-start'>
+              <div className='w-fit h-[1.5rem] text-xl md:text-[1.3125rem] text-gray-200 bg-clip-text bg-gradient-to-r md:relative md:bottom-[3px] font-creato-medium'>
+                <a href='/'>DrLambda</a>
+              </div>
+            </div>
+          </div>
+
+          {/* Desktop navigation */}
+          <nav className='hidden lg:flex w-[272px]'>
+            {/* Desktop sign in links */}
+            {userId ? (
+              <ul className='flex grow justify-end flex-wrap items-center'>
+                <DropdownButton />
+              </ul>
+            ) : (
+              <ul className='flex grow justify-end flex-nowrap items-center'>
+                <li>
+                  <Link
+                    href='/signin'
+                    className='btn-sm drop-shadow-xl rounded-full text-white w-full mb-4 sm:w-auto sm:mb-0 cursor-pointer mr-4'
+                    style={{
+                      backgroundColor: '#1D222A',
+                      backgroundSize: '100%',
+                      fontFamily: 'Lexend, sans-serif',
+                    }}
+                  >
+                    Sign in
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href='/signup'
+                    className='btn-sm drop-shadow-xl rounded-full text-white w-full mb-4 sm:w-auto sm:mb-0 cursor-pointer'
+                    style={{
+                      backgroundColor: '#1D222A',
+                      backgroundSize: '100%',
+                      fontFamily: 'Lexend, sans-serif',
+                    }}
+                  >
+                    <span>Join for Free</span>
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </nav>
+
+          <MobileMenu refList={refList} />
+        </div>
+      </div>
+
+      <GoogleAnalytics />
+    </header>
+  )
 }
 
-export default Header;
+export default Header
