b93c6fd386ec19aca7a3ebecb06f2235d6ca4280
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-10-26 13:21:24 -0700

    type page implement and scenario page start

--- tailwindui/app/(feature)/workflow-type-choice/page.tsx
+++ tailwindui/app/(feature)/workflow-type-choice/page.tsx
@@ -1,16 +1,50 @@
+'use client'
+
 import React from 'react'
 import {
   PresentationTypeIcon,
   SocialPostTypeIcon,
 } from '@/app/(feature)/workflow-type-choice/icons'
-
+import { useRouter } from 'next/navigation'
 const TypeChoicePage = () => {
+  const router = useRouter() // Initialize the router
+  // Function to navigate to the "workflow-scenario-choice" page
+  const navigateToScenarioChoice = () => {
+    router.push('/workflow-scenario-choice') // Specify the route you want to navigate to
+  }
   return (
-    <div className='bg-zinc-100'>
-      <div className='fle flex-col justify-center items-center'>
-        <div className='w-[47rem] mt-[6rem] flex text-center text-neutral-800 text-2xl font-normal font-creato-medium leading-9 tracking-wide'>
+    <div className='bg-zinc-100 min-h-screen'>
+      <div className='flex flex-col justify-center items-center gap-12'>
+        {/* title */}
+        <div className='w-[47rem] mt-[12rem] text-center text-neutral-800 text-2xl font-normal font-creato-medium leading-9 tracking-wide'>
           Hey, polumage! <br />
           What are you planning to create today?
+        </div>
+        {/* two types choices */}
+        <div className='flex flex-row gap-5'>
+          {/* Presentation */}
+          <div
+            onClick={navigateToScenarioChoice} // Attach the onClick handler
+            className='w-96 h-64 bg-neutral-50 rounded-lg shadow flex flex-col justify-center items-center gap-8 cursor-pointer'
+          >
+            {/* icons and type text box */}
+            <div className='w-[7.5rem] h-[6.75rem]'>
+              <PresentationTypeIcon />
+            </div>
+            <div className='text-center text-neutral-800 text-2xl font-bold font-creato-medium leading-snug tracking-wide'>
+              Presentation
+            </div>
+          </div>
+          {/* Social Post */}
+          <div className='w-96 h-64 bg-neutral-50 rounded-lg shadow flex flex-col justify-center items-center gap-8 cursor-pointer'>
+            {/* icons and type text box */}
+            <div className='w-[7.5rem] h-[6.75rem]'>
+              <SocialPostTypeIcon />
+            </div>
+            <div className='text-center text-neutral-800 text-2xl font-bold font-creato-medium leading-snug tracking-wide'>
+              Social Post
+            </div>
+          </div>
         </div>
       </div>
     </div>
