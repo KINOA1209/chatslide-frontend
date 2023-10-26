e2eb9ba08d5d030e46af54b862d882dde38e9ac0
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-10-26 14:07:13 -0700

    fix review slide page display problem

--- tailwindui/components/slides/NewSlideVisualizer.tsx
+++ tailwindui/components/slides/NewSlideVisualizer.tsx
@@ -4,9 +4,7 @@ import ClickableLink from '@/components/ui/ClickableLink'
 import { Slide } from '@/components/slides/NewSlidesHTML'
 import ExportToPdfButton from './exportToPdfButton'
 import dynamic from 'next/dynamic'
-import {
-    ScriptsIcon,
-} from '@/app/(feature)/icons'
+import { ScriptsIcon } from '@/app/(feature)/icons'
 import { ShareSlidesIcon } from '@/app/(feature)/workflow-review-slides/icons'
 import { ShareToggleButton } from '@/components/slides/SlideButtons'
 
@@ -23,72 +21,73 @@ const SlidesHTML = dynamic(() => import('@/components/slides/NewSlidesHTML'))
 // )
 
 const SlideVisualizer = () => {
-    const [host, setHost] = useState('https://drlambda.ai')
-    const [isSubmitting, setIsSubmitting] = useState(false)
-    const [showPopup, setShowPopup] = useState<boolean>(false)
+  const [host, setHost] = useState('https://drlambda.ai')
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [showPopup, setShowPopup] = useState<boolean>(false)
 
-    const [finalSlides, setFinalSlides] = useState<Slide[]>([])
-    const [share, setShare] = useState(false)
+  const [finalSlides, setFinalSlides] = useState<Slide[]>([])
+  const [share, setShare] = useState(false)
 
-    useEffect(() => {
-        setShare(sessionStorage.getItem('is_shared') === 'true')
-        // console.log('share', sessionStorage.getItem('is_shared'));
-    }, [])
+  useEffect(() => {
+    setShare(sessionStorage.getItem('is_shared') === 'true')
+    // console.log('share', sessionStorage.getItem('is_shared'));
+  }, [])
 
-    useEffect(() => {
-        if (
-            window.location.hostname !== 'localhost' &&
-            window.location.hostname !== '127.0.0.1'
-        ) {
-            setHost('https://' + window.location.hostname)
-        } else {
-            setHost(window.location.hostname)
-        }
-    }, [])
+  useEffect(() => {
+    if (
+      window.location.hostname !== 'localhost' &&
+      window.location.hostname !== '127.0.0.1'
+    ) {
+      setHost('https://' + window.location.hostname)
+    } else {
+      setHost(window.location.hostname)
+    }
+  }, [])
 
-    return (
-        <div>
-            <div className='px-4 sm:px-6 flex flex-col justify-center items-center gap-4'>
-                {/* buttons: export and scripts and share slides */}
-                <div className='flex flex-row justify-between items-center'>
-                    <div className='w-1/3 h-8 px-3 py-1 bg-zinc-100 rounded-lg flex justify-center items-center gap-2.5 cursor-pointer'>
-                        <div className='flex items-center overflow-hidden'>
-                            <div className='text-gray-700 text-sm font-medium font-creato-medium leading-normal tracking-wide whitespace-nowrap overflow-hidden text-ellipsis'>
-                                Want some scripts? (10⭐️)
-                            </div>
-                        </div>
-                        <div className='w-5 h-5 relative'>
-                            <ScriptsIcon />
-                        </div>
-                    </div>
-                    <ExportToPdfButton finalSlides={finalSlides} />
-                    <ShareToggleButton setShare={setShare} share={share} />
-                </div>
+  return (
+    <div>
+      <div className='px-4 sm:px-6 flex flex-col justify-center items-center gap-4'>
+        {/* buttons: export and scripts and share slides */}
+        <div className='flex flex-row justify-end items-center'>
+          {/* want some scripts? */}
+          <div className='h-8 px-3 py-1 bg-zinc-100 rounded-lg flex justify-center items-center gap-2.5 cursor-pointer'>
+            <div className='flex items-center overflow-hidden'>
+              <div className='text-gray-700 text-sm font-medium font-creato-medium leading-normal tracking-wide whitespace-nowrap overflow-hidden text-ellipsis'>
+                Want some scripts? (10⭐️)
+              </div>
+            </div>
+            <div className='w-5 h-5 relative'>
+              <ScriptsIcon />
+            </div>
+          </div>
+          <ExportToPdfButton finalSlides={finalSlides} />
+          <ShareToggleButton setShare={setShare} share={share} />
+        </div>
 
-                {/* shareable link */}
-                {share && (
-                    <div className='flex-grow'>
-                        <label className='text-sm text-zinc-100'>View only link:</label>
-                        <ClickableLink
-                            link={`${host}/shared/${sessionStorage.getItem('project_id')}`}
-                        />
-                    </div>
-                )}
-                {/* slides contents */}
-                <SlidesHTML finalSlides={finalSlides} setFinalSlides={setFinalSlides} />
+        {/* shareable link */}
+        {share && (
+          <div className='w-fit flex-grow'>
+            <label className='text-sm text-zinc-100'>View only link:</label>
+            <ClickableLink
+              link={`${host}/shared/${sessionStorage.getItem('project_id')}`}
+            />
+          </div>
+        )}
+        {/* slides contents */}
+        <SlidesHTML finalSlides={finalSlides} setFinalSlides={setFinalSlides} />
 
-                {/* Form */}
-                {/* <TranscriptForm
+        {/* Form */}
+        {/* <TranscriptForm
           isSubmitting={isSubmitting}
           setIsSubmitting={setIsSubmitting}
           finalSlides={finalSlides}
         /> */}
 
-                {/* Timer */}
-                {/* <Timer expectedSeconds={60} isSubmitting={isSubmitting} /> */}
-            </div>
-        </div>
-    )
+        {/* Timer */}
+        {/* <Timer expectedSeconds={60} isSubmitting={isSubmitting} /> */}
+      </div>
+    </div>
+  )
 }
 
 export default SlideVisualizer
--- tailwindui/components/slides/exportToPdfButton.tsx
+++ tailwindui/components/slides/exportToPdfButton.tsx
@@ -10,154 +10,153 @@ import mixpanel from 'mixpanel-browser'
 import { DownloadIcon } from '@/app/(feature)/icons'
 import SlideContainer from './SlideContainer'
 import { templateDispatch } from './templateDispatch'
-import generatePDF, { Resolution, Margin, Options } from 'react-to-pdf';
+import generatePDF, { Resolution, Margin, Options } from 'react-to-pdf'
 
 type SlidesHTMLProps = {
-    finalSlides: Slide[]
-    setFinalSlides: React.Dispatch<React.SetStateAction<Slide[]>>
+  finalSlides: Slide[]
+  setFinalSlides: React.Dispatch<React.SetStateAction<Slide[]>>
 }
 
 interface ExportToPdfProps {
-    finalSlides: Slide[]
-    //setFinalSlides: React.Dispatch<React.SetStateAction<Slide[]>>;
+  finalSlides: Slide[]
+  //setFinalSlides: React.Dispatch<React.SetStateAction<Slide[]>>;
 }
 
 const ExportToPdfButton: React.FC<ExportToPdfProps> = ({ finalSlides }) => {
-    const topic = typeof sessionStorage !== 'undefined' ? sessionStorage.getItem('topic') : '';
-    const [user, setUser] = useState(null)
-    const [downloadingPDF, setDownloadingPDF] = useState(false)
-    const [showPaymentModal, setShowPaymentModal] = useState(false)
-    const exportSlidesRef = useRef<HTMLDivElement>(null);
-    const containerRef = useRef<HTMLDivElement>(null);
-    const slideRef = useRef<HTMLDivElement>(null);
+  const topic =
+    typeof sessionStorage !== 'undefined' ? sessionStorage.getItem('topic') : ''
+  const [user, setUser] = useState(null)
+  const [downloadingPDF, setDownloadingPDF] = useState(false)
+  const [showPaymentModal, setShowPaymentModal] = useState(false)
+  const exportSlidesRef = useRef<HTMLDivElement>(null)
+  const containerRef = useRef<HTMLDivElement>(null)
+  const slideRef = useRef<HTMLDivElement>(null)
 
-    const exportOptions: Options = {
-        filename: (topic ? topic : 'drlambda') + '.pdf',
-        method: "save",
-        resolution: Resolution.MEDIUM,
-        page: {
-            margin: Margin.NONE,
-            format: [254, 143], // 960x540 px in mm
-            orientation: "landscape"
-        },
-        canvas: {
-            mimeType: "image/jpeg",
-            qualityRatio: 1
-        },
-        overrides: {
-            pdf: {
-                compress: true
-            },
-            canvas: {
-                useCORS: true,
-            }
-        }
-    };
+  const exportOptions: Options = {
+    filename: (topic ? topic : 'drlambda') + '.pdf',
+    method: 'save',
+    resolution: Resolution.MEDIUM,
+    page: {
+      margin: Margin.NONE,
+      format: [254, 143], // 960x540 px in mm
+      orientation: 'landscape',
+    },
+    canvas: {
+      mimeType: 'image/jpeg',
+      qualityRatio: 1,
+    },
+    overrides: {
+      pdf: {
+        compress: true,
+      },
+      canvas: {
+        useCORS: true,
+      },
+    },
+  }
 
-    useEffect(() => {
-        // Create a scoped async function within the hook.
-        const fetchUser = async () => {
-            const user = await AuthService.getCurrentUser()
-            if (user) {
-                setUser(user)
-            }
-        }
-        // Execute the created function directly
-        fetchUser()
-    }, [])
-
-
-    function exportToPdf() {
-        generatePDF(exportSlidesRef, exportOptions);
+  useEffect(() => {
+    // Create a scoped async function within the hook.
+    const fetchUser = async () => {
+      const user = await AuthService.getCurrentUser()
+      if (user) {
+        setUser(user)
+      }
     }
+    // Execute the created function directly
+    fetchUser()
+  }, [])
 
-    const handleSavePDF = async () => {
-        setDownloadingPDF(true)
-        const element = document.getElementById('pdf-content')
+  function exportToPdf() {
+    generatePDF(exportSlidesRef, exportOptions)
+  }
 
-        try {
-            const { userId, idToken } = await AuthService.getCurrentUserTokenAndId();
+  const handleSavePDF = async () => {
+    setDownloadingPDF(true)
+    const element = document.getElementById('pdf-content')
 
-            mixpanel.track('PDF Downloaded', {
-                'Project ID': sessionStorage.getItem('project_id'),
-            });
+    try {
+      const { userId, idToken } = await AuthService.getCurrentUserTokenAndId()
 
-            const response = await fetch('/api/save_final_html_pdf', {
-                method: 'POST',
-                headers: {
-                    'Authorization': `Bearer ${idToken}`,
-                    'Content-Type': 'application/json'
-                },
-            });
+      mixpanel.track('PDF Downloaded', {
+        'Project ID': sessionStorage.getItem('project_id'),
+      })
 
-            if (response.ok && (typeof window !== "undefined")) {
-                exportToPdf();  
-            } else if (response.status === 402) {
-                setShowPaymentModal(true);
-            } else {
-                console.error('Failed to save PDF.');
-            }
-        } catch (error) {
-            console.error('An error occurred:', error)
-        }
-        setDownloadingPDF(false)
+      const response = await fetch('/api/save_final_html_pdf', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+          'Content-Type': 'application/json',
+        },
+      })
+
+      if (response.ok && typeof window !== 'undefined') {
+        exportToPdf()
+      } else if (response.status === 402) {
+        setShowPaymentModal(true)
+      } else {
+        console.error('Failed to save PDF.')
+      }
+    } catch (error) {
+      console.error('An error occurred:', error)
     }
+    setDownloadingPDF(false)
+  }
 
-    return (
-        <div className='flex flex-wrap'>
-            <div className='w-full px-3'>
-                {showPaymentModal && (
-                    <PaywallModal
-                        setShowModal={setShowPaymentModal}
-                        message='Upgrade for more ⭐️credits.'
-                    />
-                )}
+  return (
+    <div className='flex flex-wrap flex-grow-0'>
+      <div className='px-3'>
+        {showPaymentModal && (
+          <PaywallModal
+            setShowModal={setShowPaymentModal}
+            message='Upgrade for more ⭐️credits.'
+          />
+        )}
 
-                {!user ? (
-                    // insert here
-                    <ExportToPDFModal />
-                ) : (
-                    <div
-                        className='h-8 px-3 py-1 bg-zinc-100 rounded-lg justify-center items-center gap-2.5 inline-flex cursor-pointer'
-                        onClick={handleSavePDF}
-                    >
-                        <div className='text-center text-gray-700 text-sm font-medium font-creato-medium leading-normal tracking-wide'>
-                            Export to PDF
-                        </div>
-                        <div className='w-4 h-4 relative' hidden={downloadingPDF}>
-                            <DownloadIcon />
-                        </div>
-                        <div className='text-black h-[22px] mr-2' hidden={!downloadingPDF}>
-                            <LoadingIcon />
-                        </div>
-                    </div>
-                )}
+        {!user ? (
+          // insert here
+          <ExportToPDFModal />
+        ) : (
+          <div
+            className='h-8 px-3 py-1 bg-zinc-100 rounded-lg justify-center items-center gap-2.5 inline-flex cursor-pointer'
+            onClick={handleSavePDF}
+          >
+            <div className='text-center text-gray-700 text-sm font-medium font-creato-medium leading-normal tracking-wide'>
+              Export to PDF
             </div>
-
-            {/* hidden div for export to pdf */}
-            <div style={{ overflow: 'hidden', height: 0 }} >
-                <div
-                    ref={exportSlidesRef}>
-                    {/* Render all of your slides here. This can be a map of your slides array */}
-                    {finalSlides.map((slide, index) => (
-                        <div key={index} style={{ pageBreakAfter: 'always' }}>
-                            <SlideContainer
-                                slides={finalSlides}
-                                currentSlideIndex={index}
-                                isViewing={false}
-                                isPresenting={false}
-                                scale={1}
-                                templateDispatch={templateDispatch}
-                                containerRef={containerRef}
-                                slideRef={slideRef}
-                                exportToPdfMode={true}
-                            />
-                        </div>
-                    ))}
-                </div>
+            <div className='w-4 h-4 relative' hidden={downloadingPDF}>
+              <DownloadIcon />
             </div>
+            <div className='text-black h-[22px] mr-2' hidden={!downloadingPDF}>
+              <LoadingIcon />
+            </div>
+          </div>
+        )}
+      </div>
+
+      {/* hidden div for export to pdf */}
+      <div style={{ overflow: 'hidden', height: 0, width: 0 }}>
+        <div ref={exportSlidesRef}>
+          {/* Render all of your slides here. This can be a map of your slides array */}
+          {finalSlides.map((slide, index) => (
+            <div key={index} style={{ pageBreakAfter: 'always' }}>
+              <SlideContainer
+                slides={finalSlides}
+                currentSlideIndex={index}
+                isViewing={false}
+                isPresenting={false}
+                scale={1}
+                templateDispatch={templateDispatch}
+                containerRef={containerRef}
+                slideRef={slideRef}
+                exportToPdfMode={true}
+              />
+            </div>
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default ExportToPdfButton
