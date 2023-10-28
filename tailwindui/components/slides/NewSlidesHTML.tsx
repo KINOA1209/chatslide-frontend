c74be10058ed18c372acdd72dc6df02c51dc498e
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-10-27 19:46:06 -0700

    [slides] fix window is not defined

--- tailwindui/components/slides/NewSlidesHTML.tsx
+++ tailwindui/components/slides/NewSlidesHTML.tsx
@@ -83,8 +83,8 @@ const SlidesHTML: React.FC<SlidesHTMLProps> = ({
     const allSlidesRef = useRef<HTMLDivElement>(null)
     const [saveStatus, setSaveStatus] = useState('Up to date')
     const [dimensions, setDimensions] = useState({
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width: window ? window.innerWidth : 960,
+        height: window ? window.innerHeight : 540,
       });
     const [unsavedChanges, setUnsavedChanges] = useState(false)
     const isFirstRender = useRef(true)
