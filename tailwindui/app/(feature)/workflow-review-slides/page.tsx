0e7ca4785e6c454347cd2785bf33e97a1419d75a
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-10-27 21:34:59 -0700

    [slide] export to pdf button fix

--- tailwindui/app/(feature)/workflow-review-slides/page.tsx
+++ tailwindui/app/(feature)/workflow-review-slides/page.tsx
@@ -110,8 +110,8 @@ export default function WorkflowStep3() {
         </div>
 
         <div className='flex-auto text-center self-center text-neutral-900 font-medium font-creato-medium leading-snug tracking-tight whitespace-nowrap sm:hidden'>
-            Use our desktop version to use all the functionalities!
-          </div>
+            Use our desktop version to see all the functionalities!
+        </div>
       </div>
       {/* <div className="pt-32 max-w-3xl mx-auto text-center pb-12 md:pb-20">
                 <h1 className="h1">Review Slides</h1>
