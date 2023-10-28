c7ded8a1919c4b8ed6084b59fcf8f4bf6db9bae3
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-10-27 21:07:57 -0700

    [feature layout] change bg color to bg-[#F4F4F4]

--- tailwindui/app/(feature)/layout.tsx
+++ tailwindui/app/(feature)/layout.tsx
@@ -14,11 +14,7 @@ export default function WorkflowLayout({
   return (
     <main className='flex flex-col min-h-[100vh]'>
       <div
-        className='w-[100vw] h-[100vh] -z-1 fixed'
-        style={{
-          background:
-            'linear-gradient(169deg, rgba(237,113,206,0.602) 0%, rgba(199,157,223,0.566) 13%, rgba(141,165,225,0.397) 40%, rgba(42,136,237,0.221) 50%, rgba(255,255,255,1) 77%, rgba(255,255,255,1) 100%)',
-        }}
+        className='w-[100vw] h-[100vh] -z-1 fixed bg-[#F4F4F4]'
       ></div>
       <Header loginRequired={true} isLanding={false} refList={[]} />
       <section className='grow flex flex-col'>
