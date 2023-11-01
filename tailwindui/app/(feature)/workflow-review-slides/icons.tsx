a3a6805a58045427d7efa9efd4d89b534ad2328e
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-10-31 22:43:29 -0700

    bugfix for stroke-width and other svg labels

--- tailwindui/app/(feature)/workflow-review-slides/icons.tsx
+++ tailwindui/app/(feature)/workflow-review-slides/icons.tsx
@@ -9,8 +9,8 @@ export const LeftSlideNavIcon = () => (
     <path
       d='M12 2L1.97086 14.7644C1.40104 15.4896 1.40104 16.5104 1.97086 17.2356L12 30'
       stroke='#d3d3d3'
-      stroke-width='3'
-      stroke-linecap='round'
+      strokeWidth='3'
+      strokeLinecap='round'
     />
   </svg>
 )
@@ -27,8 +27,8 @@ export const RightSlideNavIcon = () => (
       id='Vector 22'
       d='M2 2L12.0291 14.7644C12.599 15.4896 12.599 16.5104 12.0291 17.2356L2 30'
       stroke='#d3d3d3'
-      stroke-width='3'
-      stroke-linecap='round'
+      strokeWidth='3'
+      strokeLinecap='round'
     />
   </svg>
 )
@@ -41,34 +41,34 @@ export const ShareSlidesIcon = () => (
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
   >
-    <g clip-path='url(#clip0_1431_28572)'>
+    <g clipPath='url(#clip0_1431_28572)'>
       <path
         d='M3.14369 10.5725C4.56385 10.5725 5.71512 9.42128 5.71512 8.00112C5.71512 6.58096 4.56385 5.42969 3.14369 5.42969C1.72353 5.42969 0.572266 6.58096 0.572266 8.00112C0.572266 9.42128 1.72353 10.5725 3.14369 10.5725Z'
         stroke='#1D222A'
-        stroke-width='1.25'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.25'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M12.8585 15.43C14.2787 15.43 15.43 14.2787 15.43 12.8585C15.43 11.4384 14.2787 10.2871 12.8585 10.2871C11.4384 10.2871 10.2871 11.4384 10.2871 12.8585C10.2871 14.2787 11.4384 15.43 12.8585 15.43Z'
         stroke='#1D222A'
-        stroke-width='1.25'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.25'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M12.8585 5.71512C14.2787 5.71512 15.43 4.56385 15.43 3.14369C15.43 1.72353 14.2787 0.572266 12.8585 0.572266C11.4384 0.572266 10.2871 1.72353 10.2871 3.14369C10.2871 4.56385 11.4384 5.71512 12.8585 5.71512Z'
         stroke='#1D222A'
-        stroke-width='1.25'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.25'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M5.44141 6.85854L10.5614 4.28711M5.44141 9.14425L10.5614 11.7157'
         stroke='#1D222A'
-        stroke-width='1.25'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='1.25'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </g>
     <defs>
