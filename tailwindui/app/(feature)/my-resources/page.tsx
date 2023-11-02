51334acf90074df770d8f52b9213f635a52cd30b
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-11-02 02:56:57 -0700

    my project page and my resource page design

--- tailwindui/app/(feature)/my-resources/page.tsx
+++ tailwindui/app/(feature)/my-resources/page.tsx
@@ -1,10 +1,18 @@
-import NewMyFiles from '@/components/fileManagement'
-
+import NewMyFiles from '@/components/newFileManagement'
+import { FileUploadButton } from '@/components/fileUpload'
+import { CarbonConnect, IntegrationName } from 'carbon-connect'
+import { Transition } from '@headlessui/react'
+import MyResourcePageHeader from './MyResourcePageHeader'
 export const metadata = {
   title: 'Resources - DrLambda',
   description: 'Convert your documents to slides',
 }
 
 export default function FileManagement() {
-  return <NewMyFiles selectable={false} />
+  return (
+    <>
+      <MyResourcePageHeader />
+      <NewMyFiles selectable={false} />
+    </>
+  )
 }
