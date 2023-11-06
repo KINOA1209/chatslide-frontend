b106a461eb0030a9f23fa6ebadfe4a6a9858f926
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-11-06 12:27:09 -0800

    Revert "start script page"
    This reverts commit 859ca02eeed570a733d1ab3260cce202e9c990d9.

--- tailwindui/components/slides/NewSlideVisualizer.tsx
+++ tailwindui/components/slides/NewSlideVisualizer.tsx
@@ -8,89 +8,87 @@ import { ScriptsIcon } from '@/app/(feature)/icons'
 import { ShareSlidesIcon } from '@/app/(feature)/workflow-review-slides/icons'
 import { ShareToggleButton } from '@/components/slides/SlideButtons'
 import UserService from '../utils/UserService'
-import TranscriptForm from '@/components/forms/transcriptForm'
-import Timer from '@/components/ui/Timer'
 
-const SlidesHTML = dynamic(() => import('@/components/slides/NewSlidesHTML'), {
-  ssr: false,
-})
+
+const SlidesHTML = dynamic(() => import('@/components/slides/NewSlidesHTML'), { ssr: false })
+
 
 const SlideVisualizer = () => {
-  const [host, setHost] = useState('https://drlambda.ai')
-  const [isSubmitting, setIsSubmitting] = useState(false)
-  const [showPopup, setShowPopup] = useState<boolean>(false)
+    const [host, setHost] = useState('https://drlambda.ai')
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [showPopup, setShowPopup] = useState<boolean>(false)
 
-  const [finalSlides, setFinalSlides] = useState<Slide[]>([])
-  const [share, setShare] = useState(false)
-  const [isPaidUser, setIsPaidUser] = useState(false)
+    const [finalSlides, setFinalSlides] = useState<Slide[]>([])
+    const [share, setShare] = useState(false)
+    const [isPaidUser, setIsPaidUser] = useState(false);
 
-  useEffect(() => {
-    ;(async () => {
-      const isPaidUser = await UserService.isPaidUser()
-      setIsPaidUser(isPaidUser)
-    })()
-  }, [])
+    useEffect(() => {
+        (async () => {
+            const isPaidUser = await UserService.isPaidUser();
+            setIsPaidUser(isPaidUser);
+        })();
+    }, []);
 
-  useEffect(() => {
-    setShare(sessionStorage.getItem('is_shared') === 'true')
-    // console.log('share', sessionStorage.getItem('is_shared'));
-  }, [])
+    useEffect(() => {
+        setShare(sessionStorage.getItem('is_shared') === 'true')
+        // console.log('share', sessionStorage.getItem('is_shared'));
+    }, [])
 
-  useEffect(() => {
-    if (
-      window.location.hostname !== 'localhost' &&
-      window.location.hostname !== '127.0.0.1'
-    ) {
-      setHost('https://' + window.location.hostname)
-    } else {
-      setHost(window.location.hostname)
-    }
-  }, [])
+    useEffect(() => {
+        if (
+            window.location.hostname !== 'localhost' &&
+            window.location.hostname !== '127.0.0.1'
+        ) {
+            setHost('https://' + window.location.hostname)
+        } else {
+            setHost(window.location.hostname)
+        }
+    }, [])
 
-  return (
-    <div>
-      <div className='px-4 sm:px-6 flex flex-col justify-center items-center gap-4'>
-        {/* buttons: export and scripts and share slides */}
-        <div className='flex flex-row justify-end items-center'>
-          {/* want some scripts? */}
-          <div className='h-8 px-3 py-1 bg-zinc-100 rounded-lg flex justify-center items-center gap-2.5 cursor-pointer'>
-            <div className='flex items-center overflow-hidden'>
-              <div className='text-gray-700 text-sm font-medium font-creato-medium leading-normal tracking-wide whitespace-nowrap overflow-hidden text-ellipsis'>
-                {isPaidUser ? '🚀' : '🔒'} Want some scripts?
-              </div>
-            </div>
-            <div className='w-5 h-5 relative'>
-              <ScriptsIcon />
-            </div>
-          </div>
-          <ExportToPdfButton finalSlides={finalSlides} />
-          <ShareToggleButton setShare={setShare} share={share} />
-        </div>
+    return (
+        <div>
+            <div className='px-4 sm:px-6 flex flex-col justify-center items-center gap-4'>
+                {/* buttons: export and scripts and share slides */}
+                <div className='flex flex-row justify-end items-center'>
+                    {/* want some scripts? */}
+                    {/* <div className='h-8 px-3 py-1 bg-zinc-100 rounded-lg flex justify-center items-center gap-2.5 cursor-pointer'>
+                        <div className='flex items-center overflow-hidden'>
+                            <div className='text-gray-700 text-sm font-medium font-creato-medium leading-normal tracking-wide whitespace-nowrap overflow-hidden text-ellipsis'>
+                                { isPaidUser ? '🚀' : '🔒' } Want some scripts?
+                            </div>
+                        </div>
+                        <div className='w-5 h-5 relative'>
+                            <ScriptsIcon />
+                        </div>
+                    </div> */}
+                    <ExportToPdfButton finalSlides={finalSlides} />
+                    <ShareToggleButton setShare={setShare} share={share} />
+                </div>
 
-        {/* shareable link */}
-        {share && (
-          <div className='w-fit flex-grow'>
-            <label className='text-sm text-zinc-100'>View only link:</label>
-            <ClickableLink
-              link={`${host}/shared/${sessionStorage.getItem('project_id')}`}
-            />
-          </div>
-        )}
-        {/* slides contents */}
-        <SlidesHTML finalSlides={finalSlides} setFinalSlides={setFinalSlides} />
+                {/* shareable link */}
+                {share && (
+                    <div className='w-fit flex-grow'>
+                        <label className='text-sm text-zinc-100'>View only link:</label>
+                        <ClickableLink
+                            link={`${host}/shared/${sessionStorage.getItem('project_id')}`}
+                        />
+                    </div>
+                )}
+                {/* slides contents */}
+                <SlidesHTML finalSlides={finalSlides} setFinalSlides={setFinalSlides} />
 
-        {/* Form */}
-        <TranscriptForm
+                {/* Form */}
+                {/* <TranscriptForm
           isSubmitting={isSubmitting}
           setIsSubmitting={setIsSubmitting}
           finalSlides={finalSlides}
-        />
+        /> */}
 
-        {/* Timer */}
-        <Timer expectedSeconds={60} isSubmitting={isSubmitting} />
-      </div>
-    </div>
-  )
+                {/* Timer */}
+                {/* <Timer expectedSeconds={60} isSubmitting={isSubmitting} /> */}
+            </div>
+        </div>
+    )
 }
 
 export default SlideVisualizer
