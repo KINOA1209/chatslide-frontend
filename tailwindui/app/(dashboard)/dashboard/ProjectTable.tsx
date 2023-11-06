1c7c2960e099cb397f5fdcf8e3489c0e820cf8e4
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-11-05 22:25:29 -0800

    resolve conflict

--- tailwindui/app/(dashboard)/dashboard/ProjectTable.tsx
+++ tailwindui/app/(dashboard)/dashboard/ProjectTable.tsx
@@ -67,33 +67,23 @@ const ProjectTable: React.FC<Props> = ({
   onProjectClick,
   onDelete,
 }) => {
-  const [expandedProject, setExpandedProject] = useState<number | null>(null)
-
-  const toggleExpand = (index: number) => {
-    if (expandedProject === index) {
-      setExpandedProject(null)
-    } else {
-      setExpandedProject(index)
-    }
-  }
 
   return (
-    <>
-      {' '}
+    <div className="w-2/3 mx-auto">
       <div
         className='grid bg-[#ECF1FE] border border-gray-200 grid-cols-2 md:grid-cols-4'
       >
         <div className='hidden md:flex items-center justify-center w-full text-indigo-300 text-[13px] font-bold font-creato-medium uppercase leading-normal tracking-wide'>
-          Task
+          Type
         </div>
         <div className='flex items-center justify-center w-full text-indigo-300 text-[13px] font-bold font-creato-medium uppercase leading-normal tracking-wide'>
-          Project
+          Topic
         </div>
         <div className='hidden md:flex items-center justify-center w-full text-indigo-300 text-[13px] font-bold font-creato-medium uppercase leading-normal tracking-wide'>
           Resources
         </div>
         <div className='hidden md:flex items-center justify-center w-full text-indigo-300 text-[13px] font-bold font-creato-medium uppercase leading-normal tracking-wide'>
-          Date
+          Created Date
         </div>
       </div>
       <div
@@ -107,7 +97,7 @@ const ProjectTable: React.FC<Props> = ({
               <span className='ml-1'>{project.task}</span>
             </div>
             <div
-              className='p-2 flex cursor-pointer items-center text-start border-b-2 text-ellipsis overflow-hidden text-gray-600 text-[17px] italic font-creato-medium leading-normal tracking-wide'
+              className='p-2 flex cursor-pointer items-center text-start border-b-2 text-ellipsis overflow-hidden text-gray-600 text-[17px] font-bold font-creato-medium leading-normal tracking-wide'
               onClick={() => onProjectClick(project.id)}
             >
               {project.name}
@@ -115,15 +105,15 @@ const ProjectTable: React.FC<Props> = ({
             <div className='p-[2rem] border-b-2 hidden md:flex items-center justify-center text-gray-600 text-[17px] font-normal font-creato-medium leading-normal tracking-wide'>
               <div className='flex items-center justify-center'>
                 <FileIcon fileType='pdf' />
-                <span className='ml-1'>
-                  {project.resources.length} Resources
-                </span>
-                <button
-                  onClick={() => toggleExpand(index)}
-                  className='ml-2 p-1 border rounded cursor-pointer'
-                >
-                  {expandedProject === index ? <FaArrowDown /> : <FaArrowUp />}
-                </button>
+                {project.resources.map((resource, resourceIndex) => (
+                  <div
+                    key={resourceIndex}
+                    className='flex items-center justify-center gap-[0.5rem] bg-gray-100 p-1 m-1 rounded'
+                  >
+                    <FileIcon fileType={resource.type} />
+                    {resource.name}
+                  </div>
+                ))}
               </div>
             </div>
             <div className='p-2 border-b-2 flex items-center justify-center'>
@@ -137,23 +127,10 @@ const ProjectTable: React.FC<Props> = ({
                 </div>
               </div>
             </div>
-            {expandedProject === index && (
-              <div className='mt-2 flex flex-wrap col-start-2 col-span-3 border-b-2 items-center text-gray-600 text-[13px] font-normal font-creato-medium leading-normal tracking-wide'>
-                {project.resources.map((resource, resourceIndex) => (
-                  <div
-                    key={resourceIndex}
-                    className='flex items-center justify-center gap-[0.5rem] bg-gray-100 p-1 m-1 rounded'
-                  >
-                    <FileIcon fileType={resource.type} />
-                    {resource.name}
-                  </div>
-                ))}
-              </div>
-            )}
           </React.Fragment>
         ))}
       </div>
-    </>
+    </div>
   )
 }
 
