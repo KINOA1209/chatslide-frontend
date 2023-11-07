84cb88f24b4998a4b1ca352831abc3a1bff01467
Hao Yang <hyang800@usc.edu>
2023-11-06 19:14:49 -0800

    [resource][topic][outline]several bugs fix
    resource page:
    1. fix the bug when user uploading files from local, the page wont be updated
    topic page:
    1. fix the bug the files uploaded from local will show as unknown file.
    outline page:
    1. fix the bug the position of generate button will change after click it.

---
+++ tailwindui/app/(feature)/my-resources/page_client.tsx
@@ -0,0 +1,21 @@
+'use client'
+import MyFiles from '@/components/newFileManagement'
+import React, { useState, useEffect} from 'react'
+import MyResourcePageHeader from '@/app/(feature)/my-resources/MyResourcePageHeader'
+
+export default function FileManagementClient() {
+    // State to trigger update
+    const [filesUpdated, setFilesUpdated] = useState(false);
+
+    // Callback function to be called after files are uploaded
+    const handleFilesUploaded = () => {
+        setFilesUpdated(true);
+    };
+
+    return (
+        <div className= 'h-screen'>
+            <MyResourcePageHeader onFilesUploaded={handleFilesUploaded}/>
+            <MyFiles selectable={false} filesUpdated={filesUpdated} setFilesUpdated={setFilesUpdated}/>
+        </div>
+    )
+}
