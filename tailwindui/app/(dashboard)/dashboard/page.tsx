c838b97045ed76dd6fbd2d454c3d82655f259cda
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-11-09 18:16:47 -0800

    [resources] change button

--- tailwindui/app/(dashboard)/dashboard/page.tsx
+++ tailwindui/app/(dashboard)/dashboard/page.tsx
@@ -202,7 +202,7 @@ export default function Dashboard() {
   return (
     <section className='grow flex flex-col h-full'>
       <ToastContainer />
-      {/* top background container of my projects title text and  */}
+      {/* top background container of my projects title text and button */}
       <div className='mt-[3rem] flex items-end w-full bg-Grey-50 z-10 pt-[4rem] border-b-2 px-[5rem]'>
         {/* flex container controlling max width */}
         <div className='w-full max-w-7xl flex flex-wrap items-center justify-center lg:items-end lg:justify-between '>
