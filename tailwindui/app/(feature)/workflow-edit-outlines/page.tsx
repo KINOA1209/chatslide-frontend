9cfd336bc2dda2fa9f9db3e03e66b8cffc49f0ed
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-11-09 16:01:08 -0800

    [outline] refactor GPTToggleWithExplanation, add WorkflowStepsBanner to outline

--- tailwindui/app/(feature)/workflow-edit-outlines/page.tsx
+++ tailwindui/app/(feature)/workflow-edit-outlines/page.tsx
@@ -18,6 +18,7 @@ import {
 } from '../icons'
 import NewWorkflowGPTToggle from '@/components/button/NewWorkflowGPTToggle'
 import { useRouter } from 'next/navigation'
+import WorkflowStepsBanner from '@/components/WorkflowStepsBanner'
 
 interface OutlineSection {
   title: string
@@ -41,6 +42,7 @@ export default function WorkflowStep2() {
     null
   )
   const [isGpt35, setIsGpt35] = useState(true)
+  const [isSubmitting, setIsSubmitting] = useState(false)
   useEffect(() => {
     if (outlineRes) {
       const newOutlineContent = Object.keys(outlineRes).map((key) => {
@@ -90,99 +92,21 @@ export default function WorkflowStep2() {
   return (
     <div className=' bg-zinc-100'>
       {/* flex col container for steps, title, generate slides button etc */}
-      <div className='fixed mt-[3rem] flex flex-col w-full bg-Grey-50 justify-center z-10 gap-1 py-[0.75rem] border-b-2'>
-        {/* steps bar */}
-        <div className='self-center'>
-          <ProjectProgress currentInd={1} contentRef={contentRef} />
-        </div>
+      <WorkflowStepsBanner 
+        currentIndex={1} 
+        isSubmitting={isSubmitting} 
+        setIsSubmitting={setIsSubmitting} 
+        isPaidUser={true} 
+        contentRef={contentRef} 
+        nextIsPaidFeature={false} 
+        showGPTToggle={true} 
+        setIsGpt35={setIsGpt35} 
+        nextText={!isSubmitting? 'Next' : 'Creating Slides'}
+      />
 
-        {/* gpt model switch area */}
-        <div className='self-end mx-[5rem] flex flex-row gap-4 cursor-pointer'>
-          <NewWorkflowGPTToggle setIsGpt35={setIsGpt35} />
-          {/* <NewWorkflowGPTToggle isGpt35={isGpt35} setIsGpt35={setIsGpt35} /> */}
-          <div className='cursor-pointer' onClick={openPopup}>
-            <QuestionExplainIcon />
-          </div>
-        </div>
-
-        {/* Popup for explaining model difference */}
-        {showPopup && (
-          <div className='fixed top-[15%] left-[70%] w-[27rem] h-48 bg-gradient-to-l from-gray-950 to-slate-600 rounded-md shadow backdrop-blur-2xl flex flex-col'>
-            {/* Popup content */}
-            {/* You can add content, explanations, and close button here */}
-            <div
-              onClick={closePopup}
-              className='text-gray-50 cursor-pointer self-end px-4 py-2'
-            >
-              Close
-            </div>
-            {/* columns for two models */}
-            <div className='grid grid-cols-2 gap-8'>
-              <div className='flex flex-col justify-center items-center border-r-2 border-black/25'>
-                <div className='w-32 h-10 text-center mb-4'>
-                  <span className='text-zinc-100 text-2xl font-normal font-creato-medium leading-loose tracking-wide'>
-                    GPT
-                  </span>
-                  <span className='text-zinc-100 text-2xl font-bold font-creato-medium leading-loose tracking-wide'>
-                    {' '}
-                    3.5
-                  </span>
-                </div>
-                <div className="w-40 h-12 text-center text-neutral-300 text-xs font-normal font-['Creato Display'] leading-tight tracking-tight mb-[1.5rem]">
-                  Fast generation, great for small projects.
-                </div>
-                <div className="w-40 h-5 text-center text-zinc-100 text-xs font-medium font-['Creato Display'] leading-tight tracking-tight">
-                  Available to All Users
-                </div>
-              </div>
-              <div className='flex flex-col justify-center items-center'>
-                <div className='w-32 h-10 text-center mb-4'>
-                  <span className='text-zinc-100 text-2xl font-normal font-creato-medium leading-loose tracking-wide'>
-                    GPT
-                  </span>
-                  <span className='text-zinc-100 text-2xl font-bold font-creato-medium leading-loose tracking-wide'>
-                    {' '}
-                  </span>
-                  <span className='text-violet-500 text-2xl font-bold font-creato-medium leading-loose tracking-wide'>
-                    4.0
-                  </span>
-                </div>
-                <div className='w-40 h-12 text-center text-neutral-300 text-xs font-normal font-creato-medium leading-tight tracking-tight mb-[1.5rem]'>
-                  Master of deep & complex subjects.
-                </div>
-                <div className='w-40 h-5 text-center text-zinc-100 text-xs font-medium font-creato-medium leading-tight tracking-tight'>
-                  Exclusive to Plus & Pro Users
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
-
-        {/* flex row container for backlink, title and generate button */}
-        <div className='flex justify-between items-center mx-[5rem]'>
-          <div
-            className='flex-row justify-center items-center gap-4 cursor-pointer hidden sm:flex'
-            onClick={() => router.push('/workflow-generate-outlines')}
-          >
-            <LeftTurnArrowIcon></LeftTurnArrowIcon>
-            <div className='text-center text-gray-700 text-sm font-medium font-creato-medium leading-normal tracking-[0.035rem] whitespace-nowrap'>
-              Back to Summary
-            </div>
-          </div>
-
-          <div className='w-[50rem] h-9 text-center text-neutral-900 text-xl font-medium font-creato-medium leading-snug tracking-tight whitespace-nowrap hidden sm:block'>
-            Please review or edit the outlined sections and topics.
-          </div>
-
-          {outlineContent && (
-            <GenerateSlidesSubmit outline={outlineContent} isGPT35={isGpt35} />
-          )}
-        </div>
-
-        <div className='flex-auto text-center self-center text-neutral-900 font-medium font-creato-medium leading-snug tracking-tight whitespace-nowrap sm:hidden'>
-          Use our desktop version to see all the functionalities!
-        </div>
-      </div>
+      {outlineContent && (
+        <GenerateSlidesSubmit outline={outlineContent} isGPT35={isGpt35} isSubmitting={isSubmitting} setIsSubmitting={setIsSubmitting}/>
+      )}
 
       <div className='mt-[12rem] mb-[3rem]'>
         {/* overview nav section */}
