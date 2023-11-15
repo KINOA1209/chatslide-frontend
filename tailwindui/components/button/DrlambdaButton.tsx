22eda29a4db4ee767ca0cdd2f1a85bbb6a5eeb91
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-11-14 22:44:44 -0800

    [banner] fix responsiveness

--- tailwindui/components/button/DrlambdaButton.tsx
+++ tailwindui/components/button/DrlambdaButton.tsx
@@ -42,7 +42,7 @@ const DrlambdaButton: React.FC<DrlambdaButtonProps> = ({ children, onClick, isSu
         id='generate_button'
         disabled={isSubmitting}
         onClick={checkPaidUser}
-        className={`w-[12rem] h-[36px] px-5 ${getButtonBg()} rounded-[15px] flex justify-center items-center gap-2 mx-auto cursor-pointer lg:mr-[1%] }`}
+        className={`w-[8rem] lg:w-[12rem] h-[36px] ${getButtonBg()} rounded-[15px] flex justify-center items-center gap-2 cursor-pointer }`}
       >
         {isSubmitting && <SpinIcon />}
         <span className='text-white font-semibold tracking-tight whitespace-nowrap'>
