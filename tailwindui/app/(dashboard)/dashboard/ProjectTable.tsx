762c521d943d122ead8db01a9ac2de475c9a6067
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-11-16 13:00:28 -0800

    [file] unify icon display

--- tailwindui/app/(dashboard)/dashboard/ProjectTable.tsx
+++ tailwindui/app/(dashboard)/dashboard/ProjectTable.tsx
@@ -10,6 +10,7 @@ import {
   FaFilePowerpoint,
   FaFileWord,
   FaYoutube,
+  FaFileImage,
 } from 'react-icons/fa'
 import { DeleteIcon } from '@/app/(feature)/icons';
 
@@ -52,12 +53,24 @@ const TaskIcon: React.FC<{ task: 'video' | 'scripts' | 'slides' | 'presentation'
   }
 }
 
-const FileIcon: React.FC<{ fileType: string }> = ({ fileType }) => {
+export const FileIcon: React.FC<{ fileType: string }> = ({ fileType }) => {
+  fileType = fileType.toLowerCase()
   switch (fileType) {
     case 'doc':
-      return <FaFilePdf size={'1em'}/>
+      return <FaFilePdf size='32px'/>
     case 'url':
-      return <FaYoutube size={'2em'}/>
+      return <FaYoutube size='32px'/>
+    case 'youtube':
+      return <FaYoutube size='32px'/>
+    case 'pdf':
+      return <FaFilePdf size='32px'/>
+    case 'docx':
+      return <FaFileWord size='32px'/>
+    case 'jpg':
+    case 'jpeg':
+    case 'png':
+    case 'gif':
+      return <FaFileImage size='32px'/>
     default:
       return null
   }
@@ -74,16 +87,16 @@ const ProjectTable: React.FC<Props> = ({
       <div
         className='grid bg-[#ECF1FE] border border-gray-200 grid-cols-3 md:grid-cols-5'
       >
-        <div className='hidden md:flex items-center justify-center w-full text-indigo-300 text-[13px] font-bold font-creato-medium uppercase leading-normal tracking-wide'>
+        {/* <div className='hidden md:flex w-full ml-4 text-indigo-300 text-[13px] font-bold font-creato-medium uppercase leading-normal tracking-wide'>
           Type
-        </div>
-        <div className='col-span-2 flex items-center justify-center w-full text-indigo-300 text-[13px] font-bold font-creato-medium uppercase leading-normal tracking-wide'>
+        </div> */}
+        <div className='col-span-2 flex w-full ml-4 text-indigo-300 text-[13px] font-bold font-creato-medium uppercase leading-normal tracking-wide'>
           Topic
         </div>
-        <div className='hidden md:flex items-center justify-center w-full text-indigo-300 text-[13px] font-bold font-creato-medium uppercase leading-normal tracking-wide'>
+        <div className='hidden md:flex col-span-2 w-full ml-4 text-indigo-300 text-[13px] font-bold font-creato-medium uppercase leading-normal tracking-wide'>
           Resources
         </div>
-        <div className='hidden md:flex items-center justify-center w-full text-indigo-300 text-[13px] font-bold font-creato-medium uppercase leading-normal tracking-wide'>
+        <div className='hidden md:flex w-full ml-4 text-indigo-300 text-[13px] font-bold font-creato-medium uppercase leading-normal tracking-wide'>
           Created Date
         </div>
       </div>
@@ -93,17 +106,23 @@ const ProjectTable: React.FC<Props> = ({
         {' '}
         {currentProjects.map((project, index) => (
           <React.Fragment key={project.id}>
-            <div className='p-[2rem] hidden md:flex items-center border-b-2 justify-center text-gray-600 text-[17px] font-normal font-creato-medium leading-normal tracking-wide'>
+
+            {/* type */}
+            {/* <div className='p-[2rem] hidden md:flex items-center border-b-2 justify-center text-gray-600 text-[17px] font-normal font-creato-medium leading-normal tracking-wide'>
               <TaskIcon task={project.task} />
               <span className='ml-1'>{project.task}</span>
-            </div>
+            </div> */}
+
+            {/* topic */}
             <div
               className='col-span-2 p-2 flex cursor-pointer items-center text-start border-b-2 text-ellipsis overflow-hidden text-[17px] font-creato-medium leading-normal tracking-wide'
               onClick={() => onProjectClick(project.id)}
             >
               {project.name}
             </div>
-            <div className='p-[2rem] border-b-2 hidden md:flex items-center justify-center text-gray-600 text-[17px] font-normal font-creato-medium leading-normal tracking-wide'>
+
+            {/* resources */}
+            <div className='col-span-2 p-[2rem] border-b-2 hidden md:flex items-center text-gray-600 text-[17px] font-normal font-creato-medium leading-normal tracking-wide'>
               <div className='flex flex-col items-start'>
                 {/* <FileIcon fileType='pdf' /> */}
                 {project.resources && project.resources.map((resource, resourceIndex) => (
@@ -111,12 +130,16 @@ const ProjectTable: React.FC<Props> = ({
                     key={resourceIndex}
                     className='flex items-center justify-center gap-[0.5rem] bg-gray-100 p-1 m-1 rounded'
                   >
-                    <FileIcon fileType={resource.type} />
+                    <div className='max-w-[32px] min-w-[32px]'>
+                      <FileIcon fileType={resource.type} />
+                    </div>
                     {resource.name}
                   </div>
                 ))}
               </div>
             </div>
+
+            {/* create date */}
             <div className='p-2 border-b-2 flex items-center justify-center'>
               <div className='flex justify-center items-center text-gray-600 text-[13px] gap-2 font-normal font-creato-medium leading-normal tracking-[0.12rem]'>
                 <span className='hidden md:flex'>{moment(project.created_datetime).format('L')}</span>
