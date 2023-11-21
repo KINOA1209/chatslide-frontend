2aaffa330aec9fc9251a1f16b246a24b0a8aa8a8
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-11-21 10:43:04 -0800

    [modal] create closable modal class and use it for paywall and feedback

--- tailwindui/components/forms/paywallModal.tsx
+++ tailwindui/components/forms/paywallModal.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import { Pricing } from '@/components/landing/pricing'
 import ReferralLink from '../ReferralLink';
+import Modal from '../ui/Modal';
 
 interface PaywallModalProps {
   setShowModal: (value: boolean) => void;
@@ -27,57 +28,24 @@ const PaywallModal: React.FC<PaywallModalProps> = ({ setShowModal, message, show
   };
 
   return (
-    <>
-      <div
-        ref={modalRef}
-        id="staticModal"
-        data-modal-backdrop="static"
-        tabIndex={-1}
-        aria-hidden="true"
-        className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center w-full h-full"
-        onClick={handleOutsideClick}
-      >
-        {/* closable modal */}
-        <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+    <Modal showModal={true} setShowModal={setShowModal}>
+      <div className="pt-4 pb-4 space-y-4"> {/* Reduced bottom padding */}
+        <div className="text-center text-[#707C8A] text-[16px] font-bold">
+          {message}
         </div>
-        <span
-          className="hidden sm:inline-block sm:align-middle sm:h-screen"
-          aria-hidden="true"
-        >
-          &#8203;
-        </span>
 
-        
-        <div ref={modalContentRef} className="relative bg-white rounded-lg shadow w-128 max-h-[90vh] overflow-y-auto">
-          {/* Close button */}
-          <button
-            className="absolute top-0 right-4 text-2xl text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white focus:outline-none"
-            onClick={handleCloseModal}
-          >
-            &times;
-          </button>
+        <Pricing fewerCards={true} />
 
-          {/* Modal body */}
-          <div className="pt-4 pb-4 space-y-4"> {/* Reduced bottom padding */}
+        {showReferralLink &&
+          <div className="space-y-4">
             <div className="text-center text-[#707C8A] text-[16px] font-bold">
-              {message}
+              Or refer your friend for more ⭐️credits.
             </div>
-
-            <Pricing fewerCards={true} />
-
-            {showReferralLink &&
-              <div className="space-y-4">
-                <div className="text-center text-[#707C8A] text-[16px] font-bold">
-                  Or refer your friend for more ⭐️credits.
-                </div>
-                <ReferralLink />
-              </div>
-              }
+            <ReferralLink />
           </div>
-        </div>
+        }
       </div>
-    </>
+    </Modal>
   );
 };
 
