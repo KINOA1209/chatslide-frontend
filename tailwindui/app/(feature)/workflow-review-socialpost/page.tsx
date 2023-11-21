52d8df5c5525ec33117f8aab644245d488cac655
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-11-20 19:57:05 -0800

    [social post] use WorkflowStepsBanner

--- tailwindui/app/(feature)/workflow-review-socialpost/page.tsx
+++ tailwindui/app/(feature)/workflow-review-socialpost/page.tsx
@@ -11,49 +11,35 @@ import {
 } from '@/app/(feature)/icons'
 
 import { useRouter } from 'next/navigation';
+import WorkflowStepsBanner from '@/components/socialPost/socialPostWorkflowStep';
 
 export default function SocialMediaTemplate() {
   const router = useRouter()
   const contentRef = useRef<HTMLDivElement>(null)
-  return(
-    <div className='bg-gradient-to-b from-[#6A7EF9] to-[#415AF1]'>
-      {/* flex col container for steps, title, etc */}
-      <div className='fixed mt-[3rem] flex flex-col w-full bg-Grey-50 justify-center z-10 gap-1 py-[0.75rem] border-b-2'>
-        {/* steps bar */}
-        <div className='self-center'>
-          <SocialPostProjectProgress currentInd={1} contentRef={contentRef} />
+  return (
+    <section>
+      <div className='bg-gradient-to-b from-[#6A7EF9] to-[#415AF1]'>
+        {/* flex col container for steps, title, etc */}
+        <WorkflowStepsBanner
+          currentIndex={1}
+          isSubmitting={false}
+          setIsSubmitting={() => { }}
+          isPaidUser={true}
+          contentRef={contentRef}
+          nextIsPaidFeature={false}
+          showGPTToggle={false}
+          setIsGpt35={() => { }}
+          nextText={'Next'}
+          showNextButton={false}
+        />
+
+        <ToastContainer enableMultiContainer containerId={'slides'} />
+
+        <div className='mt-[1rem] max-w-4xl mx-auto px-6' ref={contentRef}>
+          <SocialPostVisualizer />
         </div>
-
-        {/* flex row container for backlink, title*/}
-        <div className='flex justify-start items-center mx-[5rem]'>
-          <div
-            className='flex-row justify-center items-center gap-4 cursor-pointer hidden sm:flex'
-            onClick={() => router.push('/workflow-generate-socialpost')}
-          >
-            <LeftTurnArrowIcon></LeftTurnArrowIcon>
-            <div className='text-center self-center text-gray-700 text-sm font-medium font-creato-medium leading-normal tracking-[0.035rem] whitespace-nowrap hidden sm:block'>
-              Back to Summary
-            </div>
-          </div>
-
-          <div className='flex-auto text-center self-center text-neutral-900 text-xl font-medium font-creato-medium leading-snug tracking-tight whitespace-nowrap hidden sm:block'>
-            Enjoy your posts! : )
-          </div>
-
-          <div className='w-[9rem]'></div>
-        </div>
-
-        <div className='flex-auto text-center self-center text-neutral-900 font-medium font-creato-medium leading-snug tracking-tight whitespace-nowrap sm:hidden'>
-            Use our desktop version to see all the functionalities!
-        </div>
+        <FeedbackButton timeout={30000} />
       </div>
-
-      <ToastContainer enableMultiContainer containerId={'slides'} />
-
-      <div className='mt-[10rem] max-w-4xl mx-auto px-6' ref={contentRef}>
-        <SocialPostVisualizer />
-      </div>
-      <FeedbackButton timeout={30000} />
-    </div>
+    </section>
   )
 }
