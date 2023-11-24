5d377f3c0343e3ebfa0810a118080da6c54384a2
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-11-24 11:16:20 -0800

    col1_img0 layout for default theme

--- tailwindui/components/slides/slideLayout.tsx
+++ tailwindui/components/slides/slideLayout.tsx
@@ -2,55 +2,85 @@ import React, { useEffect, useState, useMemo } from 'react'
 import { ImgModule } from '@/components/imgModule'
 import { MainSlideProps as BaseMainSlideProps } from './slideTemplates'
 import col1img0_png from '@/public/images/template/col1img0.png'
+import col2img0_png from '@/public/images/template/col2img0.png'
+import col3img0_png from '@/public/images/template/col3img0.png'
 import col2img1_png from '@/public/images/template/col2img1.png'
 import col3img2_png from '@/public/images/template/col3img2.png'
-
+import { useLocalImgs } from './slideTemplates'
 export type LayoutKeys =
   | ''
   | 'Col_1_img_0_layout'
+  | 'Col_2_img_0_layout'
+  | 'Col_3_img_0_layout'
   | 'Col_2_img_1_layout'
   | 'Col_3_img_2_layout'
+
 // Extend the interface with new fields
 interface MainSlideProps extends BaseMainSlideProps {}
 
-const useLocalImgs = (
-  imgs: string[],
-  imgCount: number,
-  update_callback: (imgs: string[]) => void
-) => {
-  const initialImgs = useMemo(() => {
-    let cleanedImgs = imgs.filter((url) => url !== '')
-    if (cleanedImgs.length > imgCount) {
-      cleanedImgs = cleanedImgs.slice(0, imgCount)
-    } else if (cleanedImgs.length < imgCount) {
-      cleanedImgs = [
-        ...cleanedImgs,
-        ...new Array(imgCount - cleanedImgs.length).fill(''),
-      ]
-    }
-    return cleanedImgs
-  }, [imgs, imgCount])
-
-  const [localImgs, setLocalImgs] = useState<string[]>(initialImgs)
-
-  useEffect(() => {
-    update_callback(localImgs)
-  }, [localImgs])
-
-  const updateImgAtIndex = (index: number) => {
-    const updateLocalImgs = (url: string) => {
-      const newLocalImgs = [...localImgs]
-      newLocalImgs[index] = url
-      setLocalImgs(newLocalImgs)
-      console.log('updateLocalImgs', newLocalImgs)
-    }
-    return updateLocalImgs
-  }
-
-  return { localImgs, updateImgAtIndex }
-}
-
 export const Col_1_img_0_layout = ({
+  user_name,
+  title,
+  topic,
+  subtopic,
+  content,
+  imgs,
+  update_callback,
+  canEdit,
+  autoSave,
+  isCoverPage,
+  layoutOption,
+}: MainSlideProps) => {
+  return (
+    <div className='h-full w-full flex flex-row overflow-hidden gap-[32px]'>
+      <div
+        className='w-full h-full grow p-1 text-neutral-900 text-opacity-70 text-sm font-normal font-creato-medium leading-[140%] tracking-[0.025rem]'
+        style={
+          {
+            // display: 'list-item',
+            //   listStyleType: 'disc',
+            //   listStylePosition: 'inside',
+            //   fontSize: '18pt',
+            //   marginLeft: '20px',
+          }
+        }
+      >
+        {content}
+      </div>
+    </div>
+  )
+}
+export const Col_2_img_0_layout = ({
+  user_name,
+  title,
+  topic,
+  subtopic,
+  content,
+  imgs,
+  update_callback,
+  canEdit,
+  autoSave,
+  isCoverPage,
+  layoutOption,
+}: MainSlideProps) => {
+  return (
+    <div className='h-full w-full flex flex-row overflow-hidden gap-[32px]'>
+      <div
+        className='w-full h-full grow p-1'
+        style={{
+          // display: 'list-item',
+          listStyleType: 'disc',
+          listStylePosition: 'inside',
+          fontSize: '18pt',
+          marginLeft: '20px',
+        }}
+      >
+        {content}
+      </div>
+    </div>
+  )
+}
+export const Col_3_img_0_layout = ({
   user_name,
   title,
   topic,
@@ -149,6 +179,8 @@ export const Col_3_img_2_layout = ({
 
 export const layoutOptions = {
   Col_1_img_0_layout: Col_1_img_0_layout,
+  Col_2_img_0_layout: Col_2_img_0_layout,
+  Col_3_img_0_layout: Col_3_img_0_layout,
   Col_2_img_1_layout: Col_2_img_1_layout,
   Col_3_img_2_layout: Col_3_img_2_layout,
 }
@@ -158,6 +190,14 @@ export const availableLayouts = {
     {
       name: 'Col_1_img_0_layout' as LayoutKeys,
       img: col1img0_png.src,
+    },
+    {
+      name: 'Col_2_img_0_layout' as LayoutKeys,
+      img: col2img0_png.src,
+    },
+    {
+      name: 'Col_3_img_0_layout' as LayoutKeys,
+      img: col3img0_png.src,
     },
     {
       name: 'Col_2_img_1_layout' as LayoutKeys,
