8a4208c17e99bf865d0068aa598448f321e23710
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-11-25 16:35:42 -0800

    [landing] more fine tune

--- tailwindui/components/landing/HeroText.tsx
+++ tailwindui/components/landing/HeroText.tsx
@@ -30,7 +30,7 @@ export const TextCarousel: React.FC<Props> = ({ slides, interval = 3000, color =
       <ul className="carousel-list flex flex-col transition-transform duration-500" style={{ transform: `translateY(-${currentIndex * 4}rem)` }}>
         {slides.map((slide, index) => (
           <li key={index} className="carousel-item h-[4rem] text-center py-1 flex-none">
-            <h1 className={`text-5xl font-extrabold leading-tighter tracking-tighter text-${alignment} ${color}`}>{slide}</h1>
+            <h1 className={`text-5xl font-extrabold leading-tighter tracking-tighter text-${alignment} ${color} font-creato-medium `}>{slide}</h1>
           </li>
         ))}
       </ul>
@@ -49,7 +49,7 @@ const HeroText = () => {
     <div className={`flex flex-col items-center transition-opacity duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
       <div className="flex flex-col md:flex-row items-end space-x-4">
         {/* <TextCarousel slides={["Documents", "Youtube", "Webpage", "Notion"]} interval={3000} color="text-blue-700" alignment='right'/> */}
-        <h1 className="text-5xl font-extrabold leading-tighter tracking-tighter">Documents to</h1>
+        <h1 className="text-5xl font-extrabold leading-tighter tracking-tighte font-creato-medium mb-1">Documents to</h1>
         <TextCarousel slides={["Slides", "Social Posts", "Video"]} interval={3000} color="text-purple-700" />
       </div>
     </div>
