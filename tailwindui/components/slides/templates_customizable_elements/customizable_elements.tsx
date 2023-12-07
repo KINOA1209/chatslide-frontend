eb97a5d3db121ec412b82ae07aaed5b6cb97fbab
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-12-07 09:25:03 -0800

    mit template

--- tailwindui/components/slides/templates_customizable_elements/customizable_elements.tsx
+++ tailwindui/components/slides/templates_customizable_elements/customizable_elements.tsx
@@ -19,6 +19,7 @@ type Config = {
     Stanford: TemplateElements;
     Berkeley: TemplateElements;
     Harvard: TemplateElements;
+    MIT: TemplateElements;
     Default: TemplateElements;
 };
 
@@ -73,6 +74,26 @@ const configData: Config = {
         headFont: "text-4xl font-normal font-['Gorgia'] leading-[120%]",
         headFontColor: 'text-neutral-800',
     },
+    MIT: {
+        backgroundColorCover: 'bg-gray-100', // Light gray background
+        backgroundColor: 'bg-gray-100', // Light gray background
+        titleFont:
+            'text-3xl font-bold font-creato-medium leading-[100%] text-black', // Black title font
+        titleFontColor: 'text-black', // Black title font color
+        subtopicFont:
+            'text-xl font-normal font-creato-medium uppercase leading-[150%] tracking-[0.15rem] text-red-700', // MIT red subtopic font
+        subtopicFontColor: 'text-red-700', // MIT red subtopic font color
+        contentFont:
+            'text-opacity-70 text-base font-normal font-creato-medium leading-[140%] tracking-[0.025rem] text-gray-800', // Dark gray content font
+        contentFontColor: 'text-gray-800', // Dark gray content font color
+        userNameFont:
+            'text-sm font-normal font-creato-medium leading-[140%] tracking-[0.026rem] text-gray-800', // Dark username font
+        userNameFontColor: 'text-gray-800', // Dark username font color
+        headFont:
+            'text-neutral-800 text-4xl font-normal font-creato-medium leading-[120%] tracking-tight', // Neutral 800 head font
+        headFontColor: 'text-neutral-800', // Neutral 800 head font color
+    },
+
     Default: {
         backgroundColorCover: 'bg-[#F0F0F2]',
         backgroundColor: 'bg-[#F0F0F2]',
