f3aecb7911484c6c5a64312b51de11e9efd8797e
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-12-12 15:34:38 -0800

    [paywall] further improve paywall style

--- tailwindui/components/forms/paywallModal.tsx
+++ tailwindui/components/forms/paywallModal.tsx
@@ -29,7 +29,7 @@ const PaywallModal: React.FC<PaywallModalProps> = ({ setShowModal, message, show
   return (
     <Modal showModal={true} setShowModal={setShowModal}>
       <div className="pt-4 pb-4 space-y-4"> {/* Reduced bottom padding */}
-        <h4 className="text-center text-red-700 font-bold">
+        <h4 className="h4 text-center text-red-700">
           {message}
         </h4>
 
