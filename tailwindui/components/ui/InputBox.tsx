2331188dc65485407460cbbf8c9d17c23ec866d9
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-12-13 16:13:15 -0800

    [account][InputBox][ClickableLink] update sytle

--- tailwindui/components/ui/InputBox.tsx
+++ tailwindui/components/ui/InputBox.tsx
@@ -2,7 +2,7 @@ import { MouseEventHandler, ReactNode } from "react";
 
 type InputBoxProps = {
   children: ReactNode;
-  onClick: MouseEventHandler<HTMLDivElement>;
+  onClick?: MouseEventHandler<HTMLDivElement>;
   isSubmitting?: boolean;
   isPaidUser?: boolean;
   isPaidFeature?: boolean;
@@ -11,7 +11,7 @@ type InputBoxProps = {
 export const InputBox: React.FC<InputBoxProps> = ({ children, onClick }) => {
   return (
     <div
-      className='w-full flex flex-row flex-nowrap items-center border border-2 border-gray-200 bg-gray-100 px-2 py-0.5 cursor-text rounded-lg overflow-visible'
+      className='w-full max-w-[100vw] flex flex-row flex-nowrap items-center justify-center border border-2 border-gray-200 bg-gray-100 px-3 py-2.5 gap-x-2 cursor-text rounded-lg overflow-visible'
       onClick={onClick}
     >
       {children}
