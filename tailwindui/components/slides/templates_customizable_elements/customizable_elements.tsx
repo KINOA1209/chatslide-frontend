a54035f2bbb4d50c64360d525b32e9f29843857e
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-12-14 17:59:30 -0800

    style modify from tailwind to CSSProperties

--- tailwindui/components/slides/templates_customizable_elements/customizable_elements.tsx
+++ tailwindui/components/slides/templates_customizable_elements/customizable_elements.tsx
@@ -3,77 +3,158 @@
 export type TemplateElements = {
   backgroundColorCover: string;
   backgroundColor: string;
-  //   titleFont: string;
-  //   titleFontColor: string;
   titleFontCSS: React.CSSProperties;
-  //   subtopicFont: string;
-  //   subtopicFontColor: string;
   subtopicFontCSS: React.CSSProperties;
-  //   contentFont: string;
   contentFontCSS: React.CSSProperties;
-  //   contentFontColor: string;
   userNameFont: string;
   userNameFontColor: string;
-  //   headFont: string;
-  //   headFontColor: string;
   headFontCSS: React.CSSProperties;
 };
 
 type Config = {
-  //   Stanford: TemplateElements;
-  //   Berkeley: TemplateElements;
-  //   Harvard: TemplateElements;
+  Stanford: TemplateElements;
+  Berkeley: TemplateElements;
+  Harvard: TemplateElements;
   //   MIT: TemplateElements;
   Default: TemplateElements;
 };
 
 const configData: Config = {
-  //   Stanford: {
-  //     backgroundColorCover: 'bg-[#8C1515]',
-  //     backgroundColor: 'bg-[#F0E9E9]',
-  //     titleFont:
-  //       'text-3xl font-nimbus-sans-bold font-bold leading-[110%] whitespace-nowrap',
-  //     titleFontColor: 'text-[#8C1515]',
-  //     subtopicFont:
-  //       'opacity-70 font-nimbus-sans-bold text-opacity-40 text-xl font-normal leading-[150%]',
-  //     subtopicFontColor: 'text-red-800',
-  //     contentFont: 'text-base font-nimbus-sans-regular font-normal leading-9 ',
-  //     contentFontColor: 'text-zinc-800',
-  //     userNameFont: 'text-sm font-nimbus-sans-regular font-normal leading-[120%]',
-  //     userNameFontColor: 'text-white',
-  //     headFont: 'text-4xl font-nimbus-sans-bold font-bold leading-[120%]',
-  //     headFontColor: 'text-white',
-  //   },
-  //   Berkeley: {
-  //     backgroundColorCover: 'bg-[#003262]',
-  //     backgroundColor: 'bg-[#F0F2F5]',
-  //     titleFont: 'text-3xl leading-[120%] font-sans font-bold whitespace-nowrap',
-  //     titleFontColor: 'text-[#003262]',
-  //     subtopicFont: 'text-xl font-semibold leading-[120%] font-sans',
-  //     subtopicFontColor: 'text-[#525252]',
-  //     contentFont: 'text-base font-normal font-sans leading-[120%]',
-  //     contentFontColor: 'text-[#1B1B1B]',
-  //     userNameFont: 'text-sm font-normal font-sans leading-[120%]',
-  //     userNameFontColor: 'text-white',
-  //     headFont: 'text-4xl font-bold font-sans leading-[120%]',
-  //     headFontColor: 'text-white',
-  //   },
-  //   Harvard: {
-  //     backgroundColorCover: 'bg-[#F0F0F2]',
-  //     backgroundColor: 'bg-[#F0F0F2]',
-  //     titleFont:
-  //       "text-3xl font-normal font-['Georgia'] leading-[120%] whitespace-nowrap",
-  //     titleFontColor: 'text-neutral-800 ',
-  //     subtopicFont: "text-xxl font-bold font-['Arial'] leading-[120%]",
-  //     subtopicFontColor: 'text-neutral-700',
-  //     contentFont:
-  //       "opacity-70 text-base font-normal font-['Arial'] leading-loose",
-  //     contentFontColor: 'text-neutral-700 ',
-  //     userNameFont: "opacity-70 text-sm font-normal font-['Arial'] leading-loose",
-  //     userNameFontColor: 'text-neutral-700',
-  //     headFont: "text-4xl font-normal font-['Gorgia'] leading-[120%]",
-  //     headFontColor: 'text-neutral-800',
-  //   },
+  Stanford: {
+    backgroundColorCover: 'bg-[#8C1515]',
+    backgroundColor: 'bg-[#F0E9E9]',
+    // titleFont:
+    //   'text-3xl font-nimbus-sans-bold font-bold leading-[110%] whitespace-nowrap',
+    // titleFontColor: 'text-[#8C1515]',
+    titleFontCSS: {
+      fontSize: '24pt', // text-3xl in points
+      fontWeight: 'bold', // font-bold
+      fontFamily: "'nimbus-sans-bold', sans-serif", // font-nimbus-sans-bold
+      lineHeight: 1.1, // leading-[110%] is equivalent to a line height of 1.1
+      whiteSpace: 'nowrap', // whitespace-nowrap},
+      color: '#8C1515', // text-[#8C1515] color
+    },
+    // subtopicFont:
+    //   'opacity-70 font-nimbus-sans-bold text-opacity-40 text-xl font-normal leading-[150%]',
+    // subtopicFontColor: 'text-red-800',
+    subtopicFontCSS: {
+      opacity: 0.7, // opacity-70
+      fontWeight: 'bold', // font-nimbus-sans-bold
+      fontSize: '18pt', // text-xl in points
+      fontStyle: 'normal', // font-normal
+      lineHeight: 1.5, // leading-[150%] is equivalent to a line height of 1.5
+      color: '#EF4444', // text-red-800 color
+    },
+    // contentFont: 'text-base font-nimbus-sans-regular font-normal leading-9 ',
+    // contentFontColor: 'text-zinc-800',
+    contentFontCSS: {
+      fontSize: '12pt', // text-base in points (assuming 1rem is 1pt)
+      fontWeight: 'normal', // font-normal
+      fontFamily: "'nimbus-sans-regular', sans-serif", // font-nimbus-sans-regular
+      lineHeight: 1.5, // leading-9 is equivalent to a line height of 1.5
+      color: '#4B5563', // text-zinc-800 color
+    },
+    userNameFont: 'text-sm font-nimbus-sans-regular font-normal leading-[120%]',
+    userNameFontColor: 'text-white',
+    // headFont: 'text-4xl font-nimbus-sans-bold font-bold leading-[120%]',
+    // headFontColor: 'text-white',
+    headFontCSS: {
+      fontSize: '32pt', // text-4xl in points (assuming 1rem is 1pt)
+      fontWeight: 'bold', // font-bold
+      fontFamily: "'nimbus-sans-bold', sans-serif", // font-nimbus-sans-bold
+      lineHeight: 1.2, // leading-[120%] is equivalent to a line height of 1.2
+      color: '#FFFFFF', // text-white color
+    },
+  },
+  Berkeley: {
+    backgroundColorCover: 'bg-[#003262]',
+    backgroundColor: 'bg-[#F0F2F5]',
+    // titleFont: 'text-3xl leading-[120%] font-sans font-bold whitespace-nowrap',
+    // titleFontColor: 'text-[#003262]',
+    titleFontCSS: {
+      fontSize: '24pt', // text-3xl in points (assuming 1rem is 1pt)
+      lineHeight: 1.2, // leading-[120%] is equivalent to a line height of 1.2
+      fontWeight: 'bold', // font-bold
+      fontFamily: 'sans-serif', // font-sans (assuming sans-serif as a default)
+      whiteSpace: 'nowrap', // whitespace-nowrap
+      color: '#003262', // text-[#003262] color
+    },
+    // subtopicFont: 'text-xl font-semibold leading-[120%] font-sans',
+    // subtopicFontColor: 'text-[#525252]',
+    subtopicFontCSS: {
+      fontSize: '18pt', // text-xl in points (assuming 1rem is 1pt)
+      fontWeight: 'bold', // font-semibold
+      lineHeight: 1.2, // leading-[120%] is equivalent to a line height of 1.2
+      fontFamily: 'sans-serif', // font-sans (assuming sans-serif as a default)
+      color: '#525252', // text-[#525252] color
+    },
+    // contentFont: 'text-base font-normal font-sans leading-[120%]',
+    // contentFontColor: 'text-[#1B1B1B]',
+    contentFontCSS: {
+      fontSize: '12pt', // text-base in points (assuming 1rem is 1pt)
+      fontWeight: 'normal', // font-normal
+      lineHeight: 1.2, // leading-[120%] is equivalent to a line height of 1.2
+      fontFamily: 'sans-serif', // font-sans (assuming sans-serif as a default)
+      color: '#1B1B1B', // text-[#1B1B1B] color
+    },
+    userNameFont: 'text-sm font-normal font-sans leading-[120%]',
+    userNameFontColor: 'text-white',
+    // headFont: 'text-4xl font-bold font-sans leading-[120%]',
+    // headFontColor: 'text-white',
+    headFontCSS: {
+      fontSize: '32pt', // text-4xl in points (assuming 1rem is 1pt)
+      fontWeight: 'bold', // font-bold
+      fontFamily: 'sans-serif', // font-sans (assuming sans-serif as a default)
+      lineHeight: 1.2, // leading-[120%] is equivalent to a line height of 1.2
+      color: '#FFFFFF', // text-white color
+    },
+  },
+  Harvard: {
+    backgroundColorCover: 'bg-[#F0F0F2]',
+    backgroundColor: 'bg-[#F0F0F2]',
+    // titleFont:
+    //   "text-3xl font-normal font-['Georgia'] leading-[120%] whitespace-nowrap",
+    // titleFontColor: 'text-neutral-800 ',
+    titleFontCSS: {
+      fontSize: '24pt', // text-3xl in points (assuming 1rem is 1pt)
+      fontWeight: 'normal', // font-normal
+      fontFamily: 'Georgia, sans-serif', // font-['Georgia']
+      lineHeight: 1.2, // leading-[120%] is equivalent to a line height of 1.2
+      whiteSpace: 'nowrap', // whitespace-nowrap
+      color: '#374151', // text-neutral-800 color
+    },
+    // subtopicFont: "text-xl font-bold font-['Arial'] leading-[120%]",
+    // subtopicFontColor: 'text-neutral-700',
+    subtopicFontCSS: {
+      fontSize: '18pt', // Assuming 'xxl' represents 2.625rem (converted to points)
+      fontWeight: 'bold', // font-bold
+      fontFamily: 'Arial, sans-serif', // font-['Arial']
+      lineHeight: 1.2, // leading-[120%] is equivalent to a line height of 1.2
+      color: '#4A5568', // text-neutral-700 color
+    },
+    // contentFont:
+    //   "opacity-70 text-base font-normal font-['Arial'] leading-loose",
+    // contentFontColor: 'text-neutral-700 ',
+    contentFontCSS: {
+      opacity: 0.7, // opacity-70
+      fontSize: '12pt', // text-base in points (assuming 1rem is 1pt)
+      fontWeight: 'normal', // font-normal
+      fontFamily: 'Arial, sans-serif', // font-['Arial']
+      lineHeight: 'normal', // leading-loose, assuming 'normal' or '1.5' depending on your design
+      color: '#4A5568', // text-neutral-700 color
+    },
+    userNameFont: "opacity-70 text-sm font-normal font-['Arial'] leading-loose",
+    userNameFontColor: 'text-neutral-700',
+    // headFont: "text-4xl font-normal font-['Gorgia'] leading-[120%]",
+    // headFontColor: 'text-neutral-800',
+    headFontCSS: {
+      fontSize: '32pt', // text-4xl in points (assuming 1rem is 1pt)
+      fontWeight: 'normal', // font-normal
+      fontFamily: 'Georgia, sans-serif', // font-['Gorgia']
+      lineHeight: 1.2, // leading-[120%] is equivalent to a line height of 1.2
+      color: '#374151', // text-neutral-800 color
+    },
+  },
   //   MIT: {
   //     backgroundColorCover: 'bg-gray-100', // Light gray background
   //     backgroundColor: 'bg-gray-100', // Light gray background
