56f2eac7cd7d877b6d7ece7e1e4d073373ae1a09
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-12-20 09:53:53 -0800

    fix template logo display

--- tailwindui/components/slides/templates_customizable_elements/Templates_logos.tsx
+++ tailwindui/components/slides/templates_customizable_elements/Templates_logos.tsx
@@ -4,6 +4,10 @@ import drlambdaLogo from '@/public/images/template/drlambdaLogo.png';
 import drlambdaLogoSingle from '@/public/images/template/drlambdaLogoSingle.png';
 import BerkeleyLogo from '@/public/images/template/Berkeley/Berkeley_logo.png';
 import BerkeleyLogoWhite from '@/public/images/template/Berkeley/Berkeley_logo_white.png';
+import StanfordLogo from '@/public/images/template/Stanford/StanfordLogo.png';
+import StanfordLogoLetters from '@/public/images/template/Stanford/Stanford_logo_letters.png';
+import HarvardLogo from '@/public/images/template/Harvard/Harvard_logo.png';
+import MITLogo from '@/public/images/template/MIT/MIT_Logo.png';
 
 // Inside DefaultTemplateLogo component
 export const DefaultTemplateLogo = ({
@@ -55,6 +59,54 @@ export const BerkeleyTemplateLogo = ({
 	);
 };
 
+export const StanfordTemplateLogo = ({
+	isCoverPage,
+}: {
+	isCoverPage: boolean;
+}) => {
+	return (
+		<div className='absolute inset-0 top-[90%] w-full h-14 justify-start items-center gap-7 inline-flex pl-[2rem] pb-[2rem]'>
+			<Image
+				src={StanfordLogo}
+				alt='Standford Logo'
+				className='w-[3.75rem] h-auto'
+			/>
+			<div
+				className={`grow basis-0 opacity-50 border ${
+					isCoverPage ? 'border-white' : 'border-red-800'
+				}  border-opacity-40`}
+			></div>
+		</div>
+	);
+};
+export const HarvardTemplateLogo = ({
+	isCoverPage,
+}: {
+	isCoverPage: boolean;
+}) => {
+	return (
+		<div
+			className={`fixed inset-0 top-[85%] w-full h-14 justify-start items-center gap-7 inline-flex pb-[2rem] pl-[2rem]`}
+		>
+			<Image
+				src={HarvardLogo}
+				alt='Standford Logo'
+				className='w-[10rem] h-auto'
+			/>
+		</div>
+	);
+};
+export const MITTemplateLogo = ({ isCoverPage }: { isCoverPage: boolean }) => {
+	return (
+		<div className='absolute inset-0 top-[90%] w-full h-14 justify-start items-center gap-7 inline-flex pl-[1rem] pb-[1rem]'>
+			<Image
+				src={MITLogo}
+				alt='Standford Logo'
+				className='w-[3.75rem] h-auto'
+			/>
+		</div>
+	);
+};
 // Define the type for template logo information
 type TemplateLogoInfo = {
 	templateName: TemplateKeys;
@@ -71,10 +123,18 @@ const templatesInfo: TemplateLogoInfo[] = [
 		templateName: 'Berkeley' as TemplateKeys,
 		templateLogo: BerkeleyTemplateLogo,
 	},
-	// Add other templates here
-	// { templateName: 'Stanford' as TemplateKeys, templateLogo: StanfordTemplateLogo },
-	// { templateName: 'Harvard' as TemplateKeys, templateLogo: HarvardTemplateLogo },
-	// ...
+	{
+		templateName: 'Stanford' as TemplateKeys,
+		templateLogo: StanfordTemplateLogo,
+	},
+	{
+		templateName: 'Harvard' as TemplateKeys,
+		templateLogo: HarvardTemplateLogo,
+	},
+	{
+		templateName: 'MIT' as TemplateKeys,
+		templateLogo: MITTemplateLogo,
+	},
 ];
 
 export const TemplatesLogos = Object.fromEntries(
