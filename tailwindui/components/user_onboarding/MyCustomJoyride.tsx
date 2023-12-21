3dc88180c1d66a49113ceca9cf015053831a7e48
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-12-21 15:51:16 -0800

    add exit warning and make spotlight area clickable

--- tailwindui/components/user_onboarding/MyCustomJoyride.tsx
+++ tailwindui/components/user_onboarding/MyCustomJoyride.tsx
@@ -2,149 +2,167 @@
 import React, { useState, useEffect } from 'react';
 import Joyride, { CallBackProps, Step } from 'react-joyride';
 import { UserOnboardingButton } from '../button/DrlambdaButton';
-
+import ExitUserGuideWarningPNG from '@/public/images/user_onboarding/ExitTourWarning.png';
+import './user_onboarding.css';
 export interface CustomStep extends Step {
-  // Add custom properties if needed
+	// Add custom properties if needed
 }
 
 interface MyCustomJoyrideProps {
-  steps: CustomStep[];
+	steps: CustomStep[];
 }
 
 const MyCustomJoyride: React.FC<MyCustomJoyrideProps> = ({ steps }) => {
-  const [isTourActive, setIsTourActive] = useState(false);
-  const [showConfirmation, setShowConfirmation] = useState(false);
+	const [isTourActive, setIsTourActive] = useState(false);
+	const [showConfirmation, setShowConfirmation] = useState(false);
+	// const [currentStep, setCurrentStep] = useState<number>(0);
+	const handleJoyrideCallback = (data: CallBackProps) => {
+		console.log(data);
+		// setCurrentStep(data.index);
+		if (data.action === 'skip') {
+			// Show the confirmation tooltip when skipping the tour
+			setShowConfirmation(true);
+			setIsTourActive(true);
+		} else if (data.action === 'close') {
+			// If CLOSE (End Tour) or RESET (outside click) occurs, reset the tour
+			setIsTourActive(false);
+			setShowConfirmation(true);
+		}
+	};
+	// const shouldShowOverlay = steps[currentStep]?.target !== 'body';
+	const startTour = () => {
+		setIsTourActive(true);
+		setShowConfirmation(false); // Ensure confirmation tooltip is hidden
+	};
 
-  const handleJoyrideCallback = (data: CallBackProps) => {
-    console.log(data);
+	const handleConfirmation = (confirmed: boolean) => {
+		// Handle the user's choice (confirmed or not)
+		if (confirmed) {
+			// User confirmed, reset the tour or perform other actions
+			setIsTourActive(false);
+			setShowConfirmation(false);
+		} else {
+			// User chose not to skip the tour, hide the confirmation tooltip
+			setIsTourActive(true);
+			setShowConfirmation(false);
+		}
+	};
 
-    if (data.action === 'skip') {
-      // Show the confirmation tooltip when skipping the tour
-      setShowConfirmation(true);
-      setIsTourActive(true);
-    } else if (data.action === 'close' || data.action === 'reset') {
-      // If CLOSE (End Tour) or RESET (outside click) occurs, reset the tour
-      setIsTourActive(false);
-    }
-  };
+	return (
+		<>
+			{/* {shouldShowOverlay && <div className='custom-overlay' />} */}
 
-  const startTour = () => {
-    setIsTourActive(true);
-  };
-
-  const handleConfirmation = (confirmed: boolean) => {
-    // Handle the user's choice (confirmed or not)
-    if (confirmed) {
-      // User confirmed, reset the tour or perform other actions
-      setIsTourActive(false);
-      setShowConfirmation(false);
-    } else {
-      // User chose not to skip the tour, hide the confirmation tooltip
-      setIsTourActive(true);
-      setShowConfirmation(false);
-    }
-  };
-
-  return (
-    <>
-      {/* <button
-        className='fixed top-4 left-[10rem] bg-purple-500 text-blue-500 px-4 py-2 rounded cursor-pointer z-40'
-        onClick={startTour}
-      >
-        Begin a user guide tour
-      </button> */}
-      <UserOnboardingButton onClick={startTour}></UserOnboardingButton>
-      <Joyride
-        steps={steps}
-        continuous
-        scrollToFirstStep
-        showProgress
-        showSkipButton
-        locale={{
-          back: 'Back',
-          close: 'Close',
-          last: 'End Tour',
-          next: 'Next',
-          skip: 'Skip Tour',
-        }}
-        styles={{
-          options: {
-            arrowColor: '#FF5733', // Customize arrow color
-            backgroundColor: '#FFF', // Customize the background color of the tooltip
-            overlayColor: 'rgba(0, 0, 0, 0.5)', // Customize the overlay color
-            primaryColor: 'none', // Customize the primary color (text color, button color, etc.)
-            textColor: 'none', // Customize the text color
-          },
-          buttonBack: {
-            // color: '#ffffff', // Customize the back button color
-            // background: '#FF5733', // Customize the back button background color
-            // border: '2px solid #FF5733',
-            // borderRadius: '8px',
-            // padding: '10px 20px',
-            // marginRight: '10px',
-            // cursor: 'pointer',
-            // fontWeight: 'bold',
-          },
-          buttonClose: {
-            color: '#707C8A', // Customize the close button color
-            // background: '#FF5733', // Customize the close button background color
-          },
-          buttonNext: {
-            // width: 100,
-            // height: 40,
-            // paddingLeft: 7.4,
-            // paddingRight: 7.4,
-            // paddingTop: 2.47,
-            // paddingBottom: 2.47,
-            // background: '#2943E9',
-            // borderRadius: 4.94,
-            // overflow: 'hidden',
-            // justifyContent: 'center',
-            // alignItems: 'center',
-            // gap: 6.17,
-            // display: 'inline-flex',
-            // textAlign: 'center',
-            // color: '#F4F4F4',
-            // fontSize: 15,
-            // fontFamily: 'Creato Display',
-            // fontWeight: '500',
-            // lineHeight: 14.81,
-            // letterSpacing: 0.6,
-            // wordWrap: 'break-word',
-          },
-          buttonSkip: {
-            // color: '#ffffff', // Customize the skip button color
-            // background: '#FF5733', // Customize the skip button background color
-          },
-        }}
-        callback={handleJoyrideCallback}
-        run={isTourActive}
-      />
-      {showConfirmation && (
-        <>
-          <div
-            className='confirmation-overlay fixed inset-0 bg-black bg-opacity-50 z-40'
-            onClick={() => handleConfirmation(false)} // Close the confirmation on overlay click
-          ></div>
-          <div className='confirmation-tooltip bg-white p-8 rounded-lg shadow-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50'>
-            <p className='text-lg mb-4'>Are you sure you want to quit?</p>
-            <button
-              onClick={() => handleConfirmation(true)}
-              className='bg-red-500 text-white px-4 py-2 rounded mr-4'
-            >
-              Quit
-            </button>
-            <button
-              onClick={() => handleConfirmation(false)}
-              className='bg-gray-300 text-gray-700 px-4 py-2 rounded'
-            >
-              Not now
-            </button>
-          </div>
-        </>
-      )}
-    </>
-  );
+			<UserOnboardingButton onClick={startTour}></UserOnboardingButton>
+			<Joyride
+				steps={steps}
+				continuous
+				scrollToFirstStep
+				showProgress
+				showSkipButton
+				disableOverlayClose
+				disableCloseOnEsc
+				spotlightClicks
+				locale={{
+					back: 'Back',
+					close: 'Close',
+					last: 'End Tour',
+					next: 'Next',
+					skip: 'Skip Tour',
+				}}
+				styles={{
+					options: {
+						arrowColor: '#FF5733', // Customize arrow color
+						backgroundColor: '#FFF', // Customize the background color of the tooltip
+						overlayColor: 'rgba(0, 0, 0, 0.5)', // Customize the overlay color
+						primaryColor: 'none', // Customize the primary color (text color, button color, etc.)
+						textColor: 'none', // Customize the text color
+					},
+					buttonBack: {
+						// color: '#ffffff', // Customize the back button color
+						// background: '#FF5733', // Customize the back button background color
+						// border: '2px solid #FF5733',
+						// borderRadius: '8px',
+						// padding: '10px 20px',
+						// marginRight: '10px',
+						// cursor: 'pointer',
+						// fontWeight: 'bold',
+					},
+					buttonClose: {
+						color: '#707C8A', // Customize the close button color
+						// background: '#FF5733', // Customize the close button background color
+					},
+					buttonNext: {
+						// width: 100,
+						// height: 40,
+						// paddingLeft: 7.4,
+						// paddingRight: 7.4,
+						// paddingTop: 2.47,
+						// paddingBottom: 2.47,
+						// background: '#2943E9',
+						// borderRadius: 4.94,
+						// overflow: 'hidden',
+						// justifyContent: 'center',
+						// alignItems: 'center',
+						// gap: 6.17,
+						// display: 'inline-flex',
+						// textAlign: 'center',
+						// color: '#F4F4F4',
+						// fontSize: 15,
+						// fontFamily: 'Creato Display',
+						// fontWeight: '500',
+						// lineHeight: 14.81,
+						// letterSpacing: 0.6,
+						// wordWrap: 'break-word',
+					},
+					buttonSkip: {
+						// color: '#ffffff', // Customize the skip button color
+						// background: '#FF5733', // Customize the skip button background color
+					},
+				}}
+				callback={handleJoyrideCallback}
+				run={isTourActive}
+			/>
+			{showConfirmation && (
+				<>
+					<div
+						className='confirmation-overlay fixed inset-0 bg-black bg-opacity-50 z-40'
+						onClick={() => handleConfirmation(false)} // Close the confirmation on overlay click
+					></div>
+					<div className='confirmation-tooltip bg-white p-8 rounded-lg shadow-lg fixed top-1/2 left-1/2 w-[31rem] h-auto transform -translate-x-1/2 -translate-y-1/2 z-50 flex flex-col'>
+						<img src={ExitUserGuideWarningPNG.src} alt='Exit warning' />
+						{/* explaining text */}
+						<div className='flex flex-col items-start gap-[0.5rem]'>
+							<div className='pt-[2rem] text-neutral-900 text-xl font-bold font-creato-bold leading-tight tracking-tight'>
+								Are you sure to leave the tutorial?
+							</div>
+							<p className='text-neutral-800 text-base font-normal font-creato=regular leading-normal tracking-tight text-left'>
+								You could always come back via the help section on the header.
+							</p>
+						</div>
+						<div className='pt-[3rem] flex flex-row items-center justify-end gap-[0.75rem]'>
+							<div
+								onClick={() => handleConfirmation(true)}
+								className='w-24 h-10 px-2 py-0.5 rounded border border-[#2943E9] justify-center items-center gap-1.5 inline-flex cursor-pointer'
+							>
+								<span className='text-center text-[#2943E9] text-base font-medium font-creato-medium leading-none tracking-wide'>
+									Quit
+								</span>
+							</div>
+							<div
+								onClick={() => handleConfirmation(false)}
+								className='w-24 h-10 px-2 py-0.5 bg-[#2943E9] rounded justify-center items-center gap-1.5 inline-flex cursor-pointer'
+							>
+								<span className='text-center text-zinc-100 text-base font-medium font-creato-medium leading-none tracking-wide'>
+									Not now
+								</span>
+							</div>
+						</div>
+					</div>
+					{/* <div></div> */}
+				</>
+			)}
+		</>
+	);
 };
 
 export default MyCustomJoyride;
--- tailwindui/components/user_onboarding/StepsSummaryPage.tsx
+++ tailwindui/components/user_onboarding/StepsSummaryPage.tsx
@@ -2,133 +2,86 @@
 import { CustomStep } from './MyCustomJoyride';
 import SummaryPageStep1Welcome from '@/public/images/user_onboarding/SummaryPageStep1Welcom.png';
 const StepsSummaryPage: () => CustomStep[] = () => {
-  const getImagePlaceholder = (index: number) => {
-    return `https://via.placeholder.com/300x200.png?text=Step+${index + 1}`;
-  };
-  const steps: CustomStep[] = [
-    {
-      target: 'body',
-      content: (
-        <div>
-          <img src={SummaryPageStep1Welcome.src} alt='Step 1' />
-          <div className='flex flex-col items-start gap-[0.5rem]'>
-            <div className='pt-[2rem] text-neutral-900 text-xl font-bold font-creato-bold leading-tight tracking-tight'>
-              Welcome onboard 🎉
-            </div>
-            <p className='text-neutral-800 text-base font-normal font-creato=regular leading-normal tracking-tight text-left'>
-              Start creating slides from summary page. Do you want to follow a
-              step-by step tutorial?
-            </p>
-          </div>
-        </div>
-      ),
-      locale: {
-        skip: (
-          <div
-            style={{
-              width: 100,
-              height: 40,
-              paddingLeft: 7.4,
-              paddingRight: 7.4,
-              paddingTop: 2.47,
-              paddingBottom: 2.47,
-              borderRadius: 4.94,
-              overflow: 'hidden',
-              border: '1px #2943E9 solid',
-              justifyContent: 'center',
-              alignItems: 'center',
-              gap: 6.17,
-              display: 'inline-flex',
-            }}
-          >
-            <div
-              style={{
-                textAlign: 'center',
-                color: '#2943E9',
-                fontSize: 15,
-                fontFamily: 'Creato Display',
-                fontWeight: '500',
-                lineHeight: 14.81,
-                letterSpacing: 0.6,
-                wordWrap: 'break-word',
-              }}
-            ></div>
-            Not now
-          </div>
-        ),
-        back: 'Back',
-        next: (
-          <div
-            style={{
-              width: 100,
-              height: 40,
-              paddingLeft: 7.4,
-              paddingRight: 7.4,
-              paddingTop: 2.47,
-              paddingBottom: 2.47,
-              background: '#2943E9',
-              borderRadius: 4.94,
-              overflow: 'hidden',
-              justifyContent: 'center',
-              alignItems: 'center',
-              gap: 6.17,
-              display: 'inline-flex',
-              textAlign: 'center',
-              color: '#F4F4F4',
-              fontSize: 15,
-              fontFamily: 'Creato Display',
-              fontWeight: '500',
-              lineHeight: 14.81,
-              letterSpacing: 0.6,
-              wordWrap: 'break-word',
-            }}
-          >
-            Sure
-          </div>
-        ),
-      },
-      styles: {
-        tooltip: {
-          width: '32rem', // Set the width of the tooltip
-          // Add any other styles as needed
-        },
-        // Add styles for other elements as needed
-      },
-      placement: 'center',
-      disableBeacon: true,
-      showProgress: false,
-    },
-    // Add more steps as needed
-    {
-      target: '#SummaryStep-2',
-      content: (
-        <div>
-          <h2>Step 2</h2>
-          <p>This step has another placeholder image.</p>
-          <img src={getImagePlaceholder(1)} alt='Step 2' />
-        </div>
-      ),
-      locale: { back: 'Back', next: 'Next' },
-      placement: 'bottom',
-    },
-    {
-      target: '#SummaryStep-3',
-      content: (
-        <div>
-          <h2>Step 3</h2>
-          <p>And here's a third step with an icon.</p>
-          <i
-            className='fa fa-star'
-            style={{ fontSize: '24px', color: 'gold' }}
-          ></i>
-        </div>
-      ),
-      locale: { back: 'Back', next: 'Next' },
-      placement: 'bottom',
-    },
-  ];
+	const getImagePlaceholder = (index: number) => {
+		return `https://via.placeholder.com/300x200.png?text=Step+${index + 1}`;
+	};
+	const steps: CustomStep[] = [
+		{
+			target: 'body',
+			content: (
+				<div className='h-auto z-50 flex flex-col'>
+					<img src={SummaryPageStep1Welcome.src} alt='Step 1' />
+					<div className='flex flex-col items-start gap-[0.5rem]'>
+						<div className='pt-[2rem] text-neutral-900 text-xl font-bold font-creato-bold leading-tight tracking-tight'>
+							Welcome onboard 🎉
+						</div>
+						<p className='text-neutral-800 text-base font-normal font-creato=regular leading-normal tracking-tight text-left'>
+							Start creating slides from summary page. Do you want to follow a
+							step-by step tutorial?
+						</p>
+					</div>
+				</div>
+			),
+			locale: {
+				skip: (
+					<div className='w-24 h-10 px-2 py-0.5 rounded border border-[#2943E9] justify-center items-center gap-1.5 inline-flex'>
+						<span className='text-center text-[#2943E9] text-base font-medium font-creato-medium leading-none tracking-wide'>
+							Not now
+						</span>
+					</div>
+				),
+				back: 'Back',
+				next: (
+					<div className='w-24 h-10 px-2 py-0.5 bg-[#2943E9] rounded justify-center items-center gap-1.5 inline-flex'>
+						<span className='text-center text-zinc-100 text-base font-medium font-creato-medium leading-none tracking-wide'>
+							Sure
+						</span>
+					</div>
+				),
+			},
+			styles: {
+				tooltip: {
+					width: '31rem', // Set the width of the tooltip
+					// Add any other styles as needed
+				},
+				// Add styles for other elements as needed
+			},
+			placement: 'center',
+			disableBeacon: true,
+			showProgress: true,
+		},
+		// Add more steps as needed
+		{
+			target: '#SummaryStep-2',
+			content: (
+				<div>
+					<h2>Step 2</h2>
+					<p>This step has another placeholder image.</p>
+					<img src={getImagePlaceholder(1)} alt='Step 2' />
+				</div>
+			),
 
-  return steps;
+			locale: { back: 'Back', next: 'Next' },
+			placement: 'bottom',
+		},
+		{
+			target: '#SummaryStep-3',
+			content: (
+				<div>
+					<h2>Step 3</h2>
+					<p>And here's a third step with an icon.</p>
+					<i
+						className='fa fa-star'
+						style={{ fontSize: '24px', color: 'gold' }}
+					></i>
+				</div>
+			),
+			locale: { back: 'Back', next: 'Next' },
+			placement: 'bottom',
+		},
+	];
+
+	return steps;
 };
 
 export default StepsSummaryPage;
