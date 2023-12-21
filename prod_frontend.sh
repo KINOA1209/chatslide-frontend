a19ac01c4950364b5ab75deaaf4f82ebd1c82bb6
Zhiyuan Wang <wangzyzju@gmail.com>
2023-12-21 08:50:05 -0800

    [infra-build] add production frontend server workflow

--- prod_frontend.sh
+++ prod_frontend.sh
@@ -1,2 +1,25 @@
+BUILD_PATH="/home/ubuntu/drlambda-frontend/build"
+mkdir -p BUILD_PATH
+cd BUILD_PATH
 
-curl https://dev.drlambda.ai/api/build_frontend\?key\=ayeP6I0oyRemxMa10sCrAgUtkfL5ADzjr3IKImXS7rTdbpXWPmSUoROo5jZbODjX\&lazy\=true --output latest-build.tar.gz
+timestamp=$(date "+%Y%m%d%H%M%S")
+
+# Download and save file
+BUILD_FILE="latest-build.tar.gz"
+curl https://dev.drlambda.ai/api/build_frontend\?key\=ayeP6I0oyRemxMa10sCrAgUtkfL5ADzjr3IKImXS7rTdbpXWPmSUoROo5jZbODjX\&lazy\=true --output $BUILD_FILE
+
+# Check if the download was successful
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+# Decompress the file
+tar -xzvf $BUILD_FILE
+
+# archive the file with current timestamp
+if [ -f "$BUILD_FILE" ]; then
+    mv $BUILD_FILE "$BUILD_FILE.$timestamp"
+fi
+
+# restart frontend
+pm2 restart tailwindui
