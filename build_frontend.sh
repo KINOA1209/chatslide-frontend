91a08cc6dd416caa1aeec371c35b3ca77cda3293
Zhiyuan Wang <wangzyzju@gmail.com>
2023-12-21 08:32:29 -0800

    [infra-build] optimize build logic: compare if git branch is master and skips building if git pull has no updates

--- build_frontend.sh
+++ build_frontend.sh
@@ -5,10 +5,29 @@ original_dir=$(pwd)
 
 # Navigate to the frontend build directory
 cd /home/ubuntu/build/drlambda-frontend/tailwindui
-git pull
+# Check if the directory change was successful
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
-npm install
-npm run build
+# Check if the current git branch is 'master'
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$current_branch" != "master" ]; then
+    exit 1
+fi
+
+# Get the latest commit before and after git pull
+commit_before_pull=$(git rev-parse HEAD)
+git pull
+commit_after_pull=$(git rev-parse HEAD)
+
+# Compare commits and run 'npm run build' if they are different
+if [ "$commit_before_pull" != "$commit_after_pull" ]; then
+    npm install
+    npm run build
+else
+    echo "No change detected, skip building."
+fi
 
 latest_commit=$(git log -1 --format="%H")
 echo "{\"version\": \"$latest_commit\"}" > .next/metadata.json
---
+++ prod_frontend.sh
@@ -0,0 +1,2 @@
+
+curl https://dev.drlambda.ai/api/build_frontend\?key\=ayeP6I0oyRemxMa10sCrAgUtkfL5ADzjr3IKImXS7rTdbpXWPmSUoROo5jZbODjX\&lazy\=true --output latest-build.tar.gz
