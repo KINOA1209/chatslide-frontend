ec33e0786b41e0d8d84a42e51d485e0109ca651b
Zhiyuan Wang <wangzyzju@gmail.com>
2023-12-21 08:07:17 -0800

    [infra-build] soft link to latest-build.tar.gz

--- build_frontend.sh
+++ build_frontend.sh
@@ -15,8 +15,11 @@ echo "{\"version\": \"$latest_commit\"}" > .next/metadata.json
 
 timestamp=$(date "+%Y%m%d%H%M%S")
 tar -czvf frontend-build.tar.gz.$timestamp .next
-mkdir -p /home/ubuntu/build/drlambda-frontend/bin
-mv frontend-build.tar.gz.$timestamp /home/ubuntu/build/drlambda-frontend/bin/.
+
+BIN_PATH="/home/ubuntu/build/drlambda-frontend/bin"
+mkdir -p "$BIN_PATH"
+mv frontend-build.tar.gz.$timestamp "$BIN_PATH"
+ln -s $BIN_PATH/frontend-build.tar.gz.$timestamp $BIN_PATH/latest-build.tar.gz
 
 # Return to the original directory
 cd "$original_dir"
