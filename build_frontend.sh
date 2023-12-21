63ffed894d154e900a6aab1f44ec8b4095e03bd1
Zhiyuan Wang <wangzyzju@gmail.com>
2023-12-21 00:46:58 -0800

    update build frontend script

--- build_frontend.sh
+++ build_frontend.sh
@@ -4,13 +4,16 @@
 original_dir=$(pwd)
 
 # Navigate to the frontend build directory
-cd /home/ubuntu/build/drlambda-frontend
+cd /home/ubuntu/build/drlambda-frontend/tailwindui
+git pull
 
 npm install
-# Your complex series of commands for building the frontend
 npm run build
 
-# ... other operations ...
+latest_commit=$(git log -1 --format="%H")
+echo "{\"version\": \"$latest_commit\"}" > .next/metadata.json
+
+tar -czvf next-build.tar.gz .next
 
 # Return to the original directory
 cd "$original_dir"
