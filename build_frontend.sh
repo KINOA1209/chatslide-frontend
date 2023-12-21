aa5fa7abfe27908cea9f453fb3f0a25b5cfc7653
Zhiyuan Wang <wangzyzju@gmail.com>
2023-12-21 00:59:17 -0800

    move build files to trusted archive locations

--- build_frontend.sh
+++ build_frontend.sh
@@ -13,7 +13,10 @@ npm run build
 latest_commit=$(git log -1 --format="%H")
 echo "{\"version\": \"$latest_commit\"}" > .next/metadata.json
 
-tar -czvf next-build.tar.gz .next
+timestamp=$(date "+%Y%m%d%H%M%S")
+tar -czvf frontend-build.tar.gz.$timestamp .next
+mkdir -p /home/ubuntu/build/drlambda-frontend/bin
+mv frontend-build.tar.gz.$timestamp /home/ubuntu/build/drlambda-frontend/bin/.
 
 # Return to the original directory
 cd "$original_dir"
