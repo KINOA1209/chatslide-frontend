11644f0bc012efcc3e32349c082363796487ae05
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-12-22 12:53:59 -0800

    step2 and step 3

--- tailwindui/components/user_onboarding/StepsSummaryPage.tsx
+++ tailwindui/components/user_onboarding/StepsSummaryPage.tsx
@@ -58,7 +58,7 @@ const StepsSummaryPage: () => CustomStep[] = () => {
 				<TutorialStepContent
 					action={'Enter basic information 💡'}
 					explanation={
-						'Offer more brilliant materials, your decks will been engaged with more depth.'
+						'To get started, give us some high-level intro about your project. The more you enter here, the more precise content we could generate for you.'
 					}
 				></TutorialStepContent>
 			),
@@ -80,21 +80,48 @@ const StepsSummaryPage: () => CustomStep[] = () => {
 				),
 			},
 			placement: 'bottom',
+			styles: {
+				tooltip: {
+					width: '31rem', // Set the width of the tooltip
+					// Add any other styles as needed
+				},
+				// Add styles for other elements as needed
+			},
 		},
 		{
 			target: '#SummaryStep-3',
 			content: (
-				<div>
-					<h2>Step 3</h2>
-					<p>And here's a third step with an icon.</p>
-					<i
-						className='fa fa-star'
-						style={{ fontSize: '24px', color: 'gold' }}
-					></i>
-				</div>
+				<TutorialStepContent
+					action={'Upload supporting references 🎥 📄'}
+					explanation={
+						'Offer more brilliant materials, your decks will been engaged with more depth.'
+					}
+				></TutorialStepContent>
 			),
-			locale: { back: 'Back', next: 'Next' },
+			locale: {
+				back: (
+					<div className='w-24 h-10 px-2 py-0.5 rounded border border-[#FFFFFF] justify-center items-center gap-1.5 inline-flex'>
+						<span className='text-center text-[#2943E9] text-base font-medium font-creato-medium leading-none tracking-wide'>
+							Back
+						</span>
+					</div>
+				),
+				last: (
+					<div className='w-24 h-10 px-2 py-0.5 bg-[#2943E9] rounded justify-center items-center gap-1.5 inline-flex'>
+						<span className='text-center text-zinc-100 text-base font-medium font-creato-medium leading-none tracking-wide'>
+							End tour
+						</span>
+					</div>
+				),
+			},
 			placement: 'bottom',
+			styles: {
+				tooltip: {
+					width: '31rem', // Set the width of the tooltip
+					// Add any other styles as needed
+				},
+				// Add styles for other elements as needed
+			},
 		},
 	];
 
