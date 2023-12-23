bc3c58967ba9a150901678e4f399c1aa4bcf9fa1
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2023-12-23 15:11:19 -0800

    [toPdf] use token to fetch project details

--- tailwindui/app/toPdf/[project_id]/sharePage.tsx
+++ tailwindui/app/toPdf/[project_id]/sharePage.tsx
@@ -2,11 +2,10 @@
 
 import { usePathname, useSearchParams } from 'next/navigation';
 import { useState, useEffect } from 'react';
-import { Slide } from '@/components/slides/SlidesHTML';
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import dynamic from 'next/dynamic';
 import ProjectService from '@/services/ProjectService';
+import Slide from '@/models/Slide';
 
 const SlidesHTML = dynamic(() => import('@/components/slides/SlidesHTML'), {
 	ssr: false,
@@ -17,13 +16,14 @@ const SharePage: React.FC = () => {
 	const project_id = pathname?.split('/').pop();
 	const [loading, setLoading] = useState(true);
   const page = useSearchParams()?.get('page') || 0;
+  const token = useSearchParams()?.get('token') || '';
   const [slides, setSlides] = useState<Slide[]>([]);
 
 	useEffect(() => {
 		if (project_id) {
-      sessionStorage.removeItem('foldername');
       setLoading(true);
-      ProjectService.getFolderName(project_id).then( () => {
+      ProjectService.getProjectDetails(token, project_id).then((data) => {
+        setSlides(JSON.parse(data.presentation_slides));
         setLoading(false);
       });
 		}
