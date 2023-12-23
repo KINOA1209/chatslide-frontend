eedce052b844c192211a5aa2a560c57e4157c0c0
Zhiyuan Wang <wangzyzju@gmail.com>
2023-12-22 18:58:51 -0800

    [infra-build] add logging to build_frontend.sh

--- build_frontend.sh
+++ build_frontend.sh
@@ -7,12 +7,14 @@ original_dir=$(pwd)
 cd /home/ubuntu/build/drlambda-frontend/tailwindui
 # Check if the directory change was successful
 if [ $? -ne 0 ]; then
+    echo "failed change to working directory, exiting..."
     exit 1
 fi
 
 # Check if the current git branch is 'master'
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 if [ "$current_branch" != "master" ]; then
+    echo "current git branch is not master, exiting..."
     exit 1
 fi
 
@@ -23,6 +25,7 @@ commit_after_pull=$(git rev-parse HEAD)
 
 # Compare commits and run 'npm run build' if they are different
 if [ "$commit_before_pull" != "$commit_after_pull" ]; then
+    echo "New changes pulled, npm installing and building"
     npm install
     npm run build
 else
@@ -32,13 +35,16 @@ fi
 latest_commit=$(git log -1 --format="%H")
 echo "{\"version\": \"$latest_commit\"}" > .next/metadata.json
 
+echo "Compressing .next directory"
 timestamp=$(date "+%Y%m%d%H%M%S")
 tar -czvf frontend-build.tar.gz.$timestamp .next
 
+echo "Moving binary"
 BIN_PATH="/home/ubuntu/build/drlambda-frontend/bin"
 mkdir -p "$BIN_PATH"
 mv frontend-build.tar.gz.$timestamp "$BIN_PATH"
 ln -f -s $BIN_PATH/frontend-build.tar.gz.$timestamp $BIN_PATH/latest-build.tar.gz
 
+echo "Successfully completed build"
 # Return to the original directory
 cd "$original_dir"
