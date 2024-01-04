a06b1a1cf03b516401226a5c85c97a67045e21e5
Zhiyuan Wang <wangzyzju@gmail.com>
2024-01-04 02:08:12 -0800

    [video] update VideoService to return job_id from generate video and use that to query job status

--- tailwindui/services/VideoService.tsx
+++ tailwindui/services/VideoService.tsx
@@ -1,26 +1,43 @@
+import VideoJobStatus from "@/models/VideoJobStatus";
+
 export default class VideoService {
+	// Returns a promise of the video generation job as a string, which could be used later to query the job status.
 	static async generateVideo(
 		project_id: string,
 		foldername: string | null,
 		language: string | null,
 		userToken: string,
 	): Promise<string> {
-		try {
-			const response = await fetch('/api/generate_video', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${userToken}`, // Replace this with your actual auth header
-				},
-				body: JSON.stringify({ project_id: project_id, foldername: foldername, language: language }),
-			});
+		const response = await fetch('/api/generate_video', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${userToken}`, // Replace this with your actual auth header
+			},
+			body: JSON.stringify({ project_id: project_id, foldername: foldername, language: language }),
+		});
 
-			if (!response.ok) {
-				throw new Error(`HTTP error! status: ${response.status}`);
-			}
-			return 'output.mp4';
-		} catch (error) {
-			throw error;
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
 		}
+
+		return await response.json().then(result => result['job_id']);
+	}
+
+	// Returns the VideoJobStatus object which has both status and video url if the job is completed.
+	static async getVideoJobStatus(job_id: string, userToken: string): Promise<VideoJobStatus> {
+		const response = await fetch(`/api/video_job_status?job_id=${job_id}`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${userToken}`, // Replace this with your actual auth header
+			},
+		});
+
+		if (!response.ok) {
+			throw new Error(`Error fetching video job ${job_id} status: ${response.status}`);
+		}
+
+		return await response.json() as VideoJobStatus;
 	}
 }
