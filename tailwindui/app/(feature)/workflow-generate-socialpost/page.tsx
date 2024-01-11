fe3d81ba217bb4e795c212f5428eaefe31554850
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-01-11 11:15:54 -0800

    fix build issue

--- tailwindui/app/(feature)/workflow-generate-socialpost/page.tsx
+++ tailwindui/app/(feature)/workflow-generate-socialpost/page.tsx
@@ -487,8 +487,6 @@ export default function Topic_SocialPost() {
 					setIsGpt35={setIsGpt35}
 				/>
 				{/* main content */}
-				<div className='py-10 gap-y-4 w-full flex flex-col items-center'>
-					<GPTToggleWithExplanation setIsGpt35={setIsGpt35} />
 				<div className='gap-y-4 w-full flex flex-col items-center'>
 
           <GPTToggleWithExplanation setIsGpt35={setIsGpt35} />
