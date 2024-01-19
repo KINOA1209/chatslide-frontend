dc662c32e213b053e83dbc4f229f6914cebb1e2c
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-01-18 16:33:37 -0800

    [seo] add description on shared page

--- tailwindui/app/shared/[project_id]/sharePage.tsx
+++ tailwindui/app/shared/[project_id]/sharePage.tsx
@@ -10,18 +10,20 @@ import dynamic from 'next/dynamic';
 import ProjectService from '@/services/ProjectService';
 import Slide from '@/models/Slide';
 import Project from '@/models/Project';
+import { FaTimes } from 'react-icons/fa';
 
 const SlidesHTML = dynamic(() => import('@/components/slides/SlidesHTML'), {
-	ssr: false,
+  ssr: false,
 });
 
 const SharePage: React.FC = () => {
-	const router = useRouter();
-	const pathname = usePathname();
-	const project_id = pathname?.split('/').pop();
-	const [loading, setLoading] = useState(true);
-
-	const [slides, setSlides] = useState<Slide[]>([]);
+  const router = useRouter();
+  const pathname = usePathname();
+  const project_id = pathname?.split('/').pop();
+  const [loading, setLoading] = useState(true);
+  const [description, setDescription] = useState<string>('');
+  const [slides, setSlides] = useState<Slide[]>([]);
+  const [showDescription, setShowDescription] = useState<boolean>(true);
 
   useEffect(() => {
     if (project_id) {
@@ -29,32 +31,46 @@ const SharePage: React.FC = () => {
       setLoading(true);
       ProjectService.getSharedProjectDetails(project_id).then((project: Project) => {
         setSlides(project.parsed_slides);
+        setDescription(project.description);
         setLoading(false);
       });
     }
   }, [project_id]);
 
-	return (
-		<main className='grow'>
-			<Header loginRequired={false} isLanding={false} />
-			<ToastContainer />
-			{loading ? (
+  return (
+    <main className='grow'>
+      <Header loginRequired={false} isLanding={false} />
+      <ToastContainer />
+      {loading ? (
         <div className='flex items-center justify-center min-h-screen'>Loading...</div>
-			) : (
-				<div className='flex items-center justify-center min-h-screen'>
-					<div>
-						<SlidesHTML
-							slides={slides}
-							setSlides={setSlides}
-							isViewing={true}
-						/>
-					</div>
-				</div>
-			)}
+      ) : (
+        <div className='flex flex-col items-center justify-center min-h-screen gap-8'>
+          {showDescription && description &&
+            <div className="flex sm:w-2/3 h-[10rem]text-gray-700 text-left m-2 gap-4">
+                <div className="border border-gray-200 rounded-xl overflow-y-scroll p-2">
+                {description}
+              </div>
+              <button
+                className="text-gray-500 hover:text-gray-700"
+                onClick={() => setShowDescription(false)}
+              >
+                <FaTimes />
+              </button>
+            </div>
+          }
+          <div>
+            <SlidesHTML
+              slides={slides}
+              setSlides={setSlides}
+              isViewing={true}
+            />
+          </div>
+        </div>
+      )}
 
-			<WorkflowFooter />
-		</main>
-	);
+      <WorkflowFooter />
+    </main>
+  );
 };
 
 export default SharePage;
