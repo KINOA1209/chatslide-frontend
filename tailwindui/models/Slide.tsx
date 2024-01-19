bbfdb37712de77c55c5e542c87667f49e75b8eb0
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-01-19 13:11:47 -0800

    [slide][model] add background_url

--- tailwindui/models/Slide.tsx
+++ tailwindui/models/Slide.tsx
@@ -42,6 +42,7 @@ export default class Slide {
   layout: LayoutKeys;
   logo: string;  // enum for school tempaltes, if user has custom logo, then use logo_url
   logo_url?: string;  // overwrites logo if present
+  backgrouund_url?: string;
 
   constructor() {
     this.head = 'New Slide';
