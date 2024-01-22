975984ef7ce1ceb84cd16ae0d41a9cbe26aaa614
Zhiyuan Wang <wangzyzju@gmail.com>
2024-01-21 16:46:06 -0800

    [deploy] explicitly define aws-exports.js to test amplify hosting

---
+++ tailwindui/aws-exports.js
@@ -0,0 +1,47 @@
+const awsmobile = {
+    "aws_project_region": "us-east-2",
+    "aws_cognito_identity_pool_id": "us-east-2:474762b3-d386-4c33-84c8-c532098d98e4",
+    "aws_cognito_region": "us-east-2",
+    "aws_user_pools_id": "us-east-2_XgJbG8iv1",
+    "aws_user_pools_web_client_id": "5s1gm29qjolp4mv9v35gcstpog",
+    "oauth": {
+        "domain": "yfbjukov7va1-staging.auth.us-east-2.amazoncognito.com",
+        "scope": [
+            "phone",
+            "email",
+            "openid",
+            "profile",
+            "aws.cognito.signin.user.admin"
+        ],
+        "redirectSignIn": "https://drlambda.ai/sso",
+        "redirectSignOut": "https://drlambda.ai/",
+        "responseType": "code"
+    },
+    "federationTarget": "COGNITO_USER_POOLS",
+    "aws_cognito_username_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_social_providers": [
+        "GOOGLE"
+    ],
+    "aws_cognito_signup_attributes": [],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": [
+            "REQUIRES_LOWERCASE",
+            "REQUIRES_NUMBERS",
+            "REQUIRES_SYMBOLS",
+            "REQUIRES_UPPERCASE"
+        ]
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
+};
+
+
+export default awsmobile;
