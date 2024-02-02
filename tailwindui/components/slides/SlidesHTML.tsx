ec81e308cfb992f4fa580b007631710e8236212f
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-02-02 09:53:08 -0800

    [agent] show button

--- tailwindui/components/slides/SlidesHTML.tsx
+++ tailwindui/components/slides/SlidesHTML.tsx
@@ -393,7 +393,7 @@ const SlidesHTML: React.FC<SlidesHTMLProps> = ({
 
 			{/* absolute positionde ai assistant icon */}
 			{!isChatWindowOpen && (
-				<div className='sm:fixed bottom-10 right-10 cursor-pointer'>
+				<div className='hidden sm:block fixed bottom-10 right-10 cursor-pointer'>
 					<ButtonWithExplanation
 						button={
 							<DrLambdaAIAssistantIcon
