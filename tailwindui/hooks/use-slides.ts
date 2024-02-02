20242bf32a3e2bf8ac091000fe20ba91095b1a59
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2024-02-02 14:02:08 -0800

    [bug fix] slide page template choice is not reflected in the selection box

--- tailwindui/hooks/use-slides.ts
+++ tailwindui/hooks/use-slides.ts
@@ -6,185 +6,216 @@ import { useUser } from './use-user';
 
 const useSlidesBear = createBearStore<Slide[]>()('slides', [], true);
 const useSlideIndex = createBearStore<number>()('slideIndex', 0, true);
-const useSlidesHistoryBear = createBearStore<Slide[][]>()('slidesHistory', [], true);
-const useSlidesHistoryIndex = createBearStore<number>()('slidesHistoryIndex', 0, true);
+const useSlidesHistoryBear = createBearStore<Slide[][]>()(
+	'slidesHistory',
+	[],
+	true,
+);
+const useSlidesHistoryIndex = createBearStore<number>()(
+	'slidesHistoryIndex',
+	0,
+	true,
+);
 const useVersion = createBearStore<number>()('version', 0, true);
 
 export enum SlidesStatus {
-  NotInited,
-  Initing,
-  Inited,
+	NotInited,
+	Initing,
+	Inited,
 }
 
 export enum SaveStatus {
-  UpToDate,
-  Saving,
+	UpToDate,
+	Saving,
 }
 
 let slidesStatus: SlidesStatus = SlidesStatus.NotInited;
 let saveStatus: SaveStatus = SaveStatus.UpToDate;
 
 export const useSlides = () => {
-  const { slides, setSlides } = useSlidesBear();
-  const { slideIndex, setSlideIndex } = useSlideIndex();
-  const { slidesHistory, setSlidesHistory } = useSlidesHistoryBear();
-  const { slidesHistoryIndex, setSlidesHistoryIndex } = useSlidesHistoryIndex();
-  const { version, setVersion } = useVersion();
-  const { token } = useUser();
+	const { slides, setSlides } = useSlidesBear();
+	const { slideIndex, setSlideIndex } = useSlideIndex();
+	const { slidesHistory, setSlidesHistory } = useSlidesHistoryBear();
+	const { slidesHistoryIndex, setSlidesHistoryIndex } = useSlidesHistoryIndex();
+	const { version, setVersion } = useVersion();
+	const { token } = useUser();
 
-  const init = async () => {
-    if (slidesStatus !== SlidesStatus.NotInited) return;
-    slidesStatus = SlidesStatus.Initing;
+	const init = async () => {
+		if (slidesStatus !== SlidesStatus.NotInited) return;
+		slidesStatus = SlidesStatus.Initing;
 
-    setSlides([]);
-    setSlideIndex(0);
-    setSlidesHistory([]);
-    setSlidesHistoryIndex(0);
+		setSlides([]);
+		setSlideIndex(0);
+		setSlidesHistory([]);
+		setSlidesHistoryIndex(0);
 
-    console.log('-- init slides: ', { slidesStatus, slides })
+		console.log('-- init slides: ', { slidesStatus, slides });
 
-    slidesStatus = SlidesStatus.Inited;
-  }
+		slidesStatus = SlidesStatus.Inited;
+	};
 
-  useEffect(() => {
-    void init();
-  }, []);
+	useEffect(() => {
+		void init();
+	}, []);
 
-  const addEmptyPage = (index: number) => {
-    console.log('-- add empty page: ', { index })
-    const newSlides = [...slides];
-    newSlides.splice(index, 0, new Slide());
-    setSlides(newSlides);
+	const addEmptyPage = (index: number) => {
+		console.log('-- add empty page: ', { index });
+		const newSlides = [...slides];
+		newSlides.splice(index, 0, new Slide());
+		setSlides(newSlides);
 
-    updateVersion();
-    updateSlideHistory();
-    syncSlides(newSlides);
-  }
+		updateVersion();
+		updateSlideHistory();
+		syncSlides(newSlides);
+	};
 
-  const deleteSlidePage = (index: number) => {
-    console.log('-- delete slide page: ', { index })
-    const newSlides = [...slides];
-    newSlides.splice(index, 1);
-    setSlides(newSlides);
+	const deleteSlidePage = (index: number) => {
+		console.log('-- delete slide page: ', { index });
+		const newSlides = [...slides];
+		newSlides.splice(index, 1);
+		setSlides(newSlides);
 
-    updateVersion();
-    updateSlideHistory();
-    syncSlides(newSlides);
-  }
+		updateVersion();
+		updateSlideHistory();
+		syncSlides(newSlides);
+	};
 
-  const updateSlidePage = (index: number, slide: Slide) => {
-    console.log('-- update slide page: ', { index, slide })
-    const newSlides = [...slides];
-    newSlides[index] = slide;
-    setSlides(newSlides);
+	const updateSlidePage = (index: number, slide: Slide) => {
+		console.log('-- update slide page: ', { index, slide });
+		const newSlides = [...slides];
+		newSlides[index] = slide;
+		setSlides(newSlides);
 
-    updateVersion();
-    updateSlideHistory();
-    syncSlides(newSlides, index===0);
-  }
+		updateVersion();
+		updateSlideHistory();
+		syncSlides(newSlides, index === 0);
+	};
 
-  const gotoPage = (index: number) => {
-    console.log('-- goto page: ', { index })
-    if (index < 0 || index >= slides.length) return;
-    setSlideIndex(index);
-  }
+	const gotoPage = (index: number) => {
+		console.log('-- goto page: ', { index });
+		if (index < 0 || index >= slides.length) return;
+		setSlideIndex(index);
+	};
 
-  const updateSlideHistory = () => {
-    console.log('-- slides changed, adding to history: ', { slides })
-    if (slidesHistory.length >= 10) {  // Only keep 10 versions
-      setSlidesHistory(prevHistory => prevHistory.slice(prevHistory.length - 9));
-    }
-    setSlidesHistory((prevHistory) => [...prevHistory, slides]);
-    setSlidesHistoryIndex((prevIndex) => prevIndex + 1);
-  }
+	const updateSlideHistory = () => {
+		console.log('-- slides changed, adding to history: ', { slides });
+		if (slidesHistory.length >= 10) {
+			// Only keep 10 versions
+			setSlidesHistory((prevHistory) =>
+				prevHistory.slice(prevHistory.length - 9),
+			);
+		}
+		setSlidesHistory((prevHistory) => [...prevHistory, slides]);
+		setSlidesHistoryIndex((prevIndex) => prevIndex + 1);
+	};
 
-  const undoChange = () => {
-    if (slidesHistoryIndex > 0) {
-      setSlidesHistoryIndex((prevIndex) => prevIndex - 1);
-      setSlides(slidesHistory[slidesHistoryIndex - 1]);
-    }
-    console.log('Performing undo...');
-    document.execCommand('undo', false, undefined); // Change null to undefined
-  };
+	const undoChange = () => {
+		if (slidesHistoryIndex > 0) {
+			setSlidesHistoryIndex((prevIndex) => prevIndex - 1);
+			setSlides(slidesHistory[slidesHistoryIndex - 1]);
+		}
+		console.log('Performing undo...');
+		document.execCommand('undo', false, undefined); // Change null to undefined
+	};
 
-  const redoChange = () => {
-    if (slidesHistoryIndex < slidesHistory.length - 1) {
-      setSlidesHistoryIndex((prevIndex) => prevIndex + 1);
-      setSlides(slidesHistory[slidesHistoryIndex + 1]);
-    }
-    // Add your redo logic here
-    console.log('Performing redo...');
-    document.execCommand('redo', false, undefined); // Change null to undefined
-  };
+	const redoChange = () => {
+		if (slidesHistoryIndex < slidesHistory.length - 1) {
+			setSlidesHistoryIndex((prevIndex) => prevIndex + 1);
+			setSlides(slidesHistory[slidesHistoryIndex + 1]);
+		}
+		// Add your redo logic here
+		console.log('Performing redo...');
+		document.execCommand('redo', false, undefined); // Change null to undefined
+	};
 
-  const changeTemplate = (newTemplate: TemplateKeys) => {
-    console.log('Changing template to:', newTemplate);
-    const newSlides = slides.map((slide, index) => {
-      return { ...slide, template: newTemplate };
-    });
-    setSlides(newSlides);
-    
-    updateSlideHistory();
-    syncSlides(newSlides);
-  }
+	const changeTemplate = (newTemplate: TemplateKeys) => {
+		console.log('Changing template to:', newTemplate);
+		const newSlides = slides.map((slide, index) => {
+			return { ...slide, template: newTemplate };
+		});
+		//set into session storage to update
+		sessionStorage.setItem('schoolTemplate', newTemplate);
+		setSlides(newSlides);
 
-  const initSlides = (slides: Slide[]) => {
-    setSlides(slides);
-    slidesStatus = SlidesStatus.Inited;
-  }
+		updateSlideHistory();
+		syncSlides(newSlides);
+	};
 
-  const updateVersion = () => {  // helps force update slide container
-    setVersion((prevVersion) => prevVersion + 1);
-  }
+	const initSlides = (slides: Slide[]) => {
+		setSlides(slides);
+		slidesStatus = SlidesStatus.Inited;
+	};
 
-  const syncSlides = async (slides: Slide[], is_cover_page: boolean = false) => {
-    saveStatus = SaveStatus.Saving;
+	const updateVersion = () => {
+		// helps force update slide container
+		setVersion((prevVersion) => prevVersion + 1);
+	};
 
-    const foldername = sessionStorage.getItem('foldername');
-    const project_id = sessionStorage.getItem('project_id');
+	const syncSlides = async (
+		slides: Slide[],
+		is_cover_page: boolean = false,
+	) => {
+		saveStatus = SaveStatus.Saving;
 
-    if (!foldername || !project_id || !token) {
-      console.error('No foldername, project_id or token found. Cannot save slides.');
-      return;
-    }
+		const foldername = sessionStorage.getItem('foldername');
+		const project_id = sessionStorage.getItem('project_id');
 
-    const formData = {
-      foldername: foldername,
-      final_slides: slides,
-      project_id: project_id,
-      is_cover_page: is_cover_page,
-    };
+		if (!foldername || !project_id || !token) {
+			console.error(
+				'No foldername, project_id or token found. Cannot save slides.',
+			);
+			return;
+		}
 
-    console.log('Saving slides:', formData);
+		const formData = {
+			foldername: foldername,
+			final_slides: slides,
+			project_id: project_id,
+			is_cover_page: is_cover_page,
+		};
 
-    // Send a POST request to the backend to save finalSlides
-    fetch('/api/save_slides', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((response) => {
-        if (response.ok) {
-          saveStatus = SaveStatus.UpToDate;
-          console.log('Auto-save successful.');
-        } else {
-          // Handle save error
-          console.error('Auto-save failed.');
-        }
-      })
-      .catch((error) => {
-        // Handle network error
-        console.error('Auto-save failed:', error);
-      });
-  };
+		console.log('Saving slides:', formData);
 
-  return {
-    slides, addEmptyPage, deleteSlidePage, updateSlidePage, changeTemplate, initSlides,
-    slidesHistory, slidesHistoryIndex, undoChange, redoChange,
-    slideIndex, gotoPage, slidesStatus, 
-    version, updateVersion, saveStatus
-  };
+		// Send a POST request to the backend to save finalSlides
+		fetch('/api/save_slides', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json; charset=utf-8',
+				Authorization: `Bearer ${token}`,
+			},
+			body: JSON.stringify(formData),
+		})
+			.then((response) => {
+				if (response.ok) {
+					saveStatus = SaveStatus.UpToDate;
+					console.log('Auto-save successful.');
+				} else {
+					// Handle save error
+					console.error('Auto-save failed.');
+				}
+			})
+			.catch((error) => {
+				// Handle network error
+				console.error('Auto-save failed:', error);
+			});
+	};
+
+	return {
+		slides,
+		addEmptyPage,
+		deleteSlidePage,
+		updateSlidePage,
+		changeTemplate,
+		initSlides,
+		slidesHistory,
+		slidesHistoryIndex,
+		undoChange,
+		redoChange,
+		slideIndex,
+		gotoPage,
+		slidesStatus,
+		version,
+		updateVersion,
+		saveStatus,
+	};
 };
