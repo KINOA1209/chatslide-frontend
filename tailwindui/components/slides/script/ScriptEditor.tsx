6ff81f027c582480ee0e11443ad3800c46c3cffa
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2024-02-05 23:02:40 -0800

    fix import scriptbutton bug

--- tailwindui/components/slides/script/ScriptEditor.tsx
+++ tailwindui/components/slides/script/ScriptEditor.tsx
@@ -1,22 +1,22 @@
 import Slide from '@/models/Slide';
 import React, { useState } from 'react';
-import SaveScriptsButton from './saveTranscripts';
+import SaveScriptsButton from './SaveScriptsButton';
 
 interface TranscriptEditorProps {
 	slides: Slide[];
-  updateSlidePage: (index: number, slide: Slide) => void;
+	updateSlidePage: (index: number, slide: Slide) => void;
 	currentSlideIndex: number;
 }
 
 const ScriptEditor: React.FC<TranscriptEditorProps> = ({
-  slides,
-  updateSlidePage,
+	slides,
+	updateSlidePage,
 	currentSlideIndex,
 }) => {
 	const updateTranscriptList = (newValue: string) => {
-    const newSlide = slides[currentSlideIndex];
-    newSlide.transcript = newValue;
-    updateSlidePage(currentSlideIndex, newSlide);
+		const newSlide = slides[currentSlideIndex];
+		newSlide.transcript = newValue;
+		updateSlidePage(currentSlideIndex, newSlide);
 	};
 
 	return (
@@ -31,14 +31,12 @@ const ScriptEditor: React.FC<TranscriptEditorProps> = ({
 		>
 			<div className='px-4 py-2 h-12 bg-zinc-100 flex flex-row justify-between items-center sticky top-0 border-b-2 border-gray-300'>
 				<div className='text-neutral-900 text-s font-creato-medium'>Script</div>
-				<SaveScriptsButton slides={slides}/>
+				<SaveScriptsButton slides={slides} />
 			</div>
 			<textarea
 				className='grow px-4 py-2 w-full h-full border-none text-gray-700 bg-zinc-100 text-xs font-normal font-creato-medium leading-[1.125rem] tracking-[0.015rem]'
 				value={slides[currentSlideIndex].transcript}
-				onChange={(e) =>
-					updateTranscriptList(e.target.value)
-				}
+				onChange={(e) => updateTranscriptList(e.target.value)}
 			>
 				{slides[currentSlideIndex].transcript}
 			</textarea>
