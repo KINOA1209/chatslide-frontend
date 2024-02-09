ca0962eefcea48251fe0570958d031e3d426a9f6
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-02-08 20:43:28 -0800

    [terms] fair usage policy, and image license

--- tailwindui/app/(default)/terms/page.tsx
+++ tailwindui/app/(default)/terms/page.tsx
@@ -2,56 +2,56 @@ import Footer from '@/components/ui/footer';
 import Header from '@/components/ui/header';
 
 export const metadata = {
-	title: 'Terms - DrLambda',
-	description: "DrLambda's Terms and Conditions",
+  title: 'Terms - DrLambda',
+  description: "DrLambda's Terms and Conditions",
 };
 
 export default function Terms() {
-	return (
-		<section className='bg-gradient-to-b from-gray-100 to-white'>
-			<Header loginRequired={false} isLanding={false} />
-			<div className='max-w-6xl mx-auto px-4 sm:px-6'>
-				<div className='pt-32 pb-12 md:pt-40 md:pb-20'>
-					{/* Page header */}
-					<div className='max-w-3xl mx-auto text-center pb-12 md:pb-16'>
-						<h1 className='h1'>Terms and Conditions</h1>
-					</div>
-					<div className='max-w-3xl mx-auto'>
-						<div className='pb-16'>
-							<p className='text-base text-gray-600'>
-								Please read these terms and conditions carefully before using
-								our website and services provided by DrLambda. By accessing or
-								using our Service, you agree to be bound by these Terms. If you
-								do not agree with any part of these Terms, you may not access
-								the Service.
-							</p>
-						</div>
-					</div>
-					{/* Page sections */}
-					<div className='max-w-3xl mx-auto pb-4'>
-						<div className='pb-8'>
-							<h2 className='h3 text-2xl'>1. Use of Service</h2>
-						</div>
-						<div className='pb-4'>
-							<p className='text-base text-gray-600'>
-								1.1 Account Creation: In order to use certain features of the
-								Service, you may be required to create an account. You agree to
-								provide accurate, current, and complete information during the
-								registration process and to update such information as
-								necessary.
-							</p>
-						</div>
-						<div className='pb-0'>
-							<p className='text-base text-gray-600'>
-								1.2 Prohibited Activities: You agree not to engage in any
-								activity that may interfere with or disrupt the Service or
-								violate any laws or regulations. This includes, but is not
-								limited to, transmitting any harmful or malicious code, engaging
-								in unauthorized access or use of our systems, or interfering
-								with the security or integrity of the Service.
-							</p>
-						</div>
-					</div>
+  return (
+    <section className='bg-gradient-to-b from-gray-100 to-white'>
+      <Header loginRequired={false} isLanding={false} />
+      <div className='max-w-6xl mx-auto px-4 sm:px-6'>
+        <div className='pt-32 pb-12 md:pt-40 md:pb-20'>
+          {/* Page header */}
+          <div className='max-w-3xl mx-auto text-center pb-12 md:pb-16'>
+            <h1 className='h1'>Terms and Conditions</h1>
+          </div>
+          <div className='max-w-3xl mx-auto'>
+            <div className='pb-16'>
+              <p className='text-base text-gray-600'>
+                Please read these terms and conditions carefully before using
+                our website and services provided by DrLambda. By accessing or
+                using our Service, you agree to be bound by these Terms. If you
+                do not agree with any part of these Terms, you may not access
+                the Service.
+              </p>
+            </div>
+          </div>
+          {/* Page sections */}
+          <div className='max-w-3xl mx-auto pb-4'>
+            <div className='pb-8'>
+              <h2 className='h3 text-2xl'>1. Use of Service</h2>
+            </div>
+            <div className='pb-4'>
+              <p className='text-base text-gray-600'>
+                1.1 Account Creation: In order to use certain features of the
+                Service, you may be required to create an account. You agree to
+                provide accurate, current, and complete information during the
+                registration process and to update such information as
+                necessary.
+              </p>
+            </div>
+            <div className='pb-0'>
+              <p className='text-base text-gray-600'>
+                1.2 Prohibited Activities: You agree not to engage in any
+                activity that may interfere with or disrupt the Service or
+                violate any laws or regulations. This includes, but is not
+                limited to, transmitting any harmful or malicious code, engaging
+                in unauthorized access or use of our systems, or interfering
+                with the security or integrity of the Service.
+              </p>
+            </div>
+          </div>
 
           <div className='pb-8'>
             <h2 className='h3 text-2xl'>2. Fair Usage Policy</h2>
@@ -93,115 +93,115 @@ export default function Terms() {
             </div>
           </div>
 
-					<div className='max-w-3xl mx-auto pb-4'>
-						<div className='pb-8'>
-							<h2 className='h3 text-2xl'>4. User-Generated Content</h2>
-						</div>
-						<div className='pb-4'>
-							<p className='text-base text-gray-600'>
-								4.1 Responsibility: You are solely responsible for any content
-								you post, upload, or otherwise make available through the
-								Service. You retain ownership of your User-Generated Content,
-								but you grant the Company a non-exclusive, royalty-free,
-								worldwide, perpetual, and irrevocable license to use, reproduce,
-								modify, adapt, publish, translate, distribute, and display such
-								content for the purpose of providing and promoting the Service.
-							</p>
-						</div>
-						<div className='pb-4'>
-							<p className='text-base text-gray-600'>
-								4.2 Prohibited Content: You agree not to post or share any
-								User-Generated Content that is unlawful, defamatory, abusive,
-								obscene, or otherwise objectionable. We reserve the right to
-								remove any User-Generated Content that violates these Terms or
-								is otherwise inappropriate.
-							</p>
-						</div>
-					</div>
+          <div className='max-w-3xl mx-auto pb-4'>
+            <div className='pb-8'>
+              <h2 className='h3 text-2xl'>4. User-Generated Content</h2>
+            </div>
+            <div className='pb-4'>
+              <p className='text-base text-gray-600'>
+                4.1 Responsibility: You are solely responsible for any content
+                you post, upload, or otherwise make available through the
+                Service. You retain ownership of your User-Generated Content,
+                but you grant the Company a non-exclusive, royalty-free,
+                worldwide, perpetual, and irrevocable license to use, reproduce,
+                modify, adapt, publish, translate, distribute, and display such
+                content for the purpose of providing and promoting the Service.
+              </p>
+            </div>
+            <div className='pb-4'>
+              <p className='text-base text-gray-600'>
+                4.2 Prohibited Content: You agree not to post or share any
+                User-Generated Content that is unlawful, defamatory, abusive,
+                obscene, or otherwise objectionable. We reserve the right to
+                remove any User-Generated Content that violates these Terms or
+                is otherwise inappropriate.
+              </p>
+            </div>
+          </div>
 
-					<div className='max-w-3xl mx-auto pb-4'>
-						<div className='pb-8'>
-							<h2 className='h3 text-2xl'>5. Privacy</h2>
-						</div>
-						<div className='pb-4'>
-							<p className='text-base text-gray-600'>
-								5.1 Privacy Policy: Our collection, use, and disclosure of your
-								personal information are governed by our Privacy Policy, which
-								is incorporated into these Terms by reference. Please review our{' '}
-								<a className='text-blue-600' href='/privacy'>
-									Privacy Policy
-								</a>{' '}
-								to understand our practices.
-							</p>
-						</div>
-					</div>
+          <div className='max-w-3xl mx-auto pb-4'>
+            <div className='pb-8'>
+              <h2 className='h3 text-2xl'>5. Privacy</h2>
+            </div>
+            <div className='pb-4'>
+              <p className='text-base text-gray-600'>
+                5.1 Privacy Policy: Our collection, use, and disclosure of your
+                personal information are governed by our Privacy Policy, which
+                is incorporated into these Terms by reference. Please review our{' '}
+                <a className='text-blue-600' href='/privacy'>
+                  Privacy Policy
+                </a>{' '}
+                to understand our practices.
+              </p>
+            </div>
+          </div>
 
-					<div className='max-w-3xl mx-auto pb-4'>
-						<div className='pb-8'>
-							<h2 className='h3 text-2xl'>6. Limitation of Liability</h2>
-						</div>
-						<div className='pb-4'>
-							<p className='text-base text-gray-600'>
-								6.1 Disclaimer: The Service is provided on an "as is" and "as
-								available" basis, without any warranties or conditions, express
-								or implied. We do not warrant that the Service will be
-								uninterrupted, error-free, or secure.
-							</p>
-						</div>
-						<div className='pb-4'>
-							<p className='text-base text-gray-600'>
-								6.2 Limitation of Liability: In no event shall the Company or
-								its affiliates be liable for any indirect, incidental, special,
-								consequential, or punitive damages arising out of or in
-								connection with your use of the Service, whether based on
-								contract, tort, negligence, strict liability, or otherwise.
-							</p>
-						</div>
-					</div>
+          <div className='max-w-3xl mx-auto pb-4'>
+            <div className='pb-8'>
+              <h2 className='h3 text-2xl'>6. Limitation of Liability</h2>
+            </div>
+            <div className='pb-4'>
+              <p className='text-base text-gray-600'>
+                6.1 Disclaimer: The Service is provided on an "as is" and "as
+                available" basis, without any warranties or conditions, express
+                or implied. We do not warrant that the Service will be
+                uninterrupted, error-free, or secure.
+              </p>
+            </div>
+            <div className='pb-4'>
+              <p className='text-base text-gray-600'>
+                6.2 Limitation of Liability: In no event shall the Company or
+                its affiliates be liable for any indirect, incidental, special,
+                consequential, or punitive damages arising out of or in
+                connection with your use of the Service, whether based on
+                contract, tort, negligence, strict liability, or otherwise.
+              </p>
+            </div>
+          </div>
 
-					<div className='max-w-3xl mx-auto pb-4'>
-						<div className='pb-8'>
-							<h2 className='h3 text-2xl'>7. Modifications to Terms</h2>
-						</div>
-						<div className='pb-4'>
-							<p className='text-base text-gray-600'>
-								7.1 Updates: We reserve the right to update or modify these
-								Terms at any time, without prior notice. Any changes will be
-								effective immediately upon posting on the Service. Your
-								continued use of the Service after the posting of changes
-								constitutes your acceptance of such changes.
-							</p>
-						</div>
-					</div>
+          <div className='max-w-3xl mx-auto pb-4'>
+            <div className='pb-8'>
+              <h2 className='h3 text-2xl'>7. Modifications to Terms</h2>
+            </div>
+            <div className='pb-4'>
+              <p className='text-base text-gray-600'>
+                7.1 Updates: We reserve the right to update or modify these
+                Terms at any time, without prior notice. Any changes will be
+                effective immediately upon posting on the Service. Your
+                continued use of the Service after the posting of changes
+                constitutes your acceptance of such changes.
+              </p>
+            </div>
+          </div>
 
-					<div className='max-w-3xl mx-auto pb-4'>
-						<div className='pb-8'>
-							<h2 className='h3 text-2xl'>8. Governing Law</h2>
-						</div>
-						<div className='pb-4'>
-							<p className='text-base text-gray-600'>
-								8.1 Jurisdiction: These Terms shall be governed by and construed
-								in accordance with the laws of United States or your
-								jurisdiction. Any disputes arising out of or relating to these
-								Terms or the Service shall be exclusively resolved by the state
-								and federal courts located in United States or your
-								jurisdiction.
-							</p>
-						</div>
-					</div>
+          <div className='max-w-3xl mx-auto pb-4'>
+            <div className='pb-8'>
+              <h2 className='h3 text-2xl'>8. Governing Law</h2>
+            </div>
+            <div className='pb-4'>
+              <p className='text-base text-gray-600'>
+                8.1 Jurisdiction: These Terms shall be governed by and construed
+                in accordance with the laws of United States or your
+                jurisdiction. Any disputes arising out of or relating to these
+                Terms or the Service shall be exclusively resolved by the state
+                and federal courts located in United States or your
+                jurisdiction.
+              </p>
+            </div>
+          </div>
 
-					<div className='max-w-3xl mx-auto'>
-						<div className='pt-16'>
-							<p className='text-base text-gray-600'>
-								By using our Service, you acknowledge that you have read,
-								understood, and agree to be bound by these Terms and any other
-								applicable agreements or policies referenced herein.
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
-			<Footer />
-		</section>
-	);
+          <div className='max-w-3xl mx-auto'>
+            <div className='pt-16'>
+              <p className='text-base text-gray-600'>
+                By using our Service, you acknowledge that you have read,
+                understood, and agree to be bound by these Terms and any other
+                applicable agreements or policies referenced herein.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </section>
+  );
 }
