9c406f1f9917d1e4d57384e07ec949b21d4a9863
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-02-10 10:58:56 -0800

    [hero] imporve formatting

--- tailwindui/components/landing/HeroText.tsx
+++ tailwindui/components/landing/HeroText.tsx
@@ -6,16 +6,17 @@ const HeroText = () => {
 		<div
 			className={`flex flex-col items-center transition-opacity duration-1000`}
 		>
-			<div className='flex flex-col md:flex-row items-end space-x-4'>
-				{/* <TextCarousel slides={["Documents", "Youtube", "Webpage", "Notion"]} interval={3000} color="text-blue-700" alignment='right'/> */}
-				<h1 className='text-5xl font-extrabold leading-tighter tracking-tighte font-creato-medium mb-0.5'>
+			<div className='flex flex-col sm:flex-row items-start sm:items-end space-x-4'>
+				<h1 className='text-4xl sm:text-5xl font-extrabold leading-tighter tracking-tighte font-creato-medium mb-0.5'>
 					Documents to
 				</h1>
+        <div>
 				<TextCarousel
 					slides={['Slides', 'Social Posts', 'Video']}
 					interval={3000}
 					color='text-blue-700'
 				/>
+        </div>
 			</div>
 		</div>
 	);
