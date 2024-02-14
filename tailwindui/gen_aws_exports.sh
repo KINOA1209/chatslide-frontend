ff963c5cb0bb50aca0be807b691878484f42d632
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-02-13 17:44:25 -0800

    remove aws-exports.js from repo

--- tailwindui/gen_aws_exports.sh
+++ tailwindui/gen_aws_exports.sh
@@ -1,5 +1,7 @@
 # This script is used to generate aws-exports.js file for AWS Amplify
 
+rm -f aws-exports.js
+
 sed \
 -e "s|AWS_COGNITO_IDENTITY_POOL_ID|${AWS_COGNITO_IDENTITY_POOL_ID}|g" \
 -e "s|AWS_USER_POOLS_ID|${AWS_USER_POOLS_ID}|g" \
