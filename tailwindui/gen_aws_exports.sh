40281ddae48ab0e7b423af49394f2ceed4f674f5
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-02-13 17:45:31 -0800

    check if env vars are set

--- tailwindui/gen_aws_exports.sh
+++ tailwindui/gen_aws_exports.sh
@@ -2,6 +2,22 @@
 
 rm -f aws-exports.js
 
+# check if env vars are set
+if [ -z "$AWS_COGNITO_IDENTITY_POOL_ID" ]; then
+  echo "AWS_COGNITO_IDENTITY_POOL_ID is not set"
+  exit 1
+fi
+
+if [ -z "$AWS_USER_POOLS_ID" ]; then
+  echo "AWS_USER_POOLS_ID is not set"
+  exit 1
+fi
+
+if [ -z "$AWS_USER_POOLS_WEB_CLIENT_ID" ]; then
+  echo "AWS_USER_POOLS_WEB_CLIENT_ID is not set"
+  exit 1
+fi
+
 sed \
 -e "s|AWS_COGNITO_IDENTITY_POOL_ID|${AWS_COGNITO_IDENTITY_POOL_ID}|g" \
 -e "s|AWS_USER_POOLS_ID|${AWS_USER_POOLS_ID}|g" \
