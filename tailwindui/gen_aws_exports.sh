02038e0745aa99cae53bac96b3d8cfa32b9713ed
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-02-13 17:55:28 -0800

    [secrets] update gen scripts

--- tailwindui/gen_aws_exports.sh
+++ tailwindui/gen_aws_exports.sh
@@ -1,25 +1,25 @@
 # This script is used to generate aws-exports.js file for AWS Amplify
 
+# check if env vars are set
+if [ -z "$COGNITO_IDENTITY_POOL_ID" ]; then
+  echo "COGNITO_IDENTITY_POOL_ID is not set"
+  exit 1
+fi
+
+if [ -z "$USER_POOLS_ID" ]; then
+  echo "USER_POOLS_ID is not set"
+  exit 1
+fi
+
+if [ -z "$USER_POOLS_WEB_CLIENT_ID" ]; then
+  echo "USER_POOLS_WEB_CLIENT_ID is not set"
+  exit 1
+fi
+
 rm -f aws-exports.js
 
-# check if env vars are set
-if [ -z "$AWS_COGNITO_IDENTITY_POOL_ID" ]; then
-  echo "AWS_COGNITO_IDENTITY_POOL_ID is not set"
-  exit 1
-fi
-
-if [ -z "$AWS_USER_POOLS_ID" ]; then
-  echo "AWS_USER_POOLS_ID is not set"
-  exit 1
-fi
-
-if [ -z "$AWS_USER_POOLS_WEB_CLIENT_ID" ]; then
-  echo "AWS_USER_POOLS_WEB_CLIENT_ID is not set"
-  exit 1
-fi
-
 sed \
--e "s|AWS_COGNITO_IDENTITY_POOL_ID|${AWS_COGNITO_IDENTITY_POOL_ID}|g" \
--e "s|AWS_USER_POOLS_ID|${AWS_USER_POOLS_ID}|g" \
--e "s|AWS_USER_POOLS_WEB_CLIENT_ID|${AWS_USER_POOLS_WEB_CLIENT_ID}|g" \
+-e "s|COGNITO_IDENTITY_POOL_ID|${COGNITO_IDENTITY_POOL_ID}|g" \
+-e "s|USER_POOLS_ID|${USER_POOLS_ID}|g" \
+-e "s|USER_POOLS_WEB_CLIENT_ID|${USER_POOLS_WEB_CLIENT_ID}|g" \
  aws-exports.js.template >> aws-exports.js
