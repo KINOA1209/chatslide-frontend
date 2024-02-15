658b2fc4260e6b5b707a26ba89d094ba7e078e7f
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-02-14 21:53:11 -0800

    [user] retry in init user

--- tailwindui/hooks/use-user.ts
+++ tailwindui/hooks/use-user.ts
@@ -53,42 +53,54 @@ export const useUser = () => {
 		if (userStatus !== UserStatus.NotInited) return;
 		userStatus = UserStatus.Initing;
 
-		try {
-      const { userId, idToken } = await AuthService.getCurrentUserTokenAndId();
-      let username = await AuthService.getCurrentUserDisplayName();
-      username = username?.split('@')[0] || 'User';
-      const email = await AuthService.getCurrentUserEmail();
-			const { credits, tier } =
-				await UserService.getUserCreditsAndTier(idToken);
-			const isPaidUser = [
-				'PRO_MONTHLY',
-				'PLUS_MONTHLY',
-				'PRO_YEARLY',
-				'PLUS_YEARLY',
-			].includes(tier);
+    for (let attempt = 0; attempt < 2; attempt++) {
+      try {
+        const { userId, idToken } = await AuthService.getCurrentUserTokenAndId();
+        let username = await AuthService.getCurrentUserDisplayName();
+        username = username?.split('@')[0] || 'User';
+        const email = await AuthService.getCurrentUserEmail();
+        const { credits, tier } =
+          await UserService.getUserCreditsAndTier(idToken);
+        const isPaidUser = [
+          'PRO_MONTHLY',
+          'PLUS_MONTHLY',
+          'PRO_YEARLY',
+          'PLUS_YEARLY',
+        ].includes(tier);
 
-			console.log('-- initUser: ', {
-        username: username,
-        userId: userId,
-        idToken: idToken,
-        email: email,
-        credits: credits,
-        tier: tier,
-        isPaidUser: isPaidUser,
-      });
-      
-      setCredits(credits);
-      setTier(tier);
-      setUid(userId);
-      setToken(idToken);
-			setIsPaidUser(isPaidUser);
-      setUsername(username);
-      setEmail(email);
-			userStatus = UserStatus.Inited;
-		} catch (e) {
-			console.error(e);
-			userStatus = UserStatus.NotInited;
-		}
+        console.log('-- initUser: ', {
+          username: username,
+          userId: userId,
+          idToken: idToken,
+          email: email,
+          credits: credits,
+          tier: tier,
+          isPaidUser: isPaidUser,
+        });
+
+        setCredits(credits);
+        setTier(tier);
+        setUid(userId);
+        setToken(idToken);
+        setIsPaidUser(isPaidUser);
+        setUsername(username);
+        setEmail(email);
+        userStatus = UserStatus.Inited;
+
+        // Break out of the loop if successful
+        break;
+      } catch (e) {
+        console.error(`Failed to init user: ${e}, attempt ${attempt + 1}/2...`)
+
+        // If it's the second attempt, throw the error
+        if (attempt === 1) {
+          throw e;
+        }
+
+        // Otherwise, retry the block after a delay (you can adjust the delay as needed)
+        await new Promise(resolve => setTimeout(resolve, 500));
+      }
+    }
 	};
 
   const updateCreditsAndTier = async () => {
