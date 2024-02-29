e8cd328be14b88a62a0a1176b1a389efdfdfd095
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-02-29 10:25:57 -0800

    [ToolBar] alignment

--- tailwindui/components/ui/ActionsToolBar.tsx
+++ tailwindui/components/ui/ActionsToolBar.tsx
@@ -3,161 +3,164 @@ import { RiArrowGoBackFill, RiArrowGoForwardFill } from 'react-icons/ri';
 import { GoQuestion } from 'react-icons/go';
 import { StartATourGuidePromptWindow } from '@/components/user_onboarding/CustomComponents';
 import ButtonWithExplanation from '../button/ButtonWithExplanation';
+import { ToolBar } from './ToolBar';
 
 type ActionsToolBarProps = {
-	undo?: () => void;
-	redo?: () => void;
-	canUndo?: boolean;
-	canRedo?: boolean;
-	// Other props...
-	startTour: () => void;
-	onlyShowTutorial: boolean;
+  undo?: () => void;
+  redo?: () => void;
+  canUndo?: boolean;
+  canRedo?: boolean;
+  // Other props...
+  startTour: () => void;
+  onlyShowTutorial: boolean;
 };
 
 const ActionsToolBar: React.FC<ActionsToolBarProps> = ({
-	undo,
-	redo,
-	canUndo,
-	canRedo,
-	startTour,
-	onlyShowTutorial,
+  undo,
+  redo,
+  canUndo,
+  canRedo,
+  startTour,
+  onlyShowTutorial,
 }) => {
-	const [showTutorialPrompt, setShowTutorialPrompt] = useState(false);
-	const handleStartTour = () => {
-		setShowTutorialPrompt(true);
-	};
+  const [showTutorialPrompt, setShowTutorialPrompt] = useState(false);
+  const handleStartTour = () => {
+    setShowTutorialPrompt(true);
+  };
 
-	const handleCloseTutorialPrompt = () => {
-		setShowTutorialPrompt(false);
-	};
+  const handleCloseTutorialPrompt = () => {
+    setShowTutorialPrompt(false);
+  };
 
-	const handleConfirmStartTour = () => {
-		startTour();
-		setShowTutorialPrompt(false);
-	};
-	return onlyShowTutorial ? (
-		<section
-			className={`shadow-md bg-white rounded-[0.5rem] px-4 py-2 border-2 border-[#C6C6C6]`}
-		>
-			<div className='flex flex-row gap-4'>
-				{/* user tutorial control */}
-				<ButtonWithExplanation
-					button={
-						<button
-							onClick={handleStartTour}
-							style={{
-								display: 'flex',
-								justifyContent: 'center',
-								alignItems: 'center',
-							}}
-						>
-							<GoQuestion
-								style={{
-									strokeWidth: '1',
-									flex: '1',
-									width: '1.5rem',
-									height: '1.5rem',
-									fontWeight: 'bold',
-									color: '#2943E9',
-								}}
-							/>
-						</button>
-					}
-					explanation={'Guided user tutorial'}
-				></ButtonWithExplanation>
+  const handleConfirmStartTour = () => {
+    startTour();
+    setShowTutorialPrompt(false);
+  };
+  return onlyShowTutorial ? (
+    <ToolBar>
+      {/* user tutorial control */}
+      <ButtonWithExplanation
+        button={
+          <button
+            onClick={handleStartTour}
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <GoQuestion
+              style={{
+                strokeWidth: '1',
+                flex: '1',
+                width: '1.5rem',
+                height: '1.5rem',
+                fontWeight: 'bold',
+                color: '#2943E9',
+              }}
+            />
+          </button>
+        }
+        explanation={'User tutorial'}
+      ></ButtonWithExplanation>
 
-				{showTutorialPrompt && (
-					<StartATourGuidePromptWindow
-						onClose={handleCloseTutorialPrompt}
-						onConfirm={handleConfirmStartTour}
-					/>
-				)}
-			</div>
-		</section>
-	) : (
-		<section
-			className={`shadow-md bg-white rounded-[0.5rem] px-4 py-2 border-2 border-[#C6C6C6]`}
-		>
-			<div className='flex flex-row gap-4'>
-				{/* user tutorial control */}
-				<ButtonWithExplanation
-					button={
-						<button
-							onClick={handleStartTour}
-							style={{
-								display: 'flex',
-								justifyContent: 'center',
-								alignItems: 'center',
-							}}
-						>
-							<GoQuestion
-								style={{
-									strokeWidth: '1',
-									flex: '1',
-									width: '1.5rem',
-									height: '1.5rem',
-									fontWeight: 'bold',
-									color: '#2943E9',
-								}}
-							/>
-						</button>
-					}
-					explanation={'Guided user tutorial'}
-				></ButtonWithExplanation>
+      {showTutorialPrompt && (
+        <StartATourGuidePromptWindow
+          onClose={handleCloseTutorialPrompt}
+          onConfirm={handleConfirmStartTour}
+        />
+      )}
+    </ToolBar>
+  ) : (
+    <ToolBar >
+      {/* user tutorial control */}
+      <ButtonWithExplanation
+        button={
+          <button
+            onClick={handleStartTour}
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <GoQuestion
+              style={{
+                strokeWidth: '1',
+                flex: '1',
+                width: '1.5rem',
+                height: '1.5rem',
+                fontWeight: 'bold',
+                color: '#2943E9',
+              }}
+            />
+          </button>
+        }
+        explanation={'User tutorial'}
+      ></ButtonWithExplanation>
 
-				<div style={{ backgroundColor: '#C6C6C6', width: '2px' }}></div>
-				<ButtonWithExplanation
-					button={
-						<button
-							onClick={undo}
-							style={{ color: canUndo ? '#2943E9' : '#C6C6C6' }}
-							disabled={!canUndo}
-						>
-							<RiArrowGoBackFill
-								style={{
-									strokeWidth: '1',
-									flex: '1',
-									width: '1.5rem',
-									height: '1.5rem',
-									fontWeight: 'bold',
-								}}
-							/>
-						</button>
-					}
-					explanation={'Undo'}
-				></ButtonWithExplanation>
+      <div style={{ backgroundColor: '#C6C6C6', width: '2px' }}></div>
+      <ButtonWithExplanation
+        button={
+          <button
+            onClick={undo}
+            style={{
+              color: canUndo ? '#2943E9' : '#C6C6C6',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            disabled={!canUndo}
+          >
+            <RiArrowGoBackFill
+              style={{
+                strokeWidth: '1',
+                flex: '1',
+                width: '1.5rem',
+                height: '1.5rem',
+                fontWeight: 'bold',
+              }}
+            />
+          </button>
+        }
+        explanation={'Undo'}
+      ></ButtonWithExplanation>
 
-				<div style={{ backgroundColor: '#C6C6C6', width: '2px' }}></div>
-				<ButtonWithExplanation
-					button={
-						<button
-							onClick={redo}
-							style={{ color: canRedo ? '#2943E9' : '#C6C6C6' }}
-							disabled={!canRedo}
-						>
-							<RiArrowGoForwardFill
-								style={{
-									strokeWidth: '1',
-									flex: '1',
-									width: '1.5rem',
-									height: '1.5rem',
-									fontWeight: 'bold',
-								}}
-							/>
-						</button>
-					}
-					explanation={'Redo'}
-				></ButtonWithExplanation>
+      <div style={{ backgroundColor: '#C6C6C6', width: '2px' }}></div>
+      <ButtonWithExplanation
+        button={
+          <button
+            onClick={redo}
+            style={{
+              color: canUndo ? '#2943E9' : '#C6C6C6',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            disabled={!canRedo}
+          >
+            <RiArrowGoForwardFill
+              style={{
+                strokeWidth: '1',
+                flex: '1',
+                width: '1.5rem',
+                height: '1.5rem',
+                fontWeight: 'bold',
+              }}
+            />
+          </button>
+        }
+        explanation={'Redo'}
+      ></ButtonWithExplanation>
 
-				{showTutorialPrompt && (
-					<StartATourGuidePromptWindow
-						onClose={handleCloseTutorialPrompt}
-						onConfirm={handleConfirmStartTour}
-					/>
-				)}
-			</div>
-		</section>
-	);
+      {showTutorialPrompt && (
+        <StartATourGuidePromptWindow
+          onClose={handleCloseTutorialPrompt}
+          onConfirm={handleConfirmStartTour}
+        />
+      )}
+    </ToolBar>
+  );
 };
 
 export default ActionsToolBar;
