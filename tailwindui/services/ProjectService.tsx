6ac03337c97730c09fd2133e3b377812a449b0b0
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-03-18 19:35:49 -0700

    [slides] init show logo properly

--- tailwindui/services/ProjectService.tsx
+++ tailwindui/services/ProjectService.tsx
@@ -228,7 +228,8 @@ class ProjectService {
 			slide.transcript = slideData.transcript || '';
 			slide.logo_url = slideData.logo_url || '';
 			slide.background_url = slideData.background_url || '';
-			slide.show_logo = slideData.show_logo;
+			slide.show_logo = slideData.hasOwnProperty('show_logo') ? slideData.show_logo : true;
+
 			console.log("slide.images", slide.images);
 			if (index === 0) {
 				slide.layout = slideData.layout || ('Cover_img_1_layout' as LayoutKeys);
