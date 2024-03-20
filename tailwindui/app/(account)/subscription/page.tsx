31f6b8f72f9ff0bdd3a2c78838c218cf033743bf
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-03-20 12:10:27 -0700

    [subscription] allow users to redeem codes

--- tailwindui/app/(account)/subscription/page.tsx
+++ tailwindui/app/(account)/subscription/page.tsx
@@ -8,6 +8,11 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { FeedbackForm } from '@/components/ui/feedback';
 import { PrimaryButton, UserResearchWindow } from './subscriptionCTAs';
+import { Explanation, Instruction, Title } from '@/components/ui/Text';
+import Card from '@/components/ui/Card';
+import { Panel } from '@/components/layout/Panel';
+import { Column } from '@/components/layout/Column';
+import useHydrated from '@/hooks/use-hydrated';
 
 const Subscription = () => {
 	const [portalURL, setPortalURL] = useState('');
@@ -32,26 +37,31 @@ const Subscription = () => {
 				/>
 			)}
 
-			<div className='mb-8 w-full max-w-none 2xl:max-w-[80%] mx-auto px-4 sm:px-6'>
+			<Card>
 				{/* <div className='w-fit text-[#363E4A] text-[17px] font-bold'>
 					Subscription
 				</div> */}
-				<h2 className='w-fit text-[#212121] text-xl md:text-[80px]'>
+				<Title>
 					Find tailored plans for you
-				</h2>
-			</div>
+				</Title>
 
-			<Pricing />
-			{portalURL && (
-				<button
-					onClick={() => {
-						setShowModal(true);
-					}}
-					className='w-full py-4 sm:px-6 flex flex-col justify-center items-center max-w-none 2xl:max-w-[80%] mx-auto'
-				>
-					Manage Subscription
-				</button>
-			)}
+				<Pricing />
+
+				<Instruction>
+					Looking to redeem your code? Click <Link className='text-blue-600' href='/account'>here</Link>!
+				</Instruction>
+
+				{portalURL && (
+					<button
+						onClick={() => {
+							setShowModal(true);
+						}}
+						className='w-full py-4 sm:px-6 flex flex-col justify-center items-center max-w-none 2xl:max-w-[80%] mx-auto'
+					>
+						Manage Subscription
+					</button>
+				)}
+			</Card>
 		</div>
 	);
 };
@@ -79,6 +89,9 @@ export default function SubscriptionAndUserResearch() {
 		});
 	});
 
+	// avoid hydration error during development caused by persistence
+	if (!useHydrated()) return <></>;
+
 	return (
 		<div className='flex flex-col items-center gap-[30px] md:gap-[70px] mx-auto w-full'>
 			<ToastContainer />
@@ -88,23 +101,24 @@ export default function SubscriptionAndUserResearch() {
 					onClick={handleUserResearchModal}
 				></UserResearchWindow>
 			)}
-			<section id='subscription' className='w-full'>
-				<Subscription />
-			</section>
-			{/* explaining text for filling out form */}
-			<section className='w-full px-4 py-2 flex flex-col items-start gap-[0.5rem]'>
-				<div className='text-center text-neutral-800 text-xl font-bold leading-normal font-creato-medium'>
-					Earn free credits⭐ by joining our user study
-				</div>
-				<div className='text-gray-600 text-sm font-normal font-creato-medium leading-normal tracking-[0.0175rem]'>
-					We want to learn more about what you think of DrLambda, and how you
-					use DrLambda.
-				</div>
-				<PrimaryButton
-					onClick={handleUserResearchModal}
-					label={'Join today!'}
-				></PrimaryButton>
-			</section>
+			<Column>
+				<section id='subscription' className='w-full'>
+					<Subscription />
+				</section>
+				<Card>
+					<Instruction>
+						Earn free credits⭐ by joining our user study
+					</Instruction>
+					<Explanation>
+						We want to learn more about what you think of DrLambda, and how you
+						use DrLambda.
+					</Explanation>
+					<PrimaryButton
+						onClick={handleUserResearchModal}
+						label={'Join today!'}
+					></PrimaryButton>
+				</Card>
+			</Column>
 		</div>
 	);
 }
