00f5bf7e25bc0b1c944e5ea65f440ad06e58f30f
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-03-22 16:35:40 -0700

    [audio] add more voices, missing mexican spanish and brazilian portuguese

--- tailwindui/components/language/voiceData.tsx
+++ tailwindui/components/language/voiceData.tsx
@@ -69,6 +69,68 @@ const VOICE_OPTIONS: VoiceOption = {
 			'de-DE-ConradNeural', 'de-DE-BerndNeural', 'de-DE-ChristophNeural', 'de-DE-KasperNeural',
 			'de-DE-KillianNeural', 'de-DE-KlausNeural', 'de-DE-RalfNeural'
 		]
+	},
+	'fr-FR': {
+		'female': [
+			'fr-FR-DeniseNeural', 'fr-FR-BrigitteNeural', 'fr-FR-CelesteNeural', 'fr-FR-CoralieNeural', 'fr-FR-EloiseNeural',
+			'fr-FR-JacquelineNeural', 'fr-FR-JosephineNeural', 'fr-FR-VivienneMultilingualNeural', 'fr-FR-YvetteNeural'
+		],
+		'male': [
+			'fr-FR-HenriNeural', 'fr-FR-AlainNeural', 'fr-FR-ClaudeNeural', 'fr-FR-JeromeNeural', 'fr-FR-MauriceNeural',
+			'fr-FR-RemyMultilingualNeural', 'fr-FR-YvesNeural'
+		]
+	},
+	'ru-RU': {
+		'female': ['ru-RU-SvetlanaNeural', 'ru-RU-DariyaNeural'],
+		'male': ['ru-RU-DmitryNeural']
+	},
+	'uk-UA': {
+		'female': ['uk-UA-PolinaNeural'],
+		'male': ['uk-UA-OstapNeural']
+	},
+	'pt-PT': {
+		'female': ['pt-PT-RaquelNeural', 'pt-PT-FernandaNeural'],
+		'male': ['pt-PT-DuarteNeural']
+	},
+	'it-IT': {
+		'female': [
+			'it-IT-ElsaNeural', 'it-IT-IsabellaNeural', 'it-IT-FabiolaNeural', 'it-IT-FiammaNeural', 'it-IT-ImeldaNeural',
+			'it-IT-IrmaNeural', 'it-IT-PalmiraNeural', 'it-IT-PierinaNeural'
+		],
+		'male': [
+			'it-IT-DiegoNeural', 'it-IT-BenignoNeural', 'it-IT-CalimeroNeural', 'it-IT-CataldoNeural', 'it-IT-GianniNeural',
+			'it-IT-GiuseppeNeural', 'it-IT-LisandroNeural', 'it-IT-RinaldoNeural'
+		]
+	},
+	'hi-IN': {
+		'female': ['hi-IN-SwaraNeural'],
+		'male': ['hi-IN-MadhurNeural']
+	},
+	'ja-JP': {
+		'female': ['ja-JP-NanamiNeural', 'ja-JP-AoiNeural', 'ja-JP-MayuNeural', 'ja-JP-ShioriNeural'],
+		'male': ['ja-JP-KeitaNeural', 'ja-JP-DaichiNeural', 'ja-JP-NaokiNeural']
+	},
+	'ko-KR': {
+		'female': [
+			'ko-KR-SunHiNeural', 'ko-KR-JiMinNeural', 'ko-KR-SeoHyeonNeural', 'ko-KR-SoonBokNeural', 'ko-KR-YuJinNeural'
+		],
+		'male': ['ko-KR-InJoonNeural', 'ko-KR-BongJinNeural', 'ko-KR-GookMinNeural', 'ko-KR-HyunsuNeural']
+	},
+	'ar-SA': {
+		'female': ['ar-SA-ZariyahNeural'],
+		'male': ['ar-SA-HamedNeural']
+	},
+	'he-IL': {
+		'female': ['he-IL-HilaNeural'],
+		'male': ['he-IL-AvriNeural']
+	},
+	'nl-NL': {
+		'female': ['nl-NL-FennaNeural', 'nl-NL-ColetteNeural'],
+		'male': ['nl-NL-MaartenNeural']
+	},
+	'hr-HR': {
+		'female': ['hr-HR-GabrijelaNeural'],
+		'male': ['hr-HR-SreckoNeural']
 	}
 };
 
