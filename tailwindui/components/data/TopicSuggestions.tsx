e09da6df39d0ad1c415477fe5217f3173cfac2fe
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-03-23 12:18:57 -0700

    [chatbot] add data

---
+++ tailwindui/components/data/TopicSuggestions.tsx
@@ -0,0 +1,19 @@
+const TOPIC_SUGGESTIONS = [
+	'How to use ultraviolet light to kill germs 🦠☀️',
+	'The benefits of daily meditation 🧘‍♂️✨',
+	'Understanding the basics of blockchain technology 🔗💻',
+	'Tips for sustainable living and reducing waste 🌿♻️',
+	'Exploring the world of coffee from bean to cup ☕🌍',
+	'The impact of music on mental health 🎵💖',
+	'Beginner’s guide to astronomy: Stargazing 🌌🔭',
+	'How to start your own vegetable garden 🥕🌱',
+	'The history and art of making sourdough bread 🍞🎨',
+	'Improving productivity with the Pomodoro Technique ⏱️📈',
+	'The science behind why we dream 💤🧠',
+	'Discovering the world’s ancient civilizations 🏛️🗺️',
+	'Photography tips for capturing nature’s beauty 📸🌳',
+	'Learning a new language: Strategies and benefits 🗣️🌐',
+	'DIY home decor ideas on a budget 🛠️🏠',
+];
+
+export default TOPIC_SUGGESTIONS;
---
+++ tailwindui/components/ui/ChatBotData.tsx
@@ -0,0 +1,13 @@
+const CHATBOT_SUGGESTIONS = {
+	cover: [
+		'Change topic to be more engaging',
+		'Translate topic to Spanish',
+		'Change the image on the slide',
+	],
+	noncover: [
+		'Add data to the content',
+		'Change the image on the slide',
+		'Change subtopic to be more engaging',
+		'Add an example to the content',
+	],
+};
