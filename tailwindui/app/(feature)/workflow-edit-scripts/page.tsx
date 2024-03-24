df8d2073d3561efc1640a72bdd09d9054e28d296
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-03-24 12:13:10 -0700

    [scripts] fix page

--- tailwindui/app/(feature)/workflow-edit-scripts/page.tsx
+++ tailwindui/app/(feature)/workflow-edit-scripts/page.tsx
@@ -23,88 +23,58 @@ const ScriptSection = dynamic(
 export default function WorkflowStep5() {
 	const [isSubmitting, setIsSubmitting] = useState(false);
 	const { slides, updateSlidePage } = useSlides();
-	const { project } = useProject();
-	const { token } = useUser();
-	const router = useRouter();
 	const [voice, setVoice] = useState('en-US-AvaNeural');
 
 	async function handleSubmitVideo() {
-		if (!project) {
-			console.error('No project found');
-			return;
-		}
-		const foldername = project?.foldername;
-		const project_id = project.id;
-		if (!foldername || !project_id) {
-			console.error('No language or foldername or project_id found');
-			setIsSubmitting(false);
-			const language = project?.language;
-			if (!language || !foldername) {
-				console.error('No language or foldername found');
-				return;
-			}
-
-			const fetchData = async () => {
-				try {
-					console.log('project_id:', project_id);
-					VideoService.generateVideo(project_id, foldername, voice, token);
-					router.push(addIdToRedir('workflow-review-video'));
-				} catch (error) {
-					console.error('Error in fetchData:', error);
-				}
-			};
-			fetchData();
-			setIsSubmitting(false);
-		}
-
-		useEffect(() => {
-			if (isSubmitting) {
-				handleSubmitVideo();
-			}
-		}, [isSubmitting]);
-
-		return (
-			<div className='h-full w-full bg-white flex flex-col'>
-				{/* flex col container for steps, title, etc */}
-
-				<WorkflowStepsBanner
-					currentIndex={4}
-					isSubmitting={isSubmitting}
-					setIsSubmitting={setIsSubmitting}
-					isPaidUser={true}
-					nextIsPaidFeature={true}
-					nextText='Create Video'
-				/>
-
-				<ToastContainer enableMultiContainer containerId={'script'} />
-
-				<Column>
-					<Card>
-						<BigTitle>Voice</BigTitle>
-						<Instruction>
-							Select the voice you want to use for your video.
-						</Instruction>
-						<VoiceSelector selectedVoice={voice} setSelectedVoice={setVoice} />
-					</Card>
-					<Card>
-						<BigTitle>Scripts</BigTitle>
-						<Instruction>
-							You can edit your scripts here: If you want to edit slides, you
-							can go back to the previous step.
-						</Instruction>
-						<div className='flex flex-col gap-y-2'>
-							{slides.map((_, index) => (
-								<ScriptSection
-									slides={slides}
-									index={index}
-									voice={voice}
-									updateSlidePage={updateSlidePage}
-								/>
-							))}
-						</div>
-					</Card>
-				</Column>
-			</div>
-		);
 	}
+
+	useEffect(() => {
+		if (isSubmitting) {
+			handleSubmitVideo();
+		}
+	}, [isSubmitting]);
+
+	return (
+		<div className='h-full w-full bg-white flex flex-col'>
+			{/* flex col container for steps, title, etc */}
+
+			<WorkflowStepsBanner
+				currentIndex={4}
+				isSubmitting={isSubmitting}
+				setIsSubmitting={setIsSubmitting}
+				isPaidUser={true}
+				nextIsPaidFeature={true}
+				nextText='Create Video'
+			/>
+
+			<ToastContainer enableMultiContainer containerId={'script'} />
+
+			<Column>
+				<Card>
+					<BigTitle>Voice</BigTitle>
+					<Instruction>
+						Select the voice you want to use for your video.
+					</Instruction>
+					<VoiceSelector selectedVoice={voice} setSelectedVoice={setVoice} />
+				</Card>
+				<Card>
+					<BigTitle>Scripts</BigTitle>
+					<Instruction>
+						You can edit your scripts here:
+						If you want to edit slides, you can go back to the previous step.
+					</Instruction>
+					<div className='flex flex-col gap-y-2'>
+						{slides.map((_, index) => (
+							<ScriptSection
+								slides={slides}
+								index={index}
+								voice={voice}
+								updateSlidePage={updateSlidePage}
+							/>
+						))}
+					</div>
+				</Card>
+			</Column>
+		</div>
+	);
 }
