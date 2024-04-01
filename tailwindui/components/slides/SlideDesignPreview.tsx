f6ef9b99ae4dac16faaa0d7a337c71098b6365d1
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2024-04-01 11:22:42 -0700

    make template preview images related to slide content not placeholder pics

--- tailwindui/components/slides/SlideDesignPreview.tsx
+++ tailwindui/components/slides/SlideDesignPreview.tsx
@@ -14,6 +14,7 @@ import layoutConfigData from './templates_customizable_elements/layout_elements'
 import { Explanation } from '../ui/Text';
 import { ScrollBar } from '../ui/ScrollBar';
 import { useProject } from '@/hooks/use-project';
+import { useSlides } from '@/hooks/use-slides';
 type SlideDesignPreviewProps = {
 	selectedTemplate: string;
 	selectedPalette: string;
@@ -23,7 +24,8 @@ const SlideDesignPreview: React.FC<SlideDesignPreviewProps> = ({
 	selectedTemplate,
 	selectedPalette,
 }) => {
-	const [slides, setSlides] = useState<Slide[]>([]);
+	const { slides } = useSlides();
+	const [previewSlides, setPreviewSlides] = useState<Slide[]>([]);
 	const { project } = useProject();
 	// const template = isValidTemplateKey(selectedTemplate) ? selectedTemplate : 'Default';
 	// const slides = Object.keys(layoutOptions).map((layoutKey) => {
@@ -38,6 +40,10 @@ const SlideDesignPreview: React.FC<SlideDesignPreviewProps> = ({
 	// });
 
 	useEffect(() => {
+		console.log(
+			'imported slides object layouts',
+			Object.values(slides).map((slide) => slide.layout),
+		);
 		const isValidTemplateKey = (key: string): key is TemplateKeys => {
 			return key in availableTemplates;
 		};
@@ -68,15 +74,50 @@ const SlideDesignPreview: React.FC<SlideDesignPreviewProps> = ({
 				newSlide.content = ['Some content here', 'Some more content here'];
 			}
 
-			newSlide.images = [
-				'https://img.freepik.com/free-photo/beatiful-blue-sky-with-clouds-sunny-day_839833-5069.jpg',
-				'https://upload.wikimedia.org/wikipedia/commons/0/07/Beatiful_kunar_Afghanistan.jpg',
-				'https://img.freepik.com/free-photo/roof-building-covered-snow-against-cloudy-sky_181624-37509.jpg',
-			];
+			// use imges that fit with the theme
+			// newSlide.images = [
+			// 	'https://img.freepik.com/free-photo/beatiful-blue-sky-with-clouds-sunny-day_839833-5069.jpg',
+			// 	'https://upload.wikimedia.org/wikipedia/commons/0/07/Beatiful_kunar_Afghanistan.jpg',
+			// 	'https://img.freepik.com/free-photo/roof-building-covered-snow-against-cloudy-sky_181624-37509.jpg',
+			// ];
+			if (
+				layoutKey === 'Col_1_img_1_layout' ||
+				layoutKey === 'Col_2_img_1_layout' ||
+				layoutKey === 'Col_2_img_2_layout' ||
+				layoutKey === 'Col_3_img_3_layout' ||
+				layoutKey === 'Cover_img_1_layout'
+			) {
+				// corresponding layout key and also images array should not empty
+				let slideWithImages = Object.values(slides).find(
+					(slide) => slide.layout === layoutKey && slide.images.length > 0,
+				);
+				console.log('find images for', layoutKey, slideWithImages);
+
+				if (!slideWithImages) {
+					// If no slide with images is found for the specified layout, search for a slide with 'col_3_img_3' layout
+					slideWithImages = Object.values(slides).find(
+						(slide) =>
+							slide.layout === 'Col_3_img_3_layout' && slide.images.length > 0,
+					);
+				}
+
+				if (slideWithImages) {
+					newSlide.images = slideWithImages.images;
+				} else {
+					// no such layout, placeholder pics
+					newSlide.images = [
+						'https://img.freepik.com/free-photo/beatiful-blue-sky-with-clouds-sunny-day_839833-5069.jpg',
+						'https://upload.wikimedia.org/wikipedia/commons/0/07/Beatiful_kunar_Afghanistan.jpg',
+						'https://img.freepik.com/free-photo/roof-building-covered-snow-against-cloudy-sky_181624-37509.jpg',
+					];
+				}
+			} else {
+				newSlide.images = [''];
+			}
 			return newSlide;
 		});
 
-		setSlides(newSlides);
+		setPreviewSlides(newSlides);
 	}, [selectedTemplate, selectedPalette]);
 
 	const layoutNameArray = [
@@ -86,7 +127,7 @@ const SlideDesignPreview: React.FC<SlideDesignPreviewProps> = ({
 		'2 columns without image',
 		'3 columns without image',
 		'1 column with 1 image',
-		'2 column with 1 image',
+		'2 columns with 1 image',
 		'2 columns with 2 images',
 		'3 columns with 3 images',
 	];
@@ -103,10 +144,10 @@ const SlideDesignPreview: React.FC<SlideDesignPreviewProps> = ({
 			canEdit,
 			exportToPdfMode,
 			false,
-			() => { },
-			() => { },
-			() => () => { },
-			() => { },
+			() => {},
+			() => {},
+			() => () => {},
+			() => {},
 			// slide.palette,
 			index === 0 || index === 1,
 			slide.layout,
@@ -115,7 +156,7 @@ const SlideDesignPreview: React.FC<SlideDesignPreviewProps> = ({
 		);
 	return (
 		<ScrollBar>
-			{slides.map((slide, index) => (
+			{previewSlides.map((slide, index) => (
 				<div
 					className='flex flex-col items-center gap-1 p-2'
 					key={`DesignpreviewContainer` + index.toString()}
