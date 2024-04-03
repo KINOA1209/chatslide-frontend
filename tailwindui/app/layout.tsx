854e36cceea020ab6a05c4afe1d0b8704956fe9c
Jackson Sun <jackson@jacksons-mbp-14.lan>
2024-04-03 12:11:43 -0500

    integrate Intercom

--- tailwindui/app/layout.tsx
+++ tailwindui/app/layout.tsx
@@ -65,6 +65,22 @@ export default function RootLayout({
 			<head>
 				<link rel='icon' href='/favicon.ico' />
 
+                <Script>
+                    {`
+                    window.intercomSettings = {
+                        api_base: "https://api-iam.intercom.io",
+                        app_id: "ozkjy22u",
+                    };
+                    `}
+                </Script>
+
+                <Script>
+                    {`
+                    // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/ozkjy22u'
+                    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/d0tz7e7z';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+                    `}
+                </Script>
+
 				<Script id='datadog-rum'>
 					{`(function(h,o,u,n,d) {
             h=h[d]=h[d]||{q:[],onReady:function(c){h.q.push(c)}}
