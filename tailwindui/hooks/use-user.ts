87843b41c6dcf82223f7996f6144f257b2b8f466
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-04-09 00:26:59 -0700

    [mixpanel] add tracking

--- tailwindui/hooks/use-user.ts
+++ tailwindui/hooks/use-user.ts
@@ -3,6 +3,8 @@ import { Auth as AmplifyAuth } from '@aws-amplify/auth';
 import { createBearStore } from '@/utils/create-bear-store';
 import UserService from '@/services/UserService';
 import AuthService from '@/services/AuthService';
+import mixpanel from 'mixpanel-browser'
+
 
 const useTokenBear = createBearStore<string>()('token', '', true, false);
 const useUidBear = createBearStore<string>()('uid', '', true, false);
@@ -92,6 +94,14 @@ export const useUser = () => {
 					isPaidUser: isPaidUser,
 				});
 
+				mixpanel.init('22044147cd36f20bf805d416e1235329', {
+					debug: false,
+					track_pageview: true,
+					persistence: 'localStorage',
+					ignore_dnt: true,
+				});
+
+				mixpanel.identify(uid);
 
 				window.steyAIRecord?.identify({
 					uid: uid,
