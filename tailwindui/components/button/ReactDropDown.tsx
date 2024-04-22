f9a25f58b3e1b2f6fd7fd85710c3d678c5708650
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-04-22 14:22:14 -0700

    [dropdown] add react dropdown (unused)

---
+++ tailwindui/components/button/ReactDropDown.tsx
@@ -0,0 +1,24 @@
+import Select from 'react-select';
+
+export const ReactDropDown: React.FC<{
+	options: { value: string; label: string | React.JSX.Element }[];
+	onChange: (value: string) => void;
+	placeholder: string;
+	value: string;
+}> = ({
+	options,
+	onChange,
+	placeholder,
+	value,
+}) => {
+
+		return (
+			<Select
+				options={options}
+				onChange={(selectedOption) => onChange(selectedOption?.value || '')}
+				value={options.find(option => option.value === value)}
+				placeholder={placeholder}
+				classNamePrefix="react-select"
+			/>
+		);
+	};
