3b30f8c010207f79f186a69fa30d052a625eb43e
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-04-27 12:58:12 -0700

    [video] change layout of bgm vol

--- tailwindui/app/(feature)/scripts/page.tsx
+++ tailwindui/app/(feature)/scripts/page.tsx
@@ -25,230 +25,241 @@ import ButtonWithExplanation from '@/components/button/ButtonWithExplanation';
 import { FiPlay } from 'react-icons/fi';
 import Modal from '@/components/ui/Modal';
 import RangeSlider from '@/components/ui/RangeSlider';
+import { WrappableRow } from '@/components/layout/WrappableRow';
 
 
 const ScriptSection = dynamic(
-	() => import('@/components/script/ScriptSection'),
-	{ ssr: false },
+  () => import('@/components/script/ScriptSection'),
+  { ssr: false },
 );
 
 
 export default function WorkflowStep5() {
-	const [isSubmitting, setIsSubmitting] = useState(false);
-	const { slides, updateSlidePage } = useSlides();
-	const { project, updateProject } = useProject();
-	const { username, token, updateCreditsFE, isPaidUser } = useUser();
-	const router = useRouter();
-	const [voice, setVoice] = useState('en-US-AvaNeural');
-	const [style, setStyle] = useState('');
-	const [avatar, setAvatar] = useState('');
-	const [posture, setPosture] = useState('casual-sitting');
-	const [size, setSize] = useState('medium');
-	const [position, setPosition] = useState('bottom-right');
-	const [bgm, setBgm] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { slides, updateSlidePage } = useSlides();
+  const { project, updateProject } = useProject();
+  const { username, token, updateCreditsFE, isPaidUser } = useUser();
+  const router = useRouter();
+  const [voice, setVoice] = useState('en-US-AvaNeural');
+  const [style, setStyle] = useState('');
+  const [avatar, setAvatar] = useState('');
+  const [posture, setPosture] = useState('casual-sitting');
+  const [size, setSize] = useState('medium');
+  const [position, setPosition] = useState('bottom-right');
+  const [bgm, setBgm] = useState('');
   const [bgmVolume, setBgmVolume] = useState(0.2);
-	const [showConfirmRegenModal, setShowConfirmRegenModal] = useState(false);
+  const [showConfirmRegenModal, setShowConfirmRegenModal] = useState(false);
 
-	const params = useSearchParams();
+  const params = useSearchParams();
 
-	if (!project) {
-		if (params.get('id')) {
-			router.push(`/project/${params.get('id')}`);
-		}
-		return <Blank>Project not found</Blank>;
-	}
+  if (!project) {
+    if (params.get('id')) {
+      router.push(`/project/${params.get('id')}`);
+    }
+    return <Blank>Project not found</Blank>;
+  }
 
-	const ConfirmVideoRegenModal: React.FC<{}> = () => {
-		return (
-			<Modal
-				showModal={showConfirmRegenModal}
-				setShowModal={setShowConfirmRegenModal}
-				title='Confirm Video Regeneration'
-			>
-				<Instruction>
-					You have a video generation job running. Do you want to regenerate the video? <br/>
-					This will cancel the current job and start a new one.
-				</Instruction>
-				<div className='flex flex-col gap-2 items-center'>
-				<BigBlueButton
-					onClick={() => {
-						router.push(addIdToRedir('/video'))
-					}}
-				>
-					No, View Running Job
-				</BigBlueButton>
-
-
-				<BigBlueButton
-					onClick={handleSubmitVideo}
-				>
-					{ avatar ? 
-					'Yes, Regenerate (400 ⭐️)' : 
-					'Yes, Regenerate (20 ⭐️)'}
-				</BigBlueButton>
-				</div>
-			</Modal>
-		)
-	}
-
-	async function confirmVideoJob() {
-		if (!project) {
-			console.error('No project found');
-			return;
-		}
-		const hasRunningVideoJob = await VideoService.hasRunningVideoJob(project.id, token);
-		if (hasRunningVideoJob)
-			setShowConfirmRegenModal(true);
-		else
-			handleSubmitVideo();
-	}
-
-	async function handleSubmitVideo() {
-		console.log('handleSubmitVideo');
-		if (!project) {
-			console.error('No project found');
-			return;
-		}
-		const foldername = project?.foldername;
-		const project_id = project.id;
-		if (!foldername || !project_id) {
-			console.error('No pid or foldername or project_id found');
-			setIsSubmitting(false);
-			return;
-		}
-
-		const generateVideo = async () => {
-			try {
-				console.log('project_id:', project_id);
-				updateProject('video_url', '');
-				VideoService.generateVideo(project_id, foldername, voice, token, style, avatar, posture, size, position, bgm, bgmVolume);
-				updateCreditsFE(-20);
-				router.push(addIdToRedir('/video'));
-			} catch (error) {
-				console.error('Error in fetchData:', error);
-				setIsSubmitting(false);
-			}
-		};
-		generateVideo();
-	};
+  const ConfirmVideoRegenModal: React.FC<{}> = () => {
+    return (
+      <Modal
+        showModal={showConfirmRegenModal}
+        setShowModal={setShowConfirmRegenModal}
+        title='Confirm Video Regeneration'
+      >
+        <Instruction>
+          You have a video generation job running. Do you want to regenerate the video? <br />
+          This will cancel the current job and start a new one.
+        </Instruction>
+        <div className='flex flex-col gap-2 items-center'>
+          <BigBlueButton
+            onClick={() => {
+              router.push(addIdToRedir('/video'))
+            }}
+          >
+            No, View Running Job
+          </BigBlueButton>
 
 
-	useEffect(() => {
-		if(isSubmitting) {
-			confirmVideoJob();
-			setIsSubmitting(false);
-		}
-	}, [isSubmitting]);
+          <BigBlueButton
+            onClick={handleSubmitVideo}
+          >
+            {avatar ?
+              'Yes, Regenerate (400 ⭐️)' :
+              'Yes, Regenerate (20 ⭐️)'}
+          </BigBlueButton>
+        </div>
+      </Modal>
+    )
+  }
 
-	// avoid hydration error during development caused by persistence
-	if (!useHydrated()) return <></>;
+  async function confirmVideoJob() {
+    if (!project) {
+      console.error('No project found');
+      return;
+    }
+    const hasRunningVideoJob = await VideoService.hasRunningVideoJob(project.id, token);
+    if (hasRunningVideoJob)
+      setShowConfirmRegenModal(true);
+    else
+      handleSubmitVideo();
+  }
 
-	return (
-		<div className='h-full w-full bg-white flex flex-col'>
-			{/* flex col container for steps, title, etc */}
+  async function handleSubmitVideo() {
+    console.log('handleSubmitVideo');
+    if (!project) {
+      console.error('No project found');
+      return;
+    }
+    const foldername = project?.foldername;
+    const project_id = project.id;
+    if (!foldername || !project_id) {
+      console.error('No pid or foldername or project_id found');
+      setIsSubmitting(false);
+      return;
+    }
 
-			<WorkflowStepsBanner
-				currentIndex={4}
-				isSubmitting={isSubmitting}
-				setIsSubmitting={setIsSubmitting}
-				isPaidUser={isPaidUser}
-				nextIsPaidFeature={true}
-				// todo: change credits
-				nextText={avatar ? 'Create Video (400 ⭐️)' : 'Create Video (20 ⭐️)'}
-			/>
+    const generateVideo = async () => {
+      try {
+        console.log('project_id:', project_id);
+        updateProject('video_url', '');
+        VideoService.generateVideo(project_id, foldername, voice, token, style, avatar, posture, size, position, bgm, bgmVolume);
+        updateCreditsFE(-20);
+        router.push(addIdToRedir('/video'));
+      } catch (error) {
+        console.error('Error in fetchData:', error);
+        setIsSubmitting(false);
+      }
+    };
+    generateVideo();
+  };
 
-			<ToastContainer enableMultiContainer containerId={'script'} />
 
-			{showConfirmRegenModal && <ConfirmVideoRegenModal />}
+  useEffect(() => {
+    if (isSubmitting) {
+      confirmVideoJob();
+      setIsSubmitting(false);
+    }
+  }, [isSubmitting]);
 
-			<Column>
-				<Card>
-					<BigTitle>🎙️ Voice</BigTitle>
-					<Instruction>
-						Select the voice you want to use for your video.
-					</Instruction>
-					<VoiceSelector
-						selectedVoice={voice}
-						setSelectedVoice={setVoice}
-						style={style}
-						setStyle={setStyle}
-					/>
-				</Card>
+  // avoid hydration error during development caused by persistence
+  if (!useHydrated()) return <></>;
 
-				<Card>
-					<BigTitle>
-						🎵 Background Music
-					</BigTitle>
-					<Instruction>
-						Select the background music you want to use for your video.
-					</Instruction>
-					<div className='flex flex-row gap-4 items-center'>
-						<DropDown
-							width='12rem'
-							onChange={(e) => setBgm(e.target.value)}
-							value={bgm}
-							style='input'
-						>
-							{Object.entries(bgmDisplayNames).map(([key, value]) => (
-								<option key={key} value={key}>
-									{value}
-								</option>
-							))}
-						</DropDown>
-						{bgm &&
-							<ButtonWithExplanation
-								explanation='Preview'
-								button={
-									<a href={`/bgm/${bgm}.mp3`} target='_blank'>
-										<FiPlay
-											style={{
-												strokeWidth: '2',
-												flex: '1',
-												width: '1.5rem',
-												height: '1.5rem',
-												fontWeight: 'bold',
-												color: '#344054',
-											}}
-										/>
-									</a>
-								}
-							/>}
-					</div>
-          <RangeSlider
-            onChange={(value: number) => {
-              if (value != 0)
-                setBgmVolume(value)
-            }
-            }
-            value={bgmVolume}
-            minValue={5}
-            choices={[0, 0.1, 0.2, 0.3, 0.4]}
+  return (
+    <div className='h-full w-full bg-white flex flex-col'>
+      {/* flex col container for steps, title, etc */}
+
+      <WorkflowStepsBanner
+        currentIndex={4}
+        isSubmitting={isSubmitting}
+        setIsSubmitting={setIsSubmitting}
+        isPaidUser={isPaidUser}
+        nextIsPaidFeature={true}
+        // todo: change credits
+        nextText={avatar ? 'Create Video (400 ⭐️)' : 'Create Video (20 ⭐️)'}
+      />
+
+      <ToastContainer enableMultiContainer containerId={'script'} />
+
+      {showConfirmRegenModal && <ConfirmVideoRegenModal />}
+
+      <Column>
+        <Card>
+          <BigTitle>🎙️ Voice</BigTitle>
+          <Instruction>
+            Select the voice you want to use for your video.
+          </Instruction>
+          <VoiceSelector
+            selectedVoice={voice}
+            setSelectedVoice={setVoice}
+            style={style}
+            setStyle={setStyle}
           />
+        </Card>
 
-				</Card>
+        <Card>
+          <BigTitle>
+            🎵 Background Music
+          </BigTitle>
+          <WrappableRow type='grid' cols={2}>
+            <div>
+              <Instruction>
+                Background Music:
+              </Instruction>
+              <div className='flex flex-row gap-4 items-center'>
+                <DropDown
+                  width='12rem'
+                  onChange={(e) => setBgm(e.target.value)}
+                  value={bgm}
+                  style='input'
+                >
+                  {Object.entries(bgmDisplayNames).map(([key, value]) => (
+                    <option key={key} value={key}>
+                      {value}
+                    </option>
+                  ))}
+                </DropDown>
+                {bgm &&
+                  <ButtonWithExplanation
+                    explanation='Preview'
+                    button={
+                      <a href={`/bgm/${bgm}.mp3`} target='_blank'>
+                        <FiPlay
+                          style={{
+                            strokeWidth: '2',
+                            flex: '1',
+                            width: '1.5rem',
+                            height: '1.5rem',
+                            fontWeight: 'bold',
+                            color: '#344054',
+                          }}
+                        />
+                      </a>
+                    }
+                  />}
+              </div>
+            </div>
+            <div>
+              <Instruction>
+                Volume: {bgmVolume * 100}
+              </Instruction>
+              <RangeSlider
+                onChange={(value: number) => {
+                  if (value !== 0){
+                    console.log(value)
+                    setBgmVolume(value)
+                  }
+                }}
+                value={bgmVolume}
+                minValue={0.1}
+                choices={[0, 0.1, 0.2, 0.3, 0.4]}
+              />
+            </div>
+          </WrappableRow>
 
-				<Card>
-					<BigTitle>🦹‍♂️ Avatar</BigTitle>
-					<Instruction>
-						Select the avatar you want to use for your video.<GrayLabel>Beta</GrayLabel> 
-					</Instruction>
-					<Explanation>
-						Due to the limitation of our resources, we can only provide a limited number of video generations with avatars. <br />
-						This feature will cost more credits. <br />
-						The credit cost for using an avatar may change in the future.
-					</Explanation>
-					<AvatarSelector
-						avatar={avatar}
-						setAvatar={setAvatar}
-						posture={posture}
-						setPosture={setPosture}
-						size={size}
-						setSize={setSize}
-						position={position}
-						setPosition={setPosition}
-					/>
-				</Card> 
-				{/* <Card>
+        </Card>
+
+        <Card>
+          <BigTitle>🦹‍♂️ Avatar</BigTitle>
+          <Instruction>
+            Select the avatar you want to use for your video.<GrayLabel>Beta</GrayLabel>
+          </Instruction>
+          <Explanation>
+            Due to the limitation of our resources, we can only provide a limited number of video generations with avatars. <br />
+            This feature will cost more credits. <br />
+            The credit cost for using an avatar may change in the future.
+          </Explanation>
+          <AvatarSelector
+            avatar={avatar}
+            setAvatar={setAvatar}
+            posture={posture}
+            setPosture={setPosture}
+            size={size}
+            setSize={setSize}
+            position={position}
+            setPosition={setPosition}
+          />
+        </Card>
+        {/* <Card>
 					<BigTitle>🦹‍♂️ Avatar</BigTitle>
 					<div className='flex flex-row gap-x-4 items-end'>
 						<Instruction>
@@ -262,33 +273,33 @@ export default function WorkflowStep5() {
 						/>
 					</div>
 				</Card> */}
-				<Card>
-					<BigTitle>📝 Scripts</BigTitle>
-					<Instruction>
-						<div className='flex flex-col gap-y-1'>
-							<p>💡 Script to voice tips: </p>
-							<p>⏸️ Use <span className='text-green-600'>...</span> to denote pause </p>
-							<p>*️⃣ Use <span className='text-green-600'>*word*</span> to denote emphasis </p>
-							<p>🔤 Use <span className='text-green-600'>[word]</span> to spell out the word. </p>
-							<p>🌟 For example: {' '}
-								<span className='text-blue-600 hover:cursor-pointer' onClick={() => previewVoice('denotation')}>🔈 We also supports creating *slides* from... [doc] files. </span>
-							</p>
-						</div>
-					</Instruction>
-					<div className='flex flex-col gap-y-2'>
-						{slides.map((_, index) => (
-							<ScriptSection
-								key={index}
-								slides={slides}
-								index={index}
-								voice={voice}
-								voiceStyle={style}
-								updateSlidePage={updateSlidePage}
-							/>
-						))}
-					</div>
-				</Card>
-			</Column>
-		</div>
-	);
+        <Card>
+          <BigTitle>📝 Scripts</BigTitle>
+          <Instruction>
+            <div className='flex flex-col gap-y-1'>
+              <p>💡 Script to voice tips: </p>
+              <p>⏸️ Use <span className='text-green-600'>...</span> to denote pause </p>
+              <p>*️⃣ Use <span className='text-green-600'>*word*</span> to denote emphasis </p>
+              <p>🔤 Use <span className='text-green-600'>[word]</span> to spell out the word. </p>
+              <p>🌟 For example: {' '}
+                <span className='text-blue-600 hover:cursor-pointer' onClick={() => previewVoice('denotation')}>🔈 We also supports creating *slides* from... [doc] files. </span>
+              </p>
+            </div>
+          </Instruction>
+          <div className='flex flex-col gap-y-2'>
+            {slides.map((_, index) => (
+              <ScriptSection
+                key={index}
+                slides={slides}
+                index={index}
+                voice={voice}
+                voiceStyle={style}
+                updateSlidePage={updateSlidePage}
+              />
+            ))}
+          </div>
+        </Card>
+      </Column>
+    </div>
+  );
 }
