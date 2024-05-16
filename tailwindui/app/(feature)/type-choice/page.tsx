ae4c40647aa1188adda269b3ba7e783099cbe7cb
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-05-16 01:03:35 -0700

    [scenario] ux

--- tailwindui/app/(feature)/type-choice/page.tsx
+++ tailwindui/app/(feature)/type-choice/page.tsx
@@ -3,76 +3,13 @@
 import React, { FC, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import '@/app/css/workflow-scenario-choice.css';
-import Image from 'next/image';
 import workflowTypeOptions from './options';
-import SessionStorage from '@/utils/SessionStorage';
-import AuthService from '@/services/AuthService';
 import { BackButton } from '@/components/button/DrlambdaButton';
 import { BigTitle, Explanation, Title } from '@/components/ui/Text';
 import { Column } from '@/components/layout/Column';
 import { useUser } from '@/hooks/use-user';
-import PaywallModal from '@/components/paywallModal';
 import useHydrated from '@/hooks/use-hydrated';
-import { ScenarioOption } from '../scenario-choice/slidesScenarios';
-
-export const ScenarioButton: FC<{
-	scenario: ScenarioOption;
-	navigate: (type: string) => void;
-}> = ({ scenario, navigate }) => {
-	const [showPaywall, setShowPaywall] = useState(false);
-	const { tier } = useUser();
-	return (
-		<>
-			<PaywallModal
-				showModal={showPaywall}
-				setShowModal={setShowPaywall}
-				message='🥇 Upgrade to ULTIMATE to get early access!'
-			/>
-			<div
-				key={scenario.id}
-				className={
-					'flex flex-col transition-transform transform-gpu ' +
-					(scenario?.featured ? 'hover:scale-110' : 'hover:scale-110')
-				}
-			>
-				<div
-					className={
-						'h-[250px] sm:h-[300px] rounded-lg shadow flex justify-center items-center cursor-pointer mb-4 ' +
-						(scenario?.featured ? 'bg-[#eff4ff]' : 'bg-gray-100')
-					}
-					onClick={() => {
-						if (scenario?.previewOnly) {
-							if (!tier.includes('ULTIMATE')) {
-								setShowPaywall(true);
-								return;
-							}
-						}
-						navigate(scenario.id);
-					}}
-				>
-					<Image
-						className='mx-[20px] mh-[20px]'
-						width={281}
-						height={174}
-						alt={scenario.id}
-						src={scenario.imageSrc}
-					/>
-				</div>
-				<div className='text-center my-2 leading-snug tracking-tight whitespace-nowrap font-bold'>
-					{scenario.title}
-				</div>
-				{scenario?.previewOnly && (
-					<Explanation>
-						<div className='text-center'>
-							This feature is in Beta mode. <br />
-							Early access limited to Ultimate users.
-						</div>
-					</Explanation>
-				)}
-			</div>
-		</>
-	);
-};
+import { ScenarioButton } from '@/components/button/ScenarioButton';
 
 const ScenarioChoicePage = () => {
 	const router = useRouter(); // Initialize the router
