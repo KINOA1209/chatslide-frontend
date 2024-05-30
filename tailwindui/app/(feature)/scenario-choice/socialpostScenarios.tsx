3a5d173e541d762ef34bada73699e600e0578a4e
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2024-05-29 22:02:31 -0700

    update social post scenario choice thumbnail

--- tailwindui/app/(feature)/scenario-choice/socialpostScenarios.tsx
+++ tailwindui/app/(feature)/scenario-choice/socialpostScenarios.tsx
@@ -2,32 +2,31 @@ import React from 'react';
 import { FaCoffee, FaBook, FaQuoteRight } from 'react-icons/fa';
 import { ScenarioOption } from './slidesScenarios';
 
-
 const options: ScenarioOption[] = [
 	{
 		id: 'casual_topic',
 		title: 'Casual Topic',
-		imageSrc: '/images/socialpost/casual_topic.png',
+		imageSrc: '/images/socialpost/casual_topic_thumbnail_classic.png',
 		icon: <FaCoffee />,
 	},
 	{
 		id: 'serious_subject',
 		title: 'Serious Subject',
-		imageSrc: '/images/socialpost/serious_subject.png',
+		imageSrc: '/images/socialpost/serious_subject_thumbnail_classic.png',
 		icon: <FaBook />,
 	},
 	{
 		id: 'reading_notes',
 		title: 'Quote Sharing',
-		imageSrc: '/images/socialpost/reading_notes.png',
+		imageSrc: '/images/socialpost/reading_notes_thumbnail_classic.png',
 		icon: <FaQuoteRight />,
 	},
 ];
 
 const socialpostScenarios = {
-  message: 'What style do you prefer?',
-  description: 'We will create contents tailored to your audience.',
-  options,
+	message: 'What style do you prefer?',
+	description: 'We will create contents tailored to your audience.',
+	options,
 };
 
 export default socialpostScenarios;
