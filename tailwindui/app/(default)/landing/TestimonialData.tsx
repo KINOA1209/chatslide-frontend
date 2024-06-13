c1499f96e21db1f98d2bd5cd4eec56c8f4d982f2
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-06-13 11:30:38 +0800

    [landing] differentiate testimonials

---
+++ tailwindui/app/(default)/landing/TestimonialData.tsx
@@ -0,0 +1,102 @@
+const DrLambdaTestimonialsData = [
+	{
+		name: 'Passivern',
+		title: 'Youtuber',
+		text: 'The AI slide generator and video generator is really awesome.',
+		profile_url: 'images/passivern.jpeg',
+	},
+	{
+		name: 'Nene',
+		title: 'Graphic Designer | Pianist',
+		text: 'There is an excellent AI service for this, DrLambda, it can quickly and easily turn any website, video or document into a presentation.',
+		profile_url: 'images/YPBzEepB_400x400.jpeg',
+	},
+	{
+		name: 'Edinaldo Oliveira',
+		title: 'Hacker Osint | IA',
+		text: '🇧🇷 Drlambda – rede neural que transforma páginas da web, arquivos PDF e vídeos em uma apresentação com slides	interativos. E é grátis.',
+		profile_url: 'images/nkSbABHU_400x400.jpeg',
+	},
+	{
+		name: 'Mohini Shewale',
+		title: 'AI Content Creator',
+		text: 'This game-changer revolutionized my content creation—no more copy-pasting chaos, just effortless brilliance!',
+		profile_url: 'images/1706594634503.jpeg',
+	},
+	{
+		name: 'Md Riyazuddin',
+		title: 'Founder at Digiwin | AI Enthusiast',
+		text: 'This all-in-one tool seamlessly turns my documents, YouTube videos, and web findings into stunning slides and social posts. 📊💡',
+		profile_url: 'images/t1.jpeg',
+	},
+	{
+		name: 'Marco Linke',
+		title: 'Designers Inn & Businesserfolg',
+		text: '🇩🇪 DrLamdba ist so gestaltet, dass jeder, unabhängig	vom Fachwissen, beeindruckende Präsentationen erstellen kann.',
+		profile_url: 'images/Screen-Shot-2024-03-18-at-5.36.59-PM.png',
+	},
+	{
+		name: 'Robert Leitinger',
+		title: 'Designers Inn & Businesserfolg',
+		text: 'I tried different tools for making presentations with AI - also the famous one like Tome. So i also tried DrLambda and was impressed - you get a great output with very little effort!',
+		profile_url: 'images/robert-leitinger.webp',
+	},
+	{
+		name: 'The Learning Point',
+		text: 'Great concept and cuts down on a lot of work creating presentations. ',
+		profile_url: 'images/thelearningpoint.jpeg',
+	},
+	{
+		name: 'Mega Kris',
+		text: '🇷🇺 Презентация из любых файлов: {getBrand()} соберет PDF, сайты и видеоролики в готовые слайды"',
+		profile_url: 'images/QFfM1EWi_400x400.jpeg',
+	},
+	{
+		name: 'Ismail El Kasmi',
+		title: 'CEO &amp; Founder of Landrific',
+		text: 'Interesting product. Intuitive, easy-to-use, helpful	outline and script generation.',
+		profile_url: 'images/t4.jpeg',
+	},
+	{
+		name: 'YYYJ',
+		title: 'Serial entrepreneur | Growth hacker',
+		text: 'In terms of design and build, {getBrand()} has a robust and sleek design. So far it performs well as what I expect of PDF to slide transformation.',
+		profile_url: 'images/Screen-Shot-2024-03-18-at-5.34.11-PM.png',
+	},
+	{
+		name: '小互',
+		title: '财经博主',
+		text: '🇨🇳 是一个强大的内容整理和组织工具，可以大幅度提高你的工作效率。适合研究人员、教师、学生、顾问和办公室工作人员。',
+		profile_url: 'images/xiaohu.jpeg',
+	},
+	{
+		name: 'Andrew',
+		// title: '财经博主',
+		text: "I love how easy it is to use and the cool features it offers. I give DrLambda 5 tacos, it's that good!	🌮🌮🌮🌮🌮",
+		profile_url: 'images/andrew.webp',
+	},
+	{
+		name: 'Mucio',
+		// title: '财经博主',
+		text: 'I fully recommend this incredible tool for creators of courses or education persons. You will be able to download your presentation created in minutes and you can create a video for your socialmedia.',
+		profile_url: 'images/mucio.png',
+	},
+	{
+		name: 'Latif Miah',
+		title: 'Teacher',
+		text: ' DrLambda surpasses similar software and shows potential to rival industry leaders. I wholeheartedly recommend it to educators, especially for course creation.',
+		profile_url: 'images/latif.webp',
+	},
+];
+
+const ChatSlideTestimonialsData = [
+	{
+		name: 'Yaser Alosefer',
+		title: 'Entrepreneur, Investor',
+		text: '🇸🇦 "عروض بثواني😎 لاتكتب اي عروض بعد اليوم، كل ما هو عليك	فقط كتابة ماتريد للـAI وهو يكتب العرض كامل لك مع الصور	والمعلومات من بحثك وملفاتك ويلخصها وكل شي، يعني كأنه كاتب "',
+		profile_url: 'images/yaser.jpeg',
+		ltr: true,
+	},
+];
+
+export { DrLambdaTestimonialsData, ChatSlideTestimonialsData };
