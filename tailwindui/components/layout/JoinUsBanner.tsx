da33c0e6a9f2f8c236d85ec08dcf20d9b4cd537e
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-06-13 19:19:23 +0800

    [shared] persist params when click join us

--- tailwindui/components/layout/JoinUsBanner.tsx
+++ tailwindui/components/layout/JoinUsBanner.tsx
@@ -6,21 +6,31 @@ import useHydrated from '@/hooks/use-hydrated';
 
 export const JoinUsBanner: React.FC = () => {
 	const { token } = useUser();
+	const isHydrated = useHydrated();
 
 	// avoid hydration error during development caused by persistence
-	if (!useHydrated()) return <></>;
+	if (!isHydrated) return <></>;
 
 	if (token && token !== '') {
 		return null;
 	}
 
+	// Get current URL query parameters
+	const currentQueryParams = new URLSearchParams(window.location.search);
+
+	// Add the ref parameter
+	currentQueryParams.set('ref', 'shared');
+
+	// Create the new URL with combined parameters
+	const newUrl = `/signup?${currentQueryParams.toString()}`;
+
 	return (
-		<Link href='/signup?ref=shared'>
+		<Link href={newUrl}>
 			<div
 				className='flex items-center justify-center bg-Blue text-white text-sm sm:text-lg leading-10 tracking-wide'
 				id='join-us-banner'
 			>
-				Join 210k happy users to create professional slides! 🚀
+				Join 210k happy users to create professional slides! 🚀 Start with 20⭐️ credits.
 			</div>
 		</Link>
 	);
