95c413c0777e7dab6bb5f3158aebd468baf822e9
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2024-08-12 12:34:55 +0800

    define RootLayoutProps for type error

--- tailwindui/app/layout.tsx
+++ tailwindui/app/layout.tsx
@@ -58,32 +58,27 @@ export const generateMetadata = (
 	};
 };
 
-export default function RootLayout(
-	{
-		children,
-	}: {
-		children: React.ReactNode;
-	} = { children: null },
-) {
-	// Provide default value if needed
+interface RootLayoutProps {
+	children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
 	return (
-		<>
-			<html lang='en'>
-				<head>
-					<link
-						rel='icon'
-						href={isChatslide() ? '/favicon_chatslide.ico' : '/favicon.ico'}
-					/>
-					{!isLocal() && <TrackingScripts />}
-				</head>
-				<body
-					className={`font-inter antialiased bg-white text-gray-900 tracking-tight`}
-				>
-					<div className='Simpleflex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip'>
-						{children}
-					</div>
-				</body>
-			</html>
-		</>
+		<html lang='en'>
+			<head>
+				<link
+					rel='icon'
+					href={isChatslide() ? '/favicon_chatslide.ico' : '/favicon.ico'}
+				/>
+				{!isLocal() && <TrackingScripts />}
+			</head>
+			<body
+				className={`font-inter antialiased bg-white text-gray-900 tracking-tight`}
+			>
+				<div className='Simpleflex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip'>
+					{children}
+				</div>
+			</body>
+		</html>
 	);
 }
