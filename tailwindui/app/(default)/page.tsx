20e19e6836fcc16152e6c58b19c344f1365b9daf
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-08-22 23:08:39 +0800

    [seo] fix dev err

--- tailwindui/app/(default)/page.tsx
+++ tailwindui/app/(default)/page.tsx
@@ -1,13 +1,9 @@
 import Landing from './landing/page';
 import AutoRedir from './AutoRedir';
-import { Helmet } from 'react-helmet';
 
 function App() {
 	return (
 		<>
-			<Helmet>
-				<link rel='canonical' href='/landing' />
-			</Helmet>
 			<AutoRedir />
 			<Landing />
 		</>
