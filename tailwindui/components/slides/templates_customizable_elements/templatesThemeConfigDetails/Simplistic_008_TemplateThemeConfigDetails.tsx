6178377c13250dc7ab09c4e6d8f9fd92c064532d
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-08-23 18:06:25 +0800

    [template] restore gradient bg

--- tailwindui/components/slides/templates_customizable_elements/templatesThemeConfigDetails/Simplistic_008_TemplateThemeConfigDetails.tsx
+++ tailwindui/components/slides/templates_customizable_elements/templatesThemeConfigDetails/Simplistic_008_TemplateThemeConfigDetails.tsx
@@ -14,10 +14,11 @@ export const Simplistic_008_TemplateThemeConfig: {
 	[key in PaletteKeys]?: ThemeElements;
 } = {
 	'Dynamic Purple': {
-		backgroundColorCover: '#A388F7',
+		// backgroundColorCover: '#A388F7',
 		isGradientBackground: true,
-		// backgroundColorCover: 'linear-gradient(to bottom right, #7E96F7, #A388F7)',
-		backgroundColor: '#A388F7',
+		backgroundColorCover: 'linear-gradient(to bottom right, #7E96F7, #A388F7)',
+		backgroundColor: 'linear-gradient(to bottom right, #7E96F7, #A388F7)',
+		// backgroundColor: '#A388F7',
 		titleFontCSS: {
 			fontSize: '14pt',
 			fontWeight: 'Bold',
